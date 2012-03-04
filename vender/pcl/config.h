554fd44475b91b38523483aa4e8ea7353da1d635
xicilion <lion@9465.net>
2012-03-04 04:33:47 +0000

    增加 Portable Coroutine Library，并修复 x64 和 mingw 兼容问题。

---
+++ vender/pcl/config.h
@@ -0,0 +1,121 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `free' function. */
+#define HAVE_FREE 1
+
+/* Define to 1 if you have the `getcontext' function. */
+#define HAVE_GETCONTEXT 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `longjmp' function. */
+#define HAVE_LONGJMP 1
+
+/* Define to 1 if you have the `makecontext' function. */
+#define HAVE_MAKECONTEXT 1
+
+/* Define to 1 if you have the `malloc' function. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the `setjmp' function. */
+#define HAVE_SETJMP 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define to 1 if you have the `sigaltstack' function. */
+#define HAVE_SIGALTSTACK 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `swapcontext' function. */
+#define HAVE_SWAPCONTEXT 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libpcl"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libpcl"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libpcl 1.12"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libpcl"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.12"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.12"
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to empty if the keyword `volatile' does not work. Warning: valid
+   code using `volatile' can become incorrect without. Disable with care. */
+/* #undef volatile */
---
+++ vender/pcl/pcl_private.c
@@ -0,0 +1,159 @@
+/*
+ *  PCL by Davide Libenzi (Portable Coroutine Library)
+ *  Copyright (C) 2003..2010  Davide Libenzi
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Davide Libenzi <davidel@xmailserver.org>
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pcl_config.h"
+#include "pcl.h"
+#include "pcl_private.h"
+
+static cothread_ctx *co_get_global_ctx(void)
+{
+	static cothread_ctx tctx;
+
+	if (tctx.co_curr == NULL)
+		tctx.co_curr = &tctx.co_main;
+
+	return &tctx;
+}
+
+#if !defined(CO_MULTI_THREAD)
+/*
+ * Simple case, the single thread one ...
+ */
+
+int co_thread_init(void)
+{
+	return 0;
+}
+
+void co_thread_cleanup(void)
+{
+
+}
+
+cothread_ctx *co_get_thread_ctx(void)
+{
+	return co_get_global_ctx();
+}
+
+#else
+/*
+ * MultiThread cases ...
+ */
+
+# ifdef Windows
+/*
+ * On Windows, we can use the native TLS capabilities. Pretty easy ...
+ */
+static __declspec(thread) cothread_ctx *tctx;
+
+int co_thread_init(void)
+{
+	if ((tctx = (cothread_ctx *)
+	     malloc(sizeof(cothread_ctx))) == NULL) {
+		perror("allocating context");
+		return -1;
+	}
+	memset(tctx, 0, sizeof(*tctx));
+	tctx->co_curr = &tctx->co_main;
+
+	return 0;
+}
+
+void co_thread_cleanup(void)
+{
+	free(tctx);
+}
+
+cothread_ctx *co_get_thread_ctx(void)
+{
+	/*
+	 * Even in MT mode, allows for the main thread to not call
+	 * the co_thread_init()/co_thread_cleanup() functions.
+	 */
+	return tctx != NULL ? tctx: co_get_global_ctx();
+}
+
+#else
+/*
+ * On Unix, we use pthread. Sligthly more complicated ...
+ */
+#include <pthread.h>
+
+static int valid_key;
+static pthread_key_t key;
+static pthread_once_t once_control = PTHREAD_ONCE_INIT;
+
+static void co_once_init(void)
+{
+	if (pthread_key_create(&key, free))
+		perror("creating TLS key");
+	else
+		valid_key++;
+}
+
+int co_thread_init(void)
+{
+	cothread_ctx *tctx;
+
+	pthread_once(&once_control, co_once_init);
+	if (!valid_key)
+		return -1;
+
+	if ((tctx = (cothread_ctx *)
+	     malloc(sizeof(cothread_ctx))) == NULL) {
+		perror("allocating context");
+		return -1;
+	}
+	memset(tctx, 0, sizeof(*tctx));
+	tctx->co_curr = &tctx->co_main;
+	if (pthread_setspecific(key, tctx)) {
+		perror("setting thread context");
+		free(tctx);
+		return -1;
+	}
+
+	return 0;
+}
+
+void co_thread_cleanup(void)
+{
+
+}
+
+cothread_ctx *co_get_thread_ctx(void)
+{
+	cothread_ctx *tctx = (cothread_ctx *)
+		(valid_key ? pthread_getspecific(key): NULL);
+
+	/*
+	 * Even in MT mode, allows for the main thread to not call
+	 * the co_thread_init()/co_thread_cleanup() functions.
+	 */
+	return tctx != NULL ? tctx: co_get_global_ctx();
+}
+
+#endif
+#endif
+
