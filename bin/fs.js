a7884122284fd9deda2d9748ec62b57ab997e138
xicilion <lion@9465.net>
2012-04-11 09:43:59 +0000

    fs 用例。

--- bin/fs.js
+++ bin/fs.js
@@ -40,3 +40,39 @@ assert.equal(st.isFile(), false);
 assert.equal(st.isExecutable(), true);
 assert.equal(st.isReadable(), true);
 assert.equal(st.isWritable(), true);
+
+var f = fs.open('fs.js');
+assert.doesNotThrow(function() {
+	f.read(100);
+});
+f.close();
+assert.throws(function() {
+	f.read(100);
+});
+
+f = fs.open('fs.js');
+
+var st = fs.stat('fs.js');
+assert.equal(st.size, f.size());
+
+var f1 = fs.open('fs.js.bak', 'w');
+f1.write(f.read());
+
+assert.equal(true, f.eof());
+assert.equal(true, f1.eof());
+
+f.close();
+
+st = fs.stat('fs.js');
+assert.equal(st.size, f1.size());
+
+assert.equal(st.size, f1.tell());
+f1.rewind();
+assert.equal(0, f1.tell());
+
+console.dir(f1.stat());
+
+f1.close();
+
+fs.unlink('fs.js.bak');
+
