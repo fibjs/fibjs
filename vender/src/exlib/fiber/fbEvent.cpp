fd1288450b25d89da67e6acf552055c987a89f68
xicilion <lion@9465.net>
2012-04-15 12:20:11 +0000

    修改 Event 逻辑，空等待对象不查询服务对象。

--- vender/src/exlib/fiber/fbEvent.cpp
+++ vender/src/exlib/fiber/fbEvent.cpp
@@ -12,41 +12,43 @@ namespace exlib
 {
 void Event::wait()
 {
-    if(!m_set)
-    {
-        Service* pService = Service::getFiberService();
+	if (!m_set)
+	{
+		Service* pService = Service::getFiberService();
 
-        m_blocks.put(pService->m_running);
-        pService->switchtonext();
-    }
+		m_blocks.put(pService->m_running);
+		pService->switchtonext();
+	}
 }
 
 void Event::pulse()
 {
-    Service* pService = Service::getFiberService();
-    Fiber* cntxt;
+	Service* pService = NULL;
+	Fiber* cntxt;
 
-    while(!m_blocks.empty())
-    {
-        cntxt = m_blocks.get();
-        pService->m_resume.put(cntxt);
-    }
+	while (!m_blocks.empty())
+	{
+		cntxt = m_blocks.get();
+		if (pService == NULL)
+			pService = Service::getFiberService();
+		pService->m_resume.put(cntxt);
+	}
 }
 
 void Event::set()
 {
-    m_set = true;
-    pulse();
+	m_set = true;
+	pulse();
 }
 
 void Event::reset()
 {
-    m_set = false;
+	m_set = false;
 }
 
 bool Event::trywait()
 {
-    return m_set;
+	return m_set;
 }
 
 }
