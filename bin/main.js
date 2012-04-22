1eb62875159225b6d3d1315bdc0da00e3e369b3b
xicilion <lion@9465.net>
2012-04-22 13:22:21 +0000

    增加 net 测试用例。

--- bin/main.js
+++ bin/main.js
@@ -1,12 +1,13 @@
 
 require("buffer_test.js");
 require("path_test");
-require("fiber_test");
+//require("fiber_test");
 require("fs_test");
 require("os_test");
 require("encoding_test");
 require("module_test");
 require("define_test");
+require("net_test");
 
 require('BUG_simple_api_call');
 console.log("TEST End.");
---
+++ bin/net_test.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+
+var assert = require('assert');
+var net = require('net');
+
+var s = net.socket(net.AF_INET, net.SOCK_STREAM);
