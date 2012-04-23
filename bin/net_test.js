ca2d3a7a1d7a064755516a5f12b8111b71befd75
xicilion <lion@9465.net>
2012-04-23 14:41:02 +0000

    socket 用例。

--- bin/net_test.js
+++ bin/net_test.js
@@ -5,5 +5,27 @@
 var assert = require('assert');
 var net = require('net');
 
+function accept(s)
+{
+	while(1)
+	{
+		var c = s.accept();
+		console.log(c.family);
+		c.write(c.read());
+		c.close();
+		c.dispose();
+	}
+}
+
 var s = net.socket(net.AF_INET6, net.SOCK_STREAM);
-s.connect('::1', 80);
+s.bind(8080);
+s.listen();
+accept.start(s);
+
+for(var i = 0; i < 100; i ++)
+{
+	var s1 = net.socket(net.AF_INET6, net.SOCK_STREAM);
+	s1.connect('', 8080);
+	s1.write(new Buffer("GET / HTTP/1.0\n\n"));
+	console.log(s1.read());
+}
