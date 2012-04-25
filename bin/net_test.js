1a85e7c85e53f2e4df8ae4f1ee226c674ae857c1
xicilion <lion@9465.net>
2012-04-25 09:03:51 +0000

    改写 net 用例，使用 new net.Socket 创建 socket。

--- bin/net_test.js
+++ bin/net_test.js
@@ -17,14 +17,14 @@ function accept(s)
 	}
 }
 
-var s = net.socket(net.AF_INET6, net.SOCK_STREAM);
+var s = new net.Socket(net.AF_INET6, net.SOCK_STREAM);
 s.bind(8080);
 s.listen();
 accept.start(s);
 
 for(var i = 0; i < 100; i ++)
 {
-	var s1 = net.socket(net.AF_INET, net.SOCK_STREAM);
+	var s1 = new net.Socket(net.AF_INET, net.SOCK_STREAM);
 	s1.connect('192.168.65.52', 8080);
 	s1.write(new Buffer("GET / HTTP/1.0\n\n"));
 	console.log(s1.read());
