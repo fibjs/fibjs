e3f6afa3fd5a8eae98e0302054cedd4a749e52ea
xicilion <lion@9465.net>
2012-04-25 09:44:54 +0000

    增加用例模块提示信息。

--- bin/main.js
+++ bin/main.js
@@ -1,13 +1,23 @@
 
+console.log('buffer testing....');
 require("buffer_test.js");
+console.log('path testing....');
 require("path_test");
+console.log('fiber testing....');
 //require("fiber_test");
+console.log('fs testing....');
 require("fs_test");
+console.log('os testing....');
 require("os_test");
+console.log('encoding testing....');
 require("encoding_test");
+console.log('module testing....');
 require("module_test");
+console.log('define testing....');
 require("define_test");
+console.log('net testing....');
 require("net_test");
 
+console.log('BUG_simple_api_call testing....');
 require('BUG_simple_api_call');
 console.log("TEST End.");
