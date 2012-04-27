913ef5960e4c6de97506c432e84ecb30643dc5f5
xicilion <lion@9465.net>
2012-04-27 05:53:18 +0000

    net  测试用例。

--- bin/net_test.js
+++ bin/net_test.js
@@ -4,13 +4,14 @@
 
 var assert = require('assert');
 var net = require('net');
+var coroutine = require('coroutine');
 
 function accept(s)
 {
 	while(1)
 	{
 		var c = s.accept();
-//		console.log(c.remoteAddress, c.remotePort, "->", c.localAddress, c.localPort);
+		console.log(c.remoteAddress, c.remotePort, "->", c.localAddress, c.localPort);
 		c.write(c.read());
 		c.close();
 		c.dispose();
@@ -22,10 +23,11 @@ s.bind(8080);
 s.listen();
 accept.start(s);
 
-for(var i = 0; i < 10; i ++)
+for(var i = 0; i < 1000; i ++)
 {
 	var s1 = new net.Socket(net.AF_INET6, net.SOCK_STREAM);
 	s1.connect('::1', 8080);
-	s1.write(new Buffer("GET / HTTP/1.0\n\n"));
-//	console.log(s1.read());
+	s1.write(new Buffer("GET / HTTP/1.0"));
+	console.log(s1.read());
+	s1.dispose();
 }
