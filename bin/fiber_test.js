41c95a3cb146e0660f2e7dc3225d0cc8eb0c0d71
xicilion <lion@9465.net>
2012-04-30 13:05:34 +0000

    Fiber onexit onerror 测试用例。

--- bin/fiber_test.js
+++ bin/fiber_test.js
@@ -1,12 +1,10 @@
-
 var assert = require('assert');
 var os = require('os');
 var coroutine = require('coroutine');
 
 var n = 123;
 
-function t_fiber(v1, v2)
-{
+function t_fiber(v1, v2) {
 	n = v1 + v2;
 }
 
@@ -15,15 +13,12 @@ assert.equal(n, 123);
 coroutine.sleep();
 assert.equal(n, 300);
 
-
 var f = coroutine.start(t_fiber, 123, 200);
 assert.equal(n, 300);
 f.join();
 assert.equal(n, 323);
 
-
-function t_fiber1(v1, v2)
-{
+function t_fiber1(v1, v2) {
 	n = v1 + v2 + this.v;
 }
 
@@ -33,9 +28,7 @@ f.v = 1000;
 f.join();
 assert.equal(n, 1300);
 
-
-function t_fiber2(v1, v2)
-{
+function t_fiber2(v1, v2) {
 	n = v1 + v2 + coroutine.current().v;
 }
 
@@ -52,8 +45,7 @@ coroutine.sleep(100);
 f.join();
 assert.equal(n, 1300);
 
-function t_fiber3(v1, v2)
-{
+function t_fiber3(v1, v2) {
 	n = v1 + v2 + this.caller.v;
 }
 
@@ -63,57 +55,82 @@ coroutine.current().v = 1234;
 f.join();
 assert.equal(n, 1534);
 
+n = 0;
+function t_fiber4(a1, a2) {
+	return a1 + a2 + 123;
+}
+t_fiber4.start(100, 200).onexit(function(r) {
+	n = r + 1000;
+});
+coroutine.sleep();
+assert.equal(n, 1423);
+
+var n1 = 0;
+n = 0;
+function t_fiber5(a1, a2) {
+	return a1 + a2 + 123 + notExistsValue;
+}
+t_fiber5.start(100, 200).on({
+	exit : function(r) {
+		assert.equal(r, undefined);
+		n = 1000;
+	},
+	error : function(e) {
+		n1 = 2000;
+	}
+});
+coroutine.sleep();
+assert.equal(n, 1000);
+assert.equal(n1, 2000);
+
 var nCount = 20000;
 
 var bDone = false;
 
-function t_switch()
-{
-	while(!bDone)
+function t_switch() {
+	while (!bDone)
 		coroutine.sleep();
 }
 
-//t_switch.start();
-console.time('switch '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+// t_switch.start();
+console.time('switch ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	coroutine.sleep();
-console.timeEnd('switch '+nCount+' times');
+console.timeEnd('switch ' + nCount + ' times');
 
 bDone = true;
 coroutine.sleep();
 
 var n = 0;
 
-function t_nop()
-{
+function t_nop() {
 }
 
-console.time('start '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+console.time('start ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	t_nop.start();
-console.timeEnd('start '+nCount+' times');
+console.timeEnd('start ' + nCount + ' times');
 
-console.time('run '+nCount+' times');
+console.time('run ' + nCount + ' times');
 coroutine.sleep();
-console.timeEnd('run '+nCount+' times');
+console.timeEnd('run ' + nCount + ' times');
 
-console.time('call '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+console.time('call ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	n = t_nop();
-console.timeEnd('call '+nCount+' times');
+console.timeEnd('call ' + nCount + ' times');
 
-
-console.time('os.CPUs '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+console.time('os.CPUs ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	os.CPUs();
-console.timeEnd('os.CPUs '+nCount+' times');
+console.timeEnd('os.CPUs ' + nCount + ' times');
 
-console.time('new Buffer '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+console.time('new Buffer ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	new Buffer();
-console.timeEnd('new Buffer '+nCount+' times');
+console.timeEnd('new Buffer ' + nCount + ' times');
 
-console.time('require '+nCount+' times');
-for(var i = 0; i < nCount; i ++)
+console.time('require ' + nCount + ' times');
+for ( var i = 0; i < nCount; i++)
 	require('os');
-console.timeEnd('require '+nCount+' times');
+console.timeEnd('require ' + nCount + ' times');
