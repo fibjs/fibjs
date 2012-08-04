73269b2106e47b029e4eec32e627ef1d3c5f3a21
xicilion <lion@9465.net>
2012-08-04 05:26:05 +0000

    修改错误日志输出错误信息为红色，以区别于正常信息。

--- fibjs/src/fibjs.cpp
+++ fibjs/src/fibjs.cpp
@@ -12,6 +12,24 @@
 #include <exlib/lockfree.h>
 #include "AsyncCall.h"
 
+#define RESET "\033[0m"
+#define BLACK "\033[30m" /* Black */
+#define RED "\033[31m" /* Red */
+#define GREEN "\033[32m" /* Green */
+#define YELLOW "\033[33m" /* Yellow */
+#define BLUE "\033[34m" /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN "\033[36m" /* Cyan */
+#define WHITE "\033[37m" /* White */
+#define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define BOLDRED "\033[1m\033[31m" /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m" /* Bold White */
+
 namespace fibjs
 {
 v8::Isolate* isolate;
@@ -23,6 +41,9 @@ public:
 	MyAppender() :
 			LayoutAppender("console")
 	{
+#ifdef _WIN32
+		m_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+#endif
 	}
 
 	void close()
@@ -33,14 +54,23 @@ protected:
 	void _append(const log4cpp::LoggingEvent& event)
 	{
 		if (event.priority < log4cpp::Priority::NOTICE)
+		{
 #ifndef _WIN32
-			std::cerr << "\033[1m" << event.message << "\033[22m" << std::endl;
+			std::cerr << RED << event.message << RESET << std::endl;
 #else
+			SetConsoleTextAttribute(m_hConsole, FOREGROUND_RED);
 			std::cerr << event.message << std::endl;
+			SetConsoleTextAttribute(m_hConsole, FOREGROUND_INTENSITY);
 #endif
+		}
 		else
 			std::cout << event.message << std::endl;
 	}
+
+#ifdef _WIN32
+private:
+	HANDLE m_hConsole;
+#endif
 };
 
 void initModule();
