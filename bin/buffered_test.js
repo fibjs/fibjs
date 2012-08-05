aa7310f7e511a8d9e2c1b825a6a0644893e29a20
xicilion <lion@9465.net>
2012-08-05 08:38:32 +0000

    buffered 测试用例。

--- bin/buffered_test.js
+++ bin/buffered_test.js
@@ -7,6 +7,7 @@ console.log('buffered testing....');
 var assert = require('assert');
 var io = require('io');
 var os = require('os');
+var net = require('net');
 
 var s = '0123456789\r\n';
 
@@ -17,8 +18,7 @@ var f = io.open("test0000", 'w');
 f.write(new Buffer(s));
 f.close();
 
-function t_read(sz) {
-	f = io.open("test0000");
+function t_read(f, sz) {
 	var r = new io.BufferedStream(f);
 	var p = 0;
 	var d;
@@ -33,8 +33,41 @@ function t_read(sz) {
 	f.close();
 }
 
-for ( var i = 3; i < 100000; i *= 7)
-	t_read(i);
+for ( var i = 3; i < 100000; i *= 3)
+	t_read(io.open("test0000"), i);
 
+function accept1(s) {
+	while (true) {
+		var c = s.accept();
+		var f = io.open('test0000');
+		f.copyTo(c);
+		f.close();
+		c.close();
+	}
+}
+
+var s1 = new net.Socket(net.AF_INET6, net.SOCK_STREAM);
+s1.bind(8082);
+s1.listen();
+accept1.start(s1);
+
+for ( var i = 3; i < 100000; i *= 3) {
+	var conn = new net.Socket();
+	conn.connect('127.0.0.1', 8082);
+	t_read(conn, i);
+}
+
+f = io.open("test0000");
+var r = new io.BufferedStream(f);
+r.EOL = '\r\n';
+
+var n = 0;
+
+while ((s = r.readLine()) !== undefined){
+	assert.equal('0123456789', s);
+	n ++;
+}
+assert.equal(1024, n);
+	
 f.close();
 os.unlink("test0000");
--- bin/main.js
+++ bin/main.js
@@ -6,14 +6,15 @@ require("trigger_test");
 require("lock_test");
 require("io_test");
 require("ms_test");
-require("buffered_test");
 require("os_test");
 require("encoding_test");
 require("module_test");
 require("define_test");
 require("net_test");
+require("buffered_test");
 require("url_test");
-require("resolve_test");
+require("hash_test");
+//require("resolve_test");
 require("zmq_test");
 require("uuid_test");
 
