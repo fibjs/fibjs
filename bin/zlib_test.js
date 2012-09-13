29851d86d7b213a9bdd68efcedae0c7da2e4d537
xicilion <lion@9465.net>
2012-09-13 19:24:57 +0000

    zlib 测试用例。

--- bin/zlib_test.js
+++ bin/zlib_test.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var zlib = require('zlib');
 var io = require('io');
 
-var M = 1024000;
+var M = 102400;
 var b = new Buffer(M);
 
 for(i = 0; i < M; i ++)
@@ -12,3 +12,56 @@ for(i = 0; i < M; i ++)
 
 assert.equal(zlib.inflate(zlib.deflate(b)).toString(), b.toString());
 assert.equal(zlib.gunzip(zlib.gzip(b)).toString(), b.toString());
+
+var stm = new io.MemoryStream();
+zlib.deflateTo(b, stm);
+stm.rewind();
+assert.equal(zlib.inflate(stm.read()).toString(), b.toString());
+
+var stm = new io.MemoryStream();
+zlib.inflateTo(zlib.deflate(b), stm);
+stm.rewind();
+assert.equal(stm.read().toString(), b.toString());
+
+var stm = new io.MemoryStream();
+zlib.gzipTo(b, stm);
+stm.rewind();
+assert.equal(zlib.gunzip(stm.read()).toString(), b.toString());
+
+var stm = new io.MemoryStream();
+zlib.gunzipTo(zlib.gzip(b), stm);
+stm.rewind();
+assert.equal(stm.read().toString(), b.toString());
+
+var stm = new io.MemoryStream();
+stm.write(b);
+
+stm.rewind()
+var stm1 = new io.MemoryStream();
+zlib.deflateTo(stm, stm1);
+stm1.rewind();
+assert.equal(zlib.inflate(stm1.read()).toString(), b.toString());
+
+stm.rewind()
+var stm1 = new io.MemoryStream();
+zlib.gzipTo(stm, stm1);
+stm1.rewind();
+assert.equal(zlib.gunzip(stm1.read()).toString(), b.toString());
+
+var stm = new io.MemoryStream();
+stm.write(zlib.deflate(b));
+stm.rewind()
+var stm1 = new io.MemoryStream();
+zlib.inflateTo(stm, stm1);
+stm1.rewind();
+assert.equal(stm1.read().toString(), b.toString());
+
+
+var stm = new io.MemoryStream();
+stm.write(zlib.gzip(b));
+stm.rewind()
+var stm1 = new io.MemoryStream();
+zlib.gunzipTo(stm, stm1);
+stm1.rewind();
+assert.equal(stm1.read().toString(), b.toString());
+
