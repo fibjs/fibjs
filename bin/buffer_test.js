e5bf8a2fadf30ec97c264e1c1313ac32efe3f59d
xicilion <lion@9465.net>
2012-09-14 03:39:34 +0000

    增加字符串数字类型测试用例。

--- bin/buffer_test.js
+++ bin/buffer_test.js
@@ -12,3 +12,19 @@ assert.equal(buf.length, 100);
 var buf = new Buffer("abcd");
 assert.equal(buf.length, 4);
 
+var buf = new Buffer();
+assert.doesNotThrow(function() {
+	buf.resize("12")
+});
+
+assert.throws(function() {
+	buf.resize("a12")
+});
+
+assert.throws(function() {
+	buf.resize("12a")
+});
+
+assert.throws(function() {
+	buf.resize("12.a")
+});
