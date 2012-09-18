4edbe675d71cea53bc6161128fe4b5ac3e276465
xicilion <lion@9465.net>
2012-09-18 08:10:49 +0000

    os.release 更名为 os.version。

--- bin/net_test.js
+++ bin/net_test.js
@@ -15,7 +15,7 @@ var net_config = {
 	address : '::1'
 };
 
-if (os.type == 'Windows' && os.release < "6.0")
+if (os.type == 'Windows' && os.version < "6.0")
 	net_config = {
 		family : net.AF_INET,
 		address : '127.0.0.1'
