299706ee49e3cb7e65c3a002239a0e4dbc8dce8c
xicilion <lion@9465.net>
2012-11-11 08:23:37 +0000

    os_freebsd port.

--- fibjs/src/os_freebsd.cpp
+++ fibjs/src/os_freebsd.cpp
@@ -10,9 +10,30 @@
 #ifdef FreeBSD
 
 #include "ifs/os.h"
+#include <kvm.h>
+#include <fcntl.h>
+#include <paths.h>
+#include <sys/user.h>
+#include <sys/types.h>
 #include <sys/resource.h>
 #include <sys/sysctl.h>
 #include <vm/vm_param.h>
+
+#undef NANOSEC
+#define NANOSEC ((uint64_t) 1e9)
+
+#ifndef CPUSTATES
+# define CPUSTATES 5U
+#endif
+
+#ifndef CP_USER
+# define CP_USER 0
+# define CP_NICE 1
+# define CP_SYS 2
+# define CP_IDLE 3
+# define CP_INTR 4
+#endif
+
 
 namespace fibjs {
 
@@ -178,6 +199,77 @@ result_t os_base::CPUInfo(v8::Handle<v8::Array>& retVal) {
 	}
 
 	free(cp_times);
+
+	return 0;
+}
+
+result_t os_base::get_execPath(std::string& retVal)
+{
+	char exeName[1024] = "";
+	size_t size = sizeof(exeName);
+
+	int mib[4];
+
+#ifdef __DragonFly__
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_ARGS;
+	mib[3] = getpid();
+#else
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_PATHNAME;
+	mib[3] = -1;
+#endif
+
+	if (sysctl(mib, 4, exeName, &size, NULL, 0) < 0)
+		return LastError();
+
+	retVal = exeName;
+	return 0;
+}
+
+result_t os_base::memoryUsage(v8::Handle<v8::Object>& retVal)
+{
+	size_t rss;
+
+	kvm_t *kd = NULL;
+	struct kinfo_proc *kinfo = NULL;
+	pid_t pid;
+	int nprocs;
+	size_t page_size = getpagesize();
+
+	pid = getpid();
+
+	kd = kvm_open(NULL, _PATH_DEVNULL, NULL, O_RDONLY, "kvm_open");
+	if (kd == NULL)
+		return LastError();
+
+	kinfo = kvm_getprocs(kd, KERN_PROC_PID, pid, &nprocs);
+	if (kinfo == NULL)
+	{
+		kvm_close(kd);
+		return LastError();
+	}
+
+#ifdef __DragonFly__
+	rss = kinfo->kp_vm_rssize * page_size;
+#else
+	rss = kinfo->ki_rssize * page_size;
+#endif
+	kvm_close(kd);
+
+	v8::Handle<v8::Object> info = v8::Object::New();
+
+	v8::HeapStatistics v8_heap_stats;
+	v8::V8::GetHeapStatistics(&v8_heap_stats);
+	info->Set(v8::String::New("rss"), v8::Integer::New((int32_t)rss));
+	info->Set(v8::String::New("heapTotal"),
+			v8::Integer::New((int32_t)v8_heap_stats.total_heap_size()));
+	info->Set(v8::String::New("heapUsed"),
+			v8::Integer::New((int32_t)v8_heap_stats.used_heap_size()));
+
+	retVal = info;
 
 	return 0;
 }
