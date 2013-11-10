9483d233b8e0e50621430eb5fff89d863d150041
xicilion <lion@9465.net>
2013-11-10 12:03:22 +0000

    修改 list 使用 test 测试。

--- bin/list_test.js
+++ bin/list_test.js
@@ -1,51 +1,82 @@
-console.log('list testing....');
+var test = require("test");
+test.setup();
 
 var collection = require('collection');
-var assert = require('assert');
 
 var a = new collection.List();
-
-assert.equal(a.pop(), null);
-
-a.push(1);
-assert.deepEqual(a.toArray(), [ 1 ]);
-
-a.push(2, 3, 4);
-assert.deepEqual(a.toArray(), [ 1, 2, 3, 4 ]);
-
-assert.equal(a.pop(), 4);
-assert.equal(a.length, 3);
-
-assert.deepEqual(a.slice(1).toArray(), [ 2, 3 ]);
-assert.deepEqual(a.slice(1, 2).toArray(), [ 2 ]);
-assert.deepEqual(a.slice(1, 0).toArray(), []);
-
-assert.deepEqual(a.concat(a).toArray(), [ 1, 2, 3, 1, 2, 3 ]);
-assert.deepEqual(a.concat(new collection.List()).toArray(), [ 1, 2, 3 ]);
-assert.deepEqual(a.concat([ 4, 5, 6 ]).toArray(), [ 1, 2, 3, 4, 5, 6 ]);
-assert.deepEqual(a.concat(a, [ 4, 5, 6 ], a).toArray(), [ 1, 2, 3, 1, 2, 3, 4, 5,
-		6, 1, 2, 3 ]);
 
 function isBigEnough(element, index) {
 	return (element >= 10);
 }
 
-var passed = new collection.List();
-passed.push(12, 5, 8, 130, 44);
+describe('list', function() {
+	it("pop empty list", function() {
+		assert.equal(a.pop(), null);
+	});
 
-assert.equal(passed.every(isBigEnough), false);
+	it("toArray", function() {
+		a.push(1);
+		assert.deepEqual(a.toArray(), [ 1 ]);
+	});
 
-assert.deepEqual(passed.filter(isBigEnough).toArray(), [ 12, 130, 44 ]);
+	it("push", function() {
+		a.push(2, 3, 4);
+		assert.deepEqual(a.toArray(), [ 1, 2, 3, 4 ]);
+	});
 
-var passed = new collection.List();
-passed.push(12, 54, 18, 130, 44);
+	it("pop", function() {
+		assert.equal(a.pop(), 4);
+		assert.equal(a.length, 3);
+	});
 
-assert.equal(passed.every(isBigEnough), true);
+	it("slice", function() {
+		assert.deepEqual(a.slice(1).toArray(), [ 2, 3 ]);
+		assert.deepEqual(a.slice(1, 2).toArray(), [ 2 ]);
+		assert.deepEqual(a.slice(1, 0).toArray(), []);
+	});
 
-var passed = new collection.List();
-passed.push(1, 4, 9);
-assert.deepEqual(passed.map(Math.sqrt).toArray(), [ 1, 2, 3 ]);
+	it("concat",
+			function() {
+				assert.deepEqual(a.concat(a).toArray(), [ 1, 2, 3, 1, 2, 3 ]);
+				assert.deepEqual(a.concat(new collection.List()).toArray(), [
+						1, 2, 3 ]);
+				assert.deepEqual(a.concat([ 4, 5, 6 ]).toArray(), [ 1, 2, 3, 4,
+						5, 6 ]);
+				assert.deepEqual(a.concat(a, [ 4, 5, 6 ], a).toArray(), [ 1, 2,
+						3, 1, 2, 3, 4, 5, 6, 1, 2, 3 ]);
+			});
 
-passed.forEach(function(v, i) {
-	assert.equal(passed[i], v);
+	it("every", function() {
+		var passed = new collection.List();
+		passed.push(12, 5, 8, 130, 44);
+		assert.equal(passed.every(isBigEnough), false);
+
+		passed = new collection.List();
+		passed.push(12, 54, 18, 130, 44);
+		assert.equal(passed.every(isBigEnough), true);
+	});
+
+	it("filter",
+			function() {
+				var passed = new collection.List();
+				passed.push(12, 5, 8, 130, 44);
+				assert.deepEqual(passed.filter(isBigEnough).toArray(), [ 12,
+						130, 44 ]);
+			});
+
+	it("map", function() {
+		var passed = new collection.List();
+		passed.push(1, 4, 9);
+		assert.deepEqual(passed.map(Math.sqrt).toArray(), [ 1, 2, 3 ]);
+	});
+
+	it("forEach", function() {
+		var passed = new collection.List();
+		passed.push(1, 4, 9);
+		passed.forEach(function(v, i) {
+			assert.equal(passed[i], v);
+		});
+	});
 });
+
+test.run();
