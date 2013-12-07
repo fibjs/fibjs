2e276fb6fab3dd90f1f0a6e84a9a7a03d7bb96c1
xicilion <lion@9465.net>
2013-12-07 13:11:45 +0000

    LruCache 测试用例

--- bin/utils_test.js
+++ bin/utils_test.js
@@ -81,6 +81,110 @@ describe('utils', function() {
 			assert.closeTo(100, s.uptime(), 10);
 		});
 	});
+
+	describe('LruCache', function() {
+		var c;
+
+		function deepEqual(o1, o2) {
+			assert.deepEqual(Object.keys(o1), Object.keys(o2));
+			assert.deepEqual(o1, o2);
+		}
+
+		it("new", function() {
+			c = new utils.LruCache(3, 100);
+			deepEqual(c.toJSON(), {});
+		});
+
+		it("put", function() {
+			c.put("b", 100);
+			deepEqual(c.toJSON(), {
+				"b" : 100
+			});
+
+			c.put({
+				"b" : 200,
+				"a" : 100
+			});
+			deepEqual(c.toJSON(), {
+				"a" : 100,
+				"b" : 200
+			});
+		});
+
+		it("set", function() {
+			c.set("c", 100);
+			deepEqual(c.toJSON(), {
+				"a" : 100,
+				"b" : 200
+			});
+		});
+
+		it("has", function() {
+			assert.isTrue(c.has('a'));
+			assert.isFalse(c.has('c'));
+		});
+
+		it("get", function() {
+			assert.equal(c.get('a'), 100);
+			assert.isUndefined(c.get('c'));
+
+			deepEqual(c.toJSON(), {
+				"a" : 100,
+				"b" : 200
+			});
+
+			assert.equal(c.get('b'), 200);
+			deepEqual(c.toJSON(), {
+				"b" : 200,
+				"a" : 100
+			});
+		});
+
+		it("remove", function() {
+			c.remove("b");
+			deepEqual(c.toJSON(), {
+				"a" : 100
+			});
+
+			c.remove('a');
+			deepEqual(c.toJSON(), {});
+		});
+
+		it("size", function() {
+			assert.equal(c.size, 0);
+
+			c.put('a', 100);
+			assert.equal(c.size, 1);
+
+			c.put('b', 100);
+			c.put('c', 200);
+			c.put('d', 300);
+			c.put('e', 400);
+
+			assert.equal(c.size, 3);
+
+			deepEqual(c.toJSON(), {
+				"e" : 400,
+				"d" : 300,
+				"c" : 200
+			});
+		});
+
+		it("timeout", function() {
+			coroutine.sleep(50);
+			c.put('f', 500);
+			deepEqual(c.toJSON(), {
+				"f" : 500,
+				"e" : 400,
+				"d" : 300
+			});
+
+			coroutine.sleep(51);
+			deepEqual(c.toJSON(), {
+				"f" : 500
+			});
+		});
+	});
 });
 
-//test.run();
+// test.run();
