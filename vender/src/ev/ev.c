bb637a8f857546368c7032ae856da24b34fa35c8
xicilion <lion@9465.net>
2013-12-13 13:15:07 +0000

    打开 osx 的 KQueue 引擎。

--- vender/src/ev/ev.c
+++ vender/src/ev/ev.c
@@ -2445,12 +2445,12 @@ ev_recommended_backends (void) EV_THROW
 #ifndef __NetBSD__
   /* kqueue is borked on everything but netbsd apparently */
   /* it usually doesn't work correctly on anything but sockets and pipes */
-  flags &= ~EVBACKEND_KQUEUE;
+//  flags &= ~EVBACKEND_KQUEUE;
 #endif
 #ifdef __APPLE__
   /* only select works correctly on that "unix-certified" platform */
-  flags &= ~EVBACKEND_KQUEUE; /* horribly broken, even for sockets */
-  flags &= ~EVBACKEND_POLL;   /* poll is based on kqueue from 10.5 onwards */
+//  flags &= ~EVBACKEND_KQUEUE; /* horribly broken, even for sockets */
+//  flags &= ~EVBACKEND_POLL;   /* poll is based on kqueue from 10.5 onwards */
 #endif
 #ifdef __FreeBSD__
   flags &= ~EVBACKEND_POLL;   /* poll return value is unusable (http://forums.freebsd.org/archive/index.php/t-10270.html) */
