9519ed9578649bc682bbdf07ac3c2692b3e11e62
xicilion <lion@9465.net>
2014-01-06 08:22:27 +0000

    实现部分 arm 汇编逻辑

--- vender/include/exlib/utils.h
+++ vender/include/exlib/utils.h
@@ -16,94 +16,156 @@ namespace exlib
 
 #ifdef WIN32
 
-void* _CompareAndSwap(
-		void* volatile *Destination,
-		void* Exchange,
-		void* Comparand
+void *_CompareAndSwap(
+    void *volatile *Destination,
+    void *Exchange,
+    void *Comparand
 );
 
 template<typename T>
-inline T* CompareAndSwap(T** ptr, T* old_value, T* new_value)
+inline T *CompareAndSwap(T **ptr, T *old_value, T *new_value)
 {
-	void* result = _CompareAndSwap(
-			reinterpret_cast<void**>(ptr),
-			reinterpret_cast<void*>(new_value), reinterpret_cast<void*>(old_value));
-	return reinterpret_cast<T*>(result);
+    void *result = _CompareAndSwap(
+                       reinterpret_cast<void **>(ptr),
+                       reinterpret_cast<void *>(new_value), reinterpret_cast<void *>(old_value));
+    return reinterpret_cast<T *>(result);
 }
 
 int atom_add(int *dest, int incr);
 int atom_inc(int *dest);
 int atom_dec(int *dest);
-int atom_xchg(int* ptr, int new_value);
+int atom_xchg(int *ptr, int new_value);
 
 #else
 
-#ifdef x64
+#if defined(arm)
 
 template<typename T>
-inline T* CompareAndSwap(T** ptr, T* old_value, T* new_value)
+inline T *CompareAndSwap(T **ptr, T *old_value, T *new_value)
 {
-	T* prev;
-	__asm__ __volatile__("lock; cmpxchgq %1,%2"
-			: "=a" (prev)
-			: "q" (new_value), "m" (*ptr), "0" (old_value)
-			: "memory");
-	return prev;
+    T *oldval, *res;
+
+    do
+    {
+        __asm volatile("@ __cmpxchg4\n"
+                       "ldrex %1, [%2]\n"
+                       "mov %0, #0\n"
+                       "teq %1, %3\n"
+                       "strexeq %0, %4, [%2]\n"
+                       : "=&r" (res), "=&r" (oldval)
+                       : "r" (ptr), "Ir" (old_value), "r" (new_value)
+                       : "memory", "cc");
+    }
+    while (res);
+
+    return oldval;
+}
+
+inline int atom_add(__volatile__ int *dest, int incr)
+{
+    unsigned long tmp;
+    int result;
+
+    __asm__ __volatile__("@ atomic_add_return/n"
+                         "1:ldrex %0, [%2]/n"
+                         "add %0, %0, %3/n"
+                         "strex %1, %0, [%2]/n"
+                         "teq %1, #0/n"
+                         "bne 1b"
+                         : "=&r" (result), "=&r" (tmp)
+                         : "r" (dest), "Ir" (incr)
+                         : "cc");
+
+    return result;
+}
+
+inline int atom_xchg(int *ptr, int new_value)
+{
+    int prev;
+    int tmp;
+
+    __asm __volatile("@ __xchg4\n"
+                     "1:ldrex %0, [%3]\n"
+                     "strex %1, %2, [%3]\n"
+                     "teq %1, #0\n"
+                     "bne 1b"
+                     : "=&r" (prev), "=&r" (tmp)
+                     : "r" (new_value), "r" (ptr)
+                     : "memory", "cc");
+
+    return prev;
+}
+
+#else
+
+#if defined(x64)
+
+template<typename T>
+inline T *CompareAndSwap(T **ptr, T *old_value, T *new_value)
+{
+    T *prev;
+    __asm__ __volatile__("lock; cmpxchgq %1,%2"
+                         : "=a" (prev)
+                         : "q" (new_value), "m" (*ptr), "0" (old_value)
+                         : "memory");
+    return prev;
 }
 
 #else
 
 template<typename T>
-inline T* CompareAndSwap(T** ptr, T* old_value, T* new_value)
+inline T *CompareAndSwap(T **ptr, T *old_value, T *new_value)
 {
-	T* prev;
-	__asm__ __volatile__("lock; cmpxchgl %1,%2"
-			: "=a" (prev)
-			: "q" (new_value), "m" (*ptr), "0" (old_value)
-			: "memory");
-	return prev;
+    T *prev;
+    __asm__ __volatile__("lock; cmpxchgl %1,%2"
+                         : "=a" (prev)
+                         : "q" (new_value), "m" (*ptr), "0" (old_value)
+                         : "memory");
+    return prev;
 }
 
 #endif
 
 inline int atom_add(__volatile__ int *dest, int incr)
 {
-	int ret;
-	__asm__ __volatile__("lock; xaddl %0,(%1)"
-			: "=r"(ret)
-			: "r"(dest), "0"(incr)
-			: "memory");
-	return ret + incr;
+    int ret;
+    __asm__ __volatile__("lock; xaddl %0,(%1)"
+                         : "=r"(ret)
+                         : "r"(dest), "0"(incr)
+                         : "memory");
+    return ret + incr;
 }
 
-inline int atom_inc(__volatile__ int *dest)
+inline int atom_xchg(int *ptr, int new_value)
 {
-	return atom_add(dest, 1);
-}
+    int prev;
 
-inline int atom_dec(__volatile__ int *dest)
-{
-	return atom_add(dest, -1);
-}
+    __asm __volatile(
+        "lock xchgl %2,(%1)"
+        : "=r" (prev)
+        : "r" (ptr), "0" (new_value)
+        : "memory");
 
-inline int atom_xchg(int* ptr, int new_value)
-{
-	int prev;
-
-	__asm __volatile(
-			"lock xchgl %2,(%1)"
-			: "=r" (prev)
-			: "r" (ptr), "0" (new_value)
-			: "memory");
-
-	return prev;
+    return prev;
 }
 
 #endif
 
-inline void* CompareAndSwap(void** ptr, void* old_value, void* new_value)
+inline int atom_inc(__volatile__ int *dest)
 {
-	return CompareAndSwap((char**) ptr, (char*) old_value, (char*) new_value);
+    return atom_add(dest, 1);
+}
+
+inline int atom_dec(__volatile__ int *dest)
+{
+    return atom_add(dest, -1);
+}
+
+#endif
+
+inline void *CompareAndSwap(void **ptr, void *old_value, void *new_value)
+{
+    return CompareAndSwap((char **) ptr, (char *) old_value, (char *) new_value);
 }
 
 }
--- vender/src/exlib/fiber/fbSwitch.cpp
+++ vender/src/exlib/fiber/fbSwitch.cpp
@@ -6,144 +6,142 @@ extern "C" int win_switch();
 
 int __declspec(naked) win_switch()
 {
-	__asm
-	{
-		mov eax, [esp + 4]
-		mov [eax], ebp
-		mov [eax + 004h], ebx
-		mov [eax + 008h], ecx
-		mov [eax + 00ch], edx
-		mov [eax + 010h], esi
-		mov [eax + 014h], edi
+    __asm
+    {
+        mov eax, [esp + 4]
+        mov [eax], ebp
+        mov [eax + 004h], ebx
+        mov [eax + 008h], ecx
+        mov [eax + 00ch], edx
+        mov [eax + 010h], esi
+        mov [eax + 014h], edi
 
-		lea edx, [esp + 4]
-		mov [eax + 018h], edx
-		mov edx, [esp]
-		mov [eax + 01ch], edx
+        lea edx, [esp + 4]
+        mov [eax + 018h], edx
+        mov edx, [esp]
+        mov [eax + 01ch], edx
 
-		mov eax, [esp + 08h]
-		mov ebp, [eax]
-		mov ebx, [eax + 004h]
-		mov ecx, [eax + 008h]
-		mov edx, [eax + 00ch]
-		mov esi, [eax + 010h]
-		mov edi, [eax + 014h]
-		mov esp, [eax + 018h]
-		mov eax, [eax + 01ch]
-		push eax
-		xor eax, eax
-		ret
-	}
+        mov eax, [esp + 08h]
+        mov ebp, [eax]
+        mov ebx, [eax + 004h]
+        mov ecx, [eax + 008h]
+        mov edx, [eax + 00ch]
+        mov esi, [eax + 010h]
+        mov edi, [eax + 014h]
+        mov esp, [eax + 018h]
+        mov eax, [eax + 01ch]
+        push eax
+        xor eax, eax
+        ret
+    }
 }
 #endif
 
 #ifndef _WIN32
 
 #ifdef MacOS
-asm(".section	__TEXT,__text");
+asm(".section   __TEXT,__text");
 #else
-asm(".section	.text");
+asm(".section   .text");
 #endif
 
-#ifdef x64
-
 asm(".globl nix_switch, _nix_switch");
 asm("nix_switch:");
 asm("_nix_switch:");
 
-asm("	movq	%rbp,(%rdi)");
+#if defined(x64)
 
-asm("	movq	%rbx,0x08(%rdi)");
-asm("   movq	%rcx,0x10(%rdi)");
-asm("   movq	%rdx,0x18(%rdi)");
-asm("	movq	%rsi,0x20(%rdi)");
-asm("	movq	%rdi,0x28(%rdi)");
-asm("	movq	%r12,0x30(%rdi)");
-asm("	movq	%r13,0x38(%rdi)");
-asm("	movq	%r14,0x40(%rdi)");
-asm("	movq	%r15,0x48(%rdi)");
+asm("    movq    %rbp,(%rdi)");
 
-asm("	leaq	8(%rsp),%rax");
-asm("	movq	%rax,0x50(%rdi)");
+asm("    movq    %rbx,0x08(%rdi)");
+asm("    movq    %rcx,0x10(%rdi)");
+asm("    movq    %rdx,0x18(%rdi)");
+asm("    movq    %rsi,0x20(%rdi)");
+asm("    movq    %rdi,0x28(%rdi)");
+asm("    movq    %r12,0x30(%rdi)");
+asm("    movq    %r13,0x38(%rdi)");
+asm("    movq    %r14,0x40(%rdi)");
+asm("    movq    %r15,0x48(%rdi)");
 
-asm("	movq	(%rsp),%rax");
-asm("	movq	%rax,0x58(%rdi)");
+asm("    leaq    8(%rsp),%rax");
+asm("    movq    %rax,0x50(%rdi)");
 
-asm("	movdqa	%xmm6, 0x60(%rdi)");
-asm("	movdqa	%xmm7, 0x70(%rdi)");
-asm("	movdqa	%xmm8, 0x80(%rdi)");
-asm("	movdqa	%xmm9, 0x90(%rdi)");
-asm("	movdqa	%xmm10, 0xa0(%rdi)");
-asm("	movdqa	%xmm11, 0xb0(%rdi)");
-asm("	movdqa	%xmm12, 0xc0(%rdi)");
-asm("	movdqa	%xmm13, 0xd0(%rdi)");
-asm("	movdqa	%xmm14, 0xe0(%rdi)");
-asm("	movdqa	%xmm15, 0xf0(%rdi)");
+asm("    movq    (%rsp),%rax");
+asm("    movq    %rax,0x58(%rdi)");
 
-asm("   movq	%rsi, %rdi");
+asm("    movdqa  %xmm6, 0x60(%rdi)");
+asm("    movdqa  %xmm7, 0x70(%rdi)");
+asm("    movdqa  %xmm8, 0x80(%rdi)");
+asm("    movdqa  %xmm9, 0x90(%rdi)");
+asm("    movdqa  %xmm10, 0xa0(%rdi)");
+asm("    movdqa  %xmm11, 0xb0(%rdi)");
+asm("    movdqa  %xmm12, 0xc0(%rdi)");
+asm("    movdqa  %xmm13, 0xd0(%rdi)");
+asm("    movdqa  %xmm14, 0xe0(%rdi)");
+asm("    movdqa  %xmm15, 0xf0(%rdi)");
 
-asm("	movq	(%rdi), %rbp");
-asm("	movq	0x08(%rdi), %rbx");
-asm("	movq	0x10(%rdi), %rcx");
-asm("	movq	0x18(%rdi), %rdx");
-asm("	movq	0x20(%rdi), %rsi");
+asm("    movq    %rsi, %rdi");
 
-asm("	movq	0x30(%rdi), %r12");
-asm("	movq	0x38(%rdi), %r13");
-asm("	movq	0x40(%rdi), %r14");
-asm("	movq	0x48(%rdi), %r15");
-asm("	movq	0x50(%rdi), %rsp");
+asm("    movq    (%rdi), %rbp");
+asm("    movq    0x08(%rdi), %rbx");
+asm("    movq    0x10(%rdi), %rcx");
+asm("    movq    0x18(%rdi), %rdx");
+asm("    movq    0x20(%rdi), %rsi");
 
-asm("	movq	0x58(%rdi), %rax");
+asm("    movq    0x30(%rdi), %r12");
+asm("    movq    0x38(%rdi), %r13");
+asm("    movq    0x40(%rdi), %r14");
+asm("    movq    0x48(%rdi), %r15");
+asm("    movq    0x50(%rdi), %rsp");
 
-asm("	movdqa	0x60(%rdi), %xmm6");
-asm("	movdqa	0x70(%rdi), %xmm7");
-asm("	movdqa	0x80(%rdi), %xmm8");
-asm("	movdqa	0x90(%rdi), %xmm9");
-asm("	movdqa	0xa0(%rdi), %xmm10");
-asm("	movdqa	0xb0(%rdi), %xmm11");
-asm("	movdqa	0xc0(%rdi), %xmm12");
-asm("	movdqa	0xd0(%rdi), %xmm13");
-asm("	movdqa	0xe0(%rdi), %xmm14");
-asm("	movdqa	0xf0(%rdi), %xmm15");
+asm("    movq    0x58(%rdi), %rax");
 
-asm("	movq	0x28(%rdi), %rdi");
+asm("    movdqa  0x60(%rdi), %xmm6");
+asm("    movdqa  0x70(%rdi), %xmm7");
+asm("    movdqa  0x80(%rdi), %xmm8");
+asm("    movdqa  0x90(%rdi), %xmm9");
+asm("    movdqa  0xa0(%rdi), %xmm10");
+asm("    movdqa  0xb0(%rdi), %xmm11");
+asm("    movdqa  0xc0(%rdi), %xmm12");
+asm("    movdqa  0xd0(%rdi), %xmm13");
+asm("    movdqa  0xe0(%rdi), %xmm14");
+asm("    movdqa  0xf0(%rdi), %xmm15");
 
-asm("	pushq   %rax");
-asm("	xorq	%rax,%rax");
-asm("	ret");
+asm("    movq    0x28(%rdi), %rdi");
 
-#else
+asm("    pushq   %rax");
+asm("    xorq    %rax,%rax");
+asm("    ret");
 
-asm(".globl nix_switch, _nix_switch");
-asm("nix_switch:");
-asm("_nix_switch:");
+#elif defined(I386)
 
-asm("	movl	0x4(%esp), %eax");
-asm("	movl	%ebp, (%eax)");
-asm("	movl	%ebx, 0x04(%eax)");
-asm("	movl	%ecx, 0x08(%eax)");
-asm("	movl	%edx, 0x0c(%eax)");
-asm("	movl	%esi, 0x10(%eax)");
-asm("	movl	%edi, 0x14(%eax)");
+asm("    movl    0x4(%esp), %eax");
+asm("    movl    %ebp, (%eax)");
+asm("    movl    %ebx, 0x04(%eax)");
+asm("    movl    %ecx, 0x08(%eax)");
+asm("    movl    %edx, 0x0c(%eax)");
+asm("    movl    %esi, 0x10(%eax)");
+asm("    movl    %edi, 0x14(%eax)");
 
-asm("	leal	4(%esp), %edx");
-asm("	movl	%edx, 0x18(%eax)");
-asm("	movl	(%esp), %edx");
-asm("	movl	%edx, 0x1c(%eax)");
+asm("    leal    4(%esp), %edx");
+asm("    movl    %edx, 0x18(%eax)");
+asm("    movl    (%esp), %edx");
+asm("    movl    %edx, 0x1c(%eax)");
 
-asm("	movl	0x8(%esp), %eax");
-asm("	movl	(%eax), %ebp");
-asm("	movl	0x04(%eax), %ebx");
-asm("	movl	0x08(%eax), %ecx");
-asm("	movl	0x0c(%eax), %edx");
-asm("	movl	0x10(%eax), %esi");
-asm("	movl	0x14(%eax), %edi");
-asm("	movl	0x18(%eax), %esp");
-asm("	movl	0x1c(%eax), %eax");
-asm("   pushl   %eax");
-asm("	xorl	%eax, %eax");
-asm("	ret");
+asm("    movl    0x8(%esp), %eax");
+asm("    movl    (%eax), %ebp");
+asm("    movl    0x04(%eax), %ebx");
+asm("    movl    0x08(%eax), %ecx");
+asm("    movl    0x0c(%eax), %edx");
+asm("    movl    0x10(%eax), %esi");
+asm("    movl    0x14(%eax), %edi");
+asm("    movl    0x18(%eax), %esp");
+asm("    movl    0x1c(%eax), %eax");
+asm("    pushl   %eax");
+asm("    xorl    %eax, %eax");
+asm("    ret");
+
+#elif defined(arm)
 
 #endif
 
