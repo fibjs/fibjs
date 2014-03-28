53390570d93f20ebc9b245f9ac8d4e7317fb0641
xicilion <lion@9465.net>
2014-03-28 08:10:28 +0000

    增加 addScript 测试用例

--- bin/vm_test.js
+++ bin/vm_test.js
@@ -4,12 +4,14 @@ test.setup();
 var vm = require('vm');
 
 describe("vm", function() {
-	it("sandbox", function() {
+	var sbox;
+
+	it("sandbox.add", function() {
 		var b = {
 			a: 1000
 		}
 
-		var sbox = vm.create({
+		sbox = vm.create({
 			a: 100,
 			assert: assert,
 			b: b
@@ -23,6 +25,16 @@ describe("vm", function() {
 
 		sbox.require('./vm_test/t1').fun();
 		assert.equal(1000, b.a);
+	});
+
+	it("sandbox.addScript", function() {
+		var a = sbox.addScript("t1.js", "module.exports = {a : 100};");
+		assert.equal(100, a.a);
+		assert.equal(100, sbox.require("t1").a);
+
+		var b = sbox.addScript("t2.js", "module.exports = {a : require('t1').a};");
+		assert.equal(100, b.a);
+		assert.equal(100, sbox.require("t2").a);
 	});
 });
 
