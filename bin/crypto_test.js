612c6957d2f875f6723efbc04c2fde7b0993381a
xicilion <lion@9465.net>
2014-04-10 14:02:49 +0000

    crypto 测试用例

---
+++ bin/crypto_test.js
@@ -0,0 +1,55 @@
+var test = require("test");
+test.setup();
+
+var crypto = require("crypto");
+var fs = require("fs");
+var encoding = require("encoding");
+
+describe('crypto', function() {
+	it("random", function() {
+		assert.notEqual(crypto.randomBytes(8).hex(), crypto.randomBytes(8).hex());
+	});
+
+	it("pseudoRandomBytes", function() {
+		assert.notEqual(crypto.pseudoRandomBytes(8).hex(), crypto.pseudoRandomBytes(8).hex());
+	});
+
+	describe('Cipher', function() {
+		function test_cipher(provider, file) {
+			it(file, function() {
+				var cases = encoding.jsonDecode(fs.readFile("crypto_case/" + file + ".json"));
+
+				cases.forEach(function(item) {
+					var c;
+					var mode = crypto[item[2]];
+
+					if (item.length == 7)
+						c = new crypto.Cipher(provider, mode,
+							encoding.hexDecode(item[3]), encoding.hexDecode(item[6]));
+					else if (item.length == 6)
+						c = new crypto.Cipher(provider, mode,
+							encoding.hexDecode(item[3]));
+
+					if (mode == crypto.CBC)
+						c.paddingMode(crypto.PADDING_NONE);
+					assert.equal(c[item[1]](encoding.hexDecode(item[4])).hex(), item[5], item[0]);
+				});
+
+			});
+		}
+
+		test_cipher(crypto.CAMELLIA, "camellia");
+		test_cipher(crypto.DES, "des");
+		test_cipher(crypto.DES_EDE3, "des_ede3");
+		test_cipher(crypto.BLOWFISH, "blowfish");
+		test_cipher(crypto.ARC4, "arc4");
+
+		describe('AES', function() {
+			test_cipher(crypto.AES, "aes.cbc");
+			test_cipher(crypto.AES, "aes.cfb");
+			test_cipher(crypto.AES, "aes.ecb");
+		});
+	});
+});
+
+//test.run(console.DEBUG);
