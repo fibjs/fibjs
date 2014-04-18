c976a140ceecddc7e8c553487826684d34b2e715
xicilion <lion@9465.net>
2014-04-18 16:22:47 +0000

    自动升级 v8 后重新生成 patch 文件，避免长时间后误差过大

--- vender/src/v8/update.sh
+++ vender/src/v8/update.sh
@@ -56,3 +56,22 @@ rm src/v8dll-main.cc
 rm src/platform-cygwin.cc
 
 patch -p0 --no-backup-if-mismatch < v8.patch
+
+diff -u $V8_PATH/src/arm/deoptimizer-arm.cc src/arm/deoptimizer-arm.cc > v8.patch
+diff -u $V8_PATH/src/arm/lithium-arm.cc src/arm/lithium-arm.cc >> v8.patch
+diff -u $V8_PATH/src/arm/lithium-codegen-arm.cc src/arm/lithium-codegen-arm.cc >> v8.patch
+diff -u $V8_PATH/src/arm/lithium-gap-resolver-arm.cc src/arm/lithium-gap-resolver-arm.cc >> v8.patch
+diff -u $V8_PATH/src/arm64/deoptimizer-arm64.cc src/arm64/deoptimizer-arm64.cc >> v8.patch
+diff -u $V8_PATH/src/arm64/instrument-arm64.cc src/arm64/instrument-arm64.cc >> v8.patch
+diff -u $V8_PATH/src/arm64/lithium-arm64.cc src/arm64/lithium-arm64.cc >> v8.patch
+diff -u $V8_PATH/src/arm64/lithium-codegen-arm64.cc src/arm64/lithium-codegen-arm64.cc >> v8.patch
+diff -u $V8_PATH/src/arm64/lithium-gap-resolver-arm64.cc src/arm64/lithium-gap-resolver-arm64.cc >> v8.patch
+diff -u $V8_PATH/src/platform-freebsd.cc src/platform-freebsd.cc >> v8.patch
+diff -u $V8_PATH/src/platform-linux.cc src/platform-linux.cc >> v8.patch
+diff -u $V8_PATH/src/platform-macos.cc src/platform-macos.cc >> v8.patch
+diff -u $V8_PATH/src/platform-openbsd.cc src/platform-openbsd.cc >> v8.patch
+diff -u $V8_PATH/src/platform-posix.cc src/platform-posix.cc >> v8.patch
+diff -u $V8_PATH/src/platform-qnx.cc src/platform-qnx.cc >> v8.patch
+diff -u $V8_PATH/src/platform-solaris.cc src/platform-solaris.cc >> v8.patch
+diff -u $V8_PATH/src/platform-win32.cc src/platform-win32.cc >> v8.patch
+diff -u $V8_PATH/src/platform.h src/platform.h >> v8.patch
