0040d8e4c8a003f55a87815d7c4c596595bbdc28
xicilion <lion@9465.net>
2014-04-21 15:37:12 +0000

    格式化 net_test.js

--- bin/net_test.js
+++ bin/net_test.js
@@ -31,252 +31,244 @@ function del(f) {
 	} catch (e) {}
 }
 
-describe(
-	"net",
-	function() {
-		it("backend", function() {
-			assert.equal(net.backend(), backend);
-		});
+describe("net", function() {
+	it("backend", function() {
+		assert.equal(net.backend(), backend);
+	});
 
-		it("echo",
-			function() {
-				function connect(c) {
-					console.log(c.remoteAddress, c.remotePort, "->",
-						c.localAddress, c.localPort);
-					try {
-						var b;
+	it("echo", function() {
+		function connect(c) {
+			console.log(c.remoteAddress, c.remotePort, "->",
+				c.localAddress, c.localPort);
+			try {
+				var b;
 
-						while (b = c.recv())
-							c.send(b);
-					} finally {
-						c.close();
-						c.dispose();
-					}
-				}
-
-				function accept(s) {
-					while (1)
-						connect.start(s.accept());
-				}
-
-				var s = new net.Socket(net_config.family,
-					net.SOCK_STREAM);
-				s.bind(8080);
-				s.listen();
-				accept.start(s);
-
-				function conn_socket() {
-					var s1 = new net.Socket(net_config.family,
-						net.SOCK_STREAM);
-					s1.connect(net_config.address, 8080);
-					console.log(s1.remoteAddress, s1.remotePort, "<-",
-						s1.localAddress, s1.localPort);
-					s1.send(new Buffer("GET / HTTP/1.0"));
-					assert.equal("GET / HTTP/1.0", s1.recv());
-					s1.close();
-					s1.dispose();
-				}
-
-				function conn() {
-					var s1 = net.connect(net_config.address, 8080,
-						net_config.family);
-					console.log(s1.remoteAddress, s1.remotePort, "<-",
-						s1.localAddress, s1.localPort);
-					s1.send(new Buffer("GET / HTTP/1.0"));
-					assert.equal("GET / HTTP/1.0", s1.recv());
-					s1.close();
-					s1.dispose();
-				}
-
-				conn_socket();
-				conn();
-			});
-
-		it(
-			"copyTo",
-			function() {
-				var str = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
-
-				for (var i = 0; i < 8; i++)
-					str = str + str;
-
-				function accept1(s) {
-					while (true) {
-						var c = s.accept();
-
-						// c.write(new Buffer(str));
-
-						fs.writeFile('net_temp_000001', str);
-						var f = fs.open('net_temp_000001');
-						assert.equal(f.copyTo(c), str.length);
-						f.close();
-						c.close();
-					}
-				}
-
-				var s1 = new net.Socket(net_config.family,
-					net.SOCK_STREAM);
-				s1.bind(8081);
-				s1.listen();
-				accept1.start(s1);
-
-				function t_conn() {
-					var c1 = new net.Socket();
-					c1.connect('127.0.0.1', 8081);
-
-					var f1 = fs.open('net_temp_000002', 'w');
-					assert.equal(c1.copyTo(f1), str.length);
-					c1.close();
-					f1.close();
-
-					assert.equal(str, fs.readFile('net_temp_000002'));
-				}
-
-				for (var i = 0; i < 100; i++)
-					t_conn();
-
-				for (var i = 0; i < 10; i++)
-					str = str + str;
-
-				t_conn();
-
-				del('net_temp_000001');
-				del('net_temp_000002');
-			});
-
-		it("read & recv", function() {
-			function accept2(s) {
-				while (true) {
-					var c = s.accept();
-
-					c.write(new Buffer('a'));
-					coroutine.sleep(100);
-					c.write(new Buffer('a'));
-					coroutine.sleep(100);
-					c.write(new Buffer('b'));
-					coroutine.sleep(100);
-					c.write(new Buffer('c'));
-					coroutine.sleep(100);
-					c.write(new Buffer('d'));
-					coroutine.sleep(100);
-
-					c.close();
-				}
+				while (b = c.recv())
+					c.send(b);
+			} finally {
+				c.close();
+				c.dispose();
 			}
+		}
 
-			var s2 = new net.Socket(net_config.family, net.SOCK_STREAM);
-			s2.bind(8082);
-			s2.listen();
-			accept2.start(s2);
+		function accept(s) {
+			while (1)
+				connect.start(s.accept());
+		}
 
+		var s = new net.Socket(net_config.family,
+			net.SOCK_STREAM);
+		s.bind(8080);
+		s.listen();
+		accept.start(s);
+
+		function conn_socket() {
+			var s1 = new net.Socket(net_config.family, net.SOCK_STREAM);
+			s1.connect(net_config.address, 8080);
+			console.log(s1.remoteAddress, s1.remotePort, "<-",
+				s1.localAddress, s1.localPort);
+			s1.send(new Buffer("GET / HTTP/1.0"));
+			assert.equal("GET / HTTP/1.0", s1.recv());
+			s1.close();
+			s1.dispose();
+		}
+
+		function conn() {
+			var s1 = net.connect(net_config.address, 8080, net_config.family);
+			console.log(s1.remoteAddress, s1.remotePort, "<-",
+				s1.localAddress, s1.localPort);
+			s1.send(new Buffer("GET / HTTP/1.0"));
+			assert.equal("GET / HTTP/1.0", s1.recv());
+			s1.close();
+			s1.dispose();
+		}
+
+		conn_socket();
+		conn();
+	});
+
+	it("copyTo", function() {
+		var str = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
+
+		for (var i = 0; i < 8; i++)
+			str = str + str;
+
+		function accept1(s) {
+			while (true) {
+				var c = s.accept();
+
+				// c.write(new Buffer(str));
+
+				fs.writeFile('net_temp_000001', str);
+				var f = fs.open('net_temp_000001');
+				assert.equal(f.copyTo(c), str.length);
+				f.close();
+				c.close();
+			}
+		}
+
+		var s1 = new net.Socket(net_config.family, net.SOCK_STREAM);
+		s1.bind(8081);
+		s1.listen();
+		accept1.start(s1);
+
+		function t_conn() {
 			var c1 = new net.Socket();
-			c1.connect('127.0.0.1', 8082);
-			assert.equal('a', c1.recv(100));
-			assert.equal('abc', c1.read(3));
-			assert.equal('d', c1.read(3));
-		});
+			c1.connect('127.0.0.1', 8081);
 
-		it("bind same port", function() {
-			new net.TcpServer(8811, function(c) {});
-			assert.throws(function() {
-				new net.TcpServer(8811, function(c) {});
-			});
-		});
-
-		it("stats", function() {
-			var svr = new net.TcpServer(8812, function(c) {
-				var d;
-				while (d = c.read(100))
-					c.write(d);
-			});
-			svr.asyncRun();
-
-			assert.deepEqual({
-				"total": 0,
-				"connections": 0,
-				"accept": 0,
-				"close": 0
-			}, svr.stats.toJSON());
-
-			var c1 = new net.Socket();
-			c1.connect('127.0.0.1', 8812);
-
-			coroutine.sleep(10);
-			assert.deepEqual({
-				"total": 1,
-				"connections": 1,
-				"accept": 1,
-				"close": 0
-			}, svr.stats.toJSON());
-
-			svr.stats.reset();
-			assert.deepEqual({
-				"total": 1,
-				"connections": 1,
-				"accept": 0,
-				"close": 0
-			}, svr.stats.toJSON());
-
+			var f1 = fs.open('net_temp_000002', 'w');
+			assert.equal(c1.copyTo(f1), str.length);
 			c1.close();
+			f1.close();
 
-			svr.stats.reset();
-			coroutine.sleep(10);
-			assert.deepEqual({
-				"total": 1,
-				"connections": 0,
-				"accept": 0,
-				"close": 1
-			}, svr.stats.toJSON());
-		});
+			assert.equal(str, fs.readFile('net_temp_000002'));
+		}
 
-		describe("Smtp", function() {
-			var s;
+		for (var i = 0; i < 100; i++)
+			t_conn();
 
-			it("new & connect", function() {
-				s = new net.Smtp();
-				s.connect("smtp.ym.163.com", 25);
-				s.socket.close();
-			});
+		for (var i = 0; i < 10; i++)
+			str = str + str;
 
-			it("net.openSmtp", function() {
-				s = net.openSmtp("smtp.exmail.qq.com", 25);
-			});
+		t_conn();
 
-			it("command", function() {
-				assert.equal(s.command("HELO", "baoz.me").substr(0, 4),
-					"250 ");
+		del('net_temp_000001');
+		del('net_temp_000002');
+	});
 
-				assert.throws(function() {
-					s.command("FUCK", "baoz.me");
-				});
-			});
+	it("read & recv", function() {
+		function accept2(s) {
+			while (true) {
+				var c = s.accept();
 
-			it("hello", function() {
-				s.hello();
-			});
+				c.write(new Buffer('a'));
+				coroutine.sleep(100);
+				c.write(new Buffer('a'));
+				coroutine.sleep(100);
+				c.write(new Buffer('b'));
+				coroutine.sleep(100);
+				c.write(new Buffer('c'));
+				coroutine.sleep(100);
+				c.write(new Buffer('d'));
+				coroutine.sleep(100);
 
-			xdescribe("Auth", function() {
-				it("login", function() {
-					s.login("lion@baoz.cn", "");
-				});
+				c.close();
+			}
+		}
 
-				it("from", function() {
-					s.from("lion@baoz.cn");
-				});
+		var s2 = new net.Socket(net_config.family, net.SOCK_STREAM);
+		s2.bind(8082);
+		s2.listen();
+		accept2.start(s2);
 
-				it("to", function() {
-					s.to("lion@baoz.cn");
-				});
+		var c1 = new net.Socket();
+		c1.connect('127.0.0.1', 8082);
+		assert.equal('a', c1.recv(100));
+		assert.equal('abc', c1.read(3));
+		assert.equal('d', c1.read(3));
+	});
 
-				it("data", function() {
-					s.data("from:lion@baoz.cn\r\n" + "to:lion@baoz.cn\r\n" + "subject:test title\r\n\r\n" + "test text");
-				});
-			});
-
-			it("quit", function() {
-				s.quit();
-			});
+	it("bind same port", function() {
+		new net.TcpServer(8811, function(c) {});
+		assert.throws(function() {
+			new net.TcpServer(8811, function(c) {});
 		});
 	});
+
+	it("stats", function() {
+		var svr = new net.TcpServer(8812, function(c) {
+			var d;
+			while (d = c.read(100))
+				c.write(d);
+		});
+		svr.asyncRun();
+
+		assert.deepEqual({
+			"total": 0,
+			"connections": 0,
+			"accept": 0,
+			"close": 0
+		}, svr.stats.toJSON());
+
+		var c1 = new net.Socket();
+		c1.connect('127.0.0.1', 8812);
+
+		coroutine.sleep(10);
+		assert.deepEqual({
+			"total": 1,
+			"connections": 1,
+			"accept": 1,
+			"close": 0
+		}, svr.stats.toJSON());
+
+		svr.stats.reset();
+		assert.deepEqual({
+			"total": 1,
+			"connections": 1,
+			"accept": 0,
+			"close": 0
+		}, svr.stats.toJSON());
+
+		c1.close();
+
+		svr.stats.reset();
+		coroutine.sleep(10);
+		assert.deepEqual({
+			"total": 1,
+			"connections": 0,
+			"accept": 0,
+			"close": 1
+		}, svr.stats.toJSON());
+	});
+
+	describe("Smtp", function() {
+		var s;
+
+		it("new & connect", function() {
+			s = new net.Smtp();
+			s.connect("smtp.ym.163.com", 25);
+			s.socket.close();
+		});
+
+		it("net.openSmtp", function() {
+			s = net.openSmtp("smtp.exmail.qq.com", 25);
+		});
+
+		it("command", function() {
+			assert.equal(s.command("HELO", "baoz.me").substr(0, 4),
+				"250 ");
+
+			assert.throws(function() {
+				s.command("FUCK", "baoz.me");
+			});
+		});
+
+		it("hello", function() {
+			s.hello();
+		});
+
+		xdescribe("Auth", function() {
+			it("login", function() {
+				s.login("lion@baoz.cn", "");
+			});
+
+			it("from", function() {
+				s.from("lion@baoz.cn");
+			});
+
+			it("to", function() {
+				s.to("lion@baoz.cn");
+			});
+
+			it("data", function() {
+				s.data("from:lion@baoz.cn\r\n" + "to:lion@baoz.cn\r\n" + "subject:test title\r\n\r\n" + "test text");
+			});
+		});
+
+		it("quit", function() {
+			s.quit();
+		});
+	});
+});
 
 // test.run(console.DEBUG);
