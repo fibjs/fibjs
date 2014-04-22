6d5c5f690767bdc0a7eba6cd57eff6036969664e
xicilion <lion@9465.net>
2014-04-22 06:34:04 +0000

    修改 ssl_test 测试用例，动态生成证书

--- bin/ssl_test.js
+++ bin/ssl_test.js
@@ -8,52 +8,6 @@ var fs = require('fs');
 var os = require('os');
 var coroutine = require('coroutine');
 
-var rsa1024_pem = "-----BEGIN RSA PRIVATE KEY-----\n" +
-	"MIICXQIBAAKBgQDSbmW8qlarL0lLu1XYcg+ocJgcuq5K7EgLcXyy2shAsko7etmZ\n" +
-	"NP3opOeGw58E7tjVsjuadPQ8Hf+9wd316RYwShklDDsy4Hwp4z9afnj56UmvkM0u\n" +
-	"TlsymlIbFftme6aJcYbnX9fdin78Rsa0MbzQbFdeLHsy5zKjsrbm4TS5bwIDAQAB\n" +
-	"AoGAagN2O9NxMHL1MTMi75WfL9Pxvl+KWXKqZSF6mjzAsF9iKI8euyHIXYFepzU8\n" +
-	"kual1RsjDhCnzvWqFvZplW8lXqrHf/P+rS/9Y4gBUw6pjnI/DnFIRwWHRvrUHHSC\n" +
-	"fWOdTCIKdOTkgLZuGFuhEY3RMIW0WSYejjLtftwy0RVxAzkCQQDprgbWqZ/BaafV\n" +
-	"uKKA3shUWWRst/2hV7qDus6YfEj6GfUZHEoNJW4BSuZHUiG4Cdxr0zTLtIP7tNSz\n" +
-	"rCM7FbFrAkEA5ofkxFKdPBD0CQHMb9q13AMHUVe0rJ+hSjqqIBrmqApUOneyAcMV\n" +
-	"76M0QyIQnI2p3POa4Qu/7XChDwRVl7LlDQJBANplxohsAh5fI/hQVriA/tQus/gU\n" +
-	"QdzARFaHijzjs8Tj67mrQd5lhBl7KhuwPEloFfVEcUyNiuj9yeme0VKQZL8CQQCh\n" +
-	"qCfm99vk1Cqc6lL3GRKwPrtx8iPzbVlIWU0ViGe47M1V1rvP+oK2cebjMM8fSUQV\n" +
-	"egpgx8GF+pYmlq6C22M9AkB2oRCDE2Y5UWsXizvvqFOp4LqgQOKVrwS+lVvYXZm/\n" +
-	"AA4uMJDro6IBkrUGJgYepTLQ16o2a2WwtK4ERlBm+pnC\n" +
-	"-----END RSA PRIVATE KEY-----\n";
-
-var ca_pem = "-----BEGIN CERTIFICATE-----\n" +
-	"MIICJzCCAZCgAwIBAgIAMA0GCSqGSIb3DQEBBQUAMDExCzAJBgNVBAYTAkNOMRAw\n" +
-	"DgYDVQQKEwdiYW96LmNuMRAwDgYDVQQDEwdiYW96Lm1lMB4XDTE0MDQyMTEzMTYw\n" +
-	"MVoXDTE1MDQyMTEzMTYwMVowMTELMAkGA1UEBhMCQ04xEDAOBgNVBAoTB2Jhb3ou\n" +
-	"Y24xEDAOBgNVBAMTB2Jhb3oubWUwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGB\n" +
-	"ANJuZbyqVqsvSUu7VdhyD6hwmBy6rkrsSAtxfLLayECySjt62Zk0/eik54bDnwTu\n" +
-	"2NWyO5p09Dwd/73B3fXpFjBKGSUMOzLgfCnjP1p+ePnpSa+QzS5OWzKaUhsV+2Z7\n" +
-	"polxhudf192KfvxGxrQxvNBsV14sezLnMqOytubhNLlvAgMBAAGjUDBOMAwGA1Ud\n" +
-	"EwQFMAMBAQEwHQYDVR0OBBYEFDMQn0OgAthuzps7thMvxtSSYb7eMB8GA1UdIwQY\n" +
-	"MBaAFDMQn0OgAthuzps7thMvxtSSYb7eMA0GCSqGSIb3DQEBBQUAA4GBAAqIhEYc\n" +
-	"fImzOtloyWUY87T0GtvQUE2YQLiXTx56pzk/QT4pya+3MmUpkLet55QtXXITMt4F\n" +
-	"KEB0TYVF01aB5Bg2xpuwRowIn1YGDLs9XQt8TaQ7p8c67JEjhH3a9dzTDCi+HAX4\n" +
-	"qIKQc4ofAGJjiATiB7Dqctk3h2bNS06y2vC6\n" +
-	"-----END CERTIFICATE-----\n";
-
-var crt_pem = "-----BEGIN CERTIFICATE-----\n" +
-	"MIICJjCCAY+gAwIBAgIAMA0GCSqGSIb3DQEBBQUAMDExCzAJBgNVBAYTAkNOMRAw\n" +
-	"DgYDVQQKEwdiYW96LmNuMRAwDgYDVQQDEwdiYW96Lm1lMB4XDTE0MDQyMTEzMTgz\n" +
-	"MFoXDTE1MDQyMTEzMTgzMFowMzELMAkGA1UEBhMCQ04xEDAOBgNVBAoTB2Jhb3ou\n" +
-	"Y24xEjAQBgNVBAMTCWxvY2FsaG9zdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkC\n" +
-	"gYEA0m5lvKpWqy9JS7tV2HIPqHCYHLquSuxIC3F8strIQLJKO3rZmTT96KTnhsOf\n" +
-	"BO7Y1bI7mnT0PB3/vcHd9ekWMEoZJQw7MuB8KeM/Wn54+elJr5DNLk5bMppSGxX7\n" +
-	"ZnumiXGG51/X3Yp+/EbGtDG80GxXXix7Mucyo7K25uE0uW8CAwEAAaNNMEswCQYD\n" +
-	"VR0TBAIwADAdBgNVHQ4EFgQUMxCfQ6AC2G7Omzu2Ey/G1JJhvt4wHwYDVR0jBBgw\n" +
-	"FoAUMxCfQ6AC2G7Omzu2Ey/G1JJhvt4wDQYJKoZIhvcNAQEFBQADgYEAj1cEcKNR\n" +
-	"uxKjcmy0JXBiM1ADKBFEpmREPmR9oiUXiB7UcNzEDZuh49hHYELl/cxCC6utF4Zq\n" +
-	"wS/7NDim6qrL5H5cUPgaqYhP5sWES6sPhcDgTgF4QtHfdJdbDWjbN3wpzHaOujiA\n" +
-	"BNezFdEB4uBhbTvkdAoMzklsGyc39Gegb5w=\n" +
-	"-----END CERTIFICATE-----\n";
-
 var net_config = {
 	family: net.AF_INET6,
 	address: '::1'
@@ -65,11 +19,15 @@ if (os.type == 'Windows' && os.version < "6.0")
 		address: '127.0.0.1'
 	};
 
-var crt = new crypto.X509Cert();
-crt.load(crt_pem);
-
 var pk = new crypto.PKey();
-pk.importKey(rsa1024_pem);
+pk.genRsaKey(1024);
+
+var crt = new crypto.X509Req("CN=localhost", pk).sign("CN=baoz.me", pk);
+var ca = new crypto.X509Req("CN=baoz.me", pk).sign("CN=baoz.me", pk, {
+	ca: true
+});
+
+var ca_pem = ca.dump()[0];
 
 function del(f) {
 	try {
@@ -224,6 +182,30 @@ describe('ssl', function() {
 		for (i = 0; i < 10; i++) {
 			var s1 = new net.Socket();
 			s1.connect("127.0.0.1", 9083);
+
+			var ss = new ssl.Socket();
+			ss.connect(s1);
+
+			ss.write(new Buffer("GET / HTTP/1.0"));
+			assert.equal("GET / HTTP/1.0", ss.read());
+
+			ss.close();
+			s1.close();
+		}
+	});
+
+	it("Server", function() {
+		var svr = new ssl.Server(crt, pk, 9084, function(s) {
+			var buf;
+
+			while (buf = s.read())
+				s.write(buf);
+		});
+		svr.asyncRun();
+
+		for (i = 0; i < 10; i++) {
+			var s1 = new net.Socket();
+			s1.connect("127.0.0.1", 9084);
 
 			var ss = new ssl.Socket();
 			ss.connect(s1);
