ef470bba38f9518cae70f5c9ca8cfd4b57a3d324
xicilion <lion@9465.net>
2014-04-23 18:17:54 +0000

    继续 fuck gcc

--- fibjs/src/fuck_sym.c
+++ fibjs/src/fuck_sym.c
@@ -1,10 +1,35 @@
 #include <string.h>
+#include <stdarg.h>
 #include <osconfig.h>
 
-#if defined(Linux) && defined(x64)
+#ifdef Linux
+
+#if defined(x64)
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
     return memcpy(dest, src, n);
 }
+#endif
+
+void __longjmp_chk()
+{
+}
+
+void __fread_chk()
+{
+}
+
+int __isoc99_sscanf(const char *s, const char *format, ...)
+{
+    va_list arg;
+    int done;
+
+    va_start(arg, format);
+    done = vsscanf(s, format, arg);
+    va_end(arg);
+
+    return done;
+}
+
 #endif
