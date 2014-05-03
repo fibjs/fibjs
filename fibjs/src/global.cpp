1f9559a796bd4e90a22ebec22cb55237bfebf683
xicilion <lion@9465.net>
2014-05-03 17:41:46 +0000

    合并 global 代码

--- fibjs/src/global.cpp
+++ fibjs/src/global.cpp
@@ -1,5 +1,6 @@
 #include "ifs/global.h"
 #include "ifs/coroutine.h"
+#include "ifs/vm.h"
 
 namespace fibjs
 {
@@ -20,6 +21,26 @@ result_t global_base::GC()
 {
     v8::V8::LowMemoryNotification();
     return 0;
+}
+
+result_t global_base::run(const char *fname)
+{
+    obj_ptr<SandBox_base> sbox;
+    result_t hr = vm_base::current(sbox);
+    if (hr < 0)
+        return hr;
+
+    return sbox->run(fname);
+}
+
+result_t global_base::require(const char *id, v8::Local<v8::Value> &retVal)
+{
+    obj_ptr<SandBox_base> sbox;
+    result_t hr = vm_base::current(sbox);
+    if (hr < 0)
+        return hr;
+
+    return sbox->require(id, retVal);
 }
 
 }
