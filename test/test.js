9cf58bf9bf27607a5665a45443428164ebe6cb38
响马 <lion@9465.net>
2014-07-06 13:41:11 +0800

    删除临时测试文件

--- test/test.js
+++
@@ -1,9 +0,0 @@
-console.config(["console", "syslog"]);
-
-console.debug(100);
-console.info(100);
-console.notice(100);
-console.warn(100);
-console.error(100);
-console.crit(100);
-console.alert(100);
