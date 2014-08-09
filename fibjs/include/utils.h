52d0f02b91fb98855d419baafa2ce4c914c633ad
响马 <lion@9465.net>
2014-08-09 18:21:50 +0800

    消除 ARRAYSIZE 在 Windows 下的编译警告

--- fibjs/include/utils.h
+++ fibjs/include/utils.h
@@ -275,8 +275,10 @@ typedef int result_t;
     {} \
      
 
+#ifndef ARRAYSIZE
 #define ARRAYSIZE(a) \
     ((sizeof(a) / sizeof(*(a))) / static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
+#endif
 
 #ifdef _MSC_VER
 
