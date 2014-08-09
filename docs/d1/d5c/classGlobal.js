74794dcae50c403e34a851e4bf6e6cddd3dc55d4
响马 <lion@9465.net>
2014-08-10 01:44:02 +0800

    更新文档

---
+++ docs/d1/d5c/classGlobal.js
@@ -0,0 +1,9 @@
+var classGlobal =
+[
+    [ "Buffer", "d1/d5c/classGlobal.html#a91a01882d655933dc5dad77d89031c95", null ],
+    [ "run", "d1/d5c/classGlobal.html#a5f2cebdcf10424a6dfcc99993b8df786", null ],
+    [ "require", "d1/d5c/classGlobal.html#a901e499d2a9b5d5ddd4272c62de54409", null ],
+    [ "GC", "d1/d5c/classGlobal.html#a9f77f3208cfe5602e545b303225625c0", null ],
+    [ "console", "d1/d5c/classGlobal.html#a3e3e5df99e3bfbbc2bcbf26995224a23", null ],
+    [ "global", "d1/d5c/classGlobal.html#a5f18b32ab87d923f41bbd1f1c1fdbfce", null ]
+];
--- docs/df/d84/classglobal.js
+++ docs/df/d84/classglobal.js
@@ -2,8 +2,6 @@ var classglobal =
 [
     [ "Buffer", "df/d84/classglobal.html#a77b5ab3a88955255ac7abc7251919fbc", null ],
     [ "run", "df/d84/classglobal.html#a917c15ea7d2eafaa6df0aea5868a86e7", null ],
-    [ "repl", "df/d84/classglobal.html#aa976e0437bfcac393e2ef5e1ccea5bca", null ],
-    [ "define", "df/d84/classglobal.html#a0ee91eecae9f066671a13227e61947e0", null ],
     [ "require", "df/d84/classglobal.html#a9b11defd1000737a5b70b50edfcc8156", null ],
     [ "GC", "df/d84/classglobal.html#a02a28758a633a7b1493471415c8949ba", null ],
     [ "console", "df/d84/classglobal.html#a69cd8fa430cb4536156e8052a37c9303", null ]
