d9041c68dda490f25b3950e06922a4ee66e0a1fd
响马 <lion@9465.net>
2014-08-17 03:04:17 +0800

    删除废弃的文档，重新生成

--- docs/dc/d22/classutils.js
+++
@@ -1,6 +0,0 @@
-var classutils =
-[
-    [ "Stats", "dc/d22/classutils.html#a40030184cca731b9860965ecc32b3911", null ],
-    [ "LruCache", "dc/d22/classutils.html#a22dcdab2ff3b36051fb0431ce218f679", null ],
-    [ "buildInfo", "dc/d22/classutils.html#a628aa35e76af2d90fb0c3464650e9c1c", null ]
-];
