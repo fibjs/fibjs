4210da6812fef6d3adb40d222ec3d33b98803c91
响马 <lion@9465.net>
2014-08-29 21:22:15 +0800

    修改 fuck_sym 扩展名

---
+++ fibjs/src/fuck_sym.cpp
@@ -0,0 +1,47 @@
+#include <osconfig.h>
+
+#ifdef Linux
+
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern "C"
+{
+
+#if defined(x64)
+    __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+    void *__wrap_memcpy(void *dest, const void *src, size_t n)
+    {
+        return memcpy(dest, src, n);
+    }
+#endif
+
+    void __longjmp_chk()
+    {
+        puts("unexpected __longjmp_chk.");
+        exit(-1);
+    }
+
+    size_t __fread_chk(void *, size_t, size_t, size_t, FILE *)
+    {
+        puts("unexpected __fread_chk.");
+        exit(-1);
+        return 0;
+    }
+
+    int __isoc99_sscanf(const char *s, const char *format, ...)
+    {
+        va_list arg;
+        int done;
+
+        va_start(arg, format);
+        done = vsscanf(s, format, arg);
+        va_end(arg);
+
+        return done;
+    }
+}
+
+#endif
