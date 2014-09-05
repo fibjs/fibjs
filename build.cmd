95f66b00c77246addd56399660177cc5678f64f3
响马 <lion@9465.net>
2014-09-06 02:49:30 +0800

    开启  vs.net 并行编译

--- build.cmd
+++ build.cmd
@@ -11,31 +11,31 @@ echo build [Release ^| Release32 ^| Debug ^| Debug32 ^| clean]
 goto out
 
 :release
-msbuild fibjs.sln /t:Build /p:Configuration=Release;Platform=x64
+msbuild fibjs.sln /t:Build /p:Configuration=Release;Platform=x64 /m
 cd bin\Windows_Release
 fibjs ../../fibjs/gen_install.js
 cd ..\..
 cd installer
-msbuild installer.sln /t:Build /p:Configuration=Release;Platform=x64
+msbuild installer.sln /t:Build /p:Configuration=Release;Platform=x64 /m
 cd ..
 goto out
 
 :debug
-msbuild fibjs.sln /t:Build /p:Configuration=Debug;Platform=x64
+msbuild fibjs.sln /t:Build /p:Configuration=Debug;Platform=x64 /m
 goto out
 
 :release32
-msbuild fibjs.sln /t:Build /p:Configuration=Release;Platform=Win32
+msbuild fibjs.sln /t:Build /p:Configuration=Release;Platform=Win32 /m
 cd bin\Windows_Release32
 fibjs ../../fibjs/gen_install.js
 cd ..\..
 cd installer
-msbuild installer.sln /t:Build /p:Configuration=Release;Platform=Win32
+msbuild installer.sln /t:Build /p:Configuration=Release;Platform=Win32 /m
 cd ..
 goto out
 
 :debug32
-msbuild fibjs.sln /t:Build /p:Configuration=Debug;Platform=Win32
+msbuild fibjs.sln /t:Build /p:Configuration=Debug;Platform=Win32 /m
 goto out
 
 :clean
