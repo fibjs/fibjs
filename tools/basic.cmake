56e56e88543f8abd87f2ef09c4b4be829e35f03d
响马 <lion@9465.net>
2014-09-11 14:22:05 +0800

    增加 visibility=hidden 编译选项

--- tools/basic.cmake
+++ tools/basic.cmake
@@ -12,7 +12,7 @@ if(NOT flags)
 	set(flags " ")
 endif()
 
-set(flags "${flags} -fshort-wchar -fsigned-char -fmessage-length=0 -Wno-long-long -fdata-sections -Wno-strict-aliasing -ffunction-sections -fno-exceptions -D_FILE_OFFSET_BITS=64")
+set(flags "${flags} -fshort-wchar -fsigned-char -fmessage-length=0 -Wno-long-long -fdata-sections -Wno-strict-aliasing -ffunction-sections -fno-exceptions -fvisibility=hidden -D_FILE_OFFSET_BITS=64")
 set(link_flags " ")
 
 if(${OS} STREQUAL "Darwin")
