d47874c643cac604687b4a605f442054aef805bd
响马 <lion@9465.net>
2014-09-13 00:41:37 +0800

    再打 v8 freebsd 补丁，fuck v8

--- vender/v8/src/base/platform/platform-freebsd.cc
+++ vender/v8/src/base/platform/platform-freebsd.cc
@@ -145,21 +145,21 @@ std::vector<OS::SharedLibraryAddress> OS::GetSharedLibraryAddresses() {
     if (bytes_read < 8) break;
     unsigned end = StringToLong(addr_buffer);
     char buffer[MAP_LENGTH];
-    int bytes_read = -1;
+    int bytes_pos = -1;
     do {
-      bytes_read++;
-      if (bytes_read >= MAP_LENGTH - 1)
+      bytes_pos++;
+      if (bytes_pos >= MAP_LENGTH - 1)
         break;
-      bytes_read = read(fd, buffer + bytes_read, 1);
-      if (bytes_read < 1) break;
-    } while (buffer[bytes_read] != '\n');
-    buffer[bytes_read] = 0;
+      bytes_pos = read(fd, buffer + bytes_pos, 1);
+      if (bytes_pos < 1) break;
+    } while (buffer[bytes_pos] != '\n');
+    buffer[bytes_pos] = 0;
     // Ignore mappings that are not executable.
     if (buffer[3] != 'x') continue;
     char* start_of_path = index(buffer, '/');
     // There may be no filename in this line.  Skip to next.
     if (start_of_path == NULL) continue;
-    buffer[bytes_read] = 0;
+    buffer[bytes_pos] = 0;
     result.push_back(SharedLibraryAddress(start_of_path, start, end));
   }
   close(fd);
