5a2052a072b422e59722b3fc692000acde95a132
响马 <lion@9465.net>
2014-09-12 15:53:27 +0800

    增加新的 weak_ptr，用于建立反向弱引用

--- fibjs/include/obj_ptr.h
+++ fibjs/include/obj_ptr.h
@@ -17,13 +17,51 @@ namespace fibjs
 class obj_base
 {
 public:
+    class weak_stub
+    {
+    public:
+        weak_stub(obj_base *obj) :
+            refs_(1), this_(obj)
+        {
+        }
+
+        void Ref()
+        {
+            exlib::atom_inc(&refs_);
+        }
+
+        void Unref()
+        {
+            if (exlib::atom_dec(&refs_) == 0)
+                delete this;
+        }
+
+        void clean()
+        {
+            exlib::atom_xchg(&this_, (obj_base *)NULL);
+            Unref();
+        }
+
+        obj_base *obj()
+        {
+            return this_;
+        }
+
+    private:
+        volatile int32_t refs_;
+        obj_base *volatile this_;
+    };
+
+public:
     obj_base() :
-        refs_(0)
+        refs_(0), weak_(NULL)
     {
     }
 
     virtual ~obj_base()
     {
+        if (weak_)
+            weak_->clean();
     }
 
 public:
@@ -44,6 +82,18 @@ public:
             delete this;
     }
 
+    weak_stub *weak_ptr()
+    {
+        if (weak_)
+            return weak_;
+
+        weak_stub *pNew = new weak_stub(this);
+        if (exlib::CompareAndSwap(&weak_, (weak_stub *)NULL, pNew))
+            delete pNew;
+
+        return weak_;
+    }
+
 protected:
     int32_t internalRef()
     {
@@ -57,6 +107,7 @@ protected:
 
 private:
     volatile int32_t refs_;
+    weak_stub *volatile weak_;
 };
 
 template<class T>
@@ -167,6 +218,115 @@ private:
 
 private:
     T *volatile p;
+};
+
+template<class T>
+class weak_ptr
+{
+public:
+    weak_ptr() :
+        p(NULL)
+    {
+    }
+
+    weak_ptr(T *lp) :
+        p(NULL)
+    {
+        _assign(lp);
+    }
+
+    weak_ptr(const weak_ptr<T> &lp) :
+        p(NULL)
+    {
+        _assign(lp);
+    }
+
+    ~weak_ptr()
+    {
+        Release();
+    }
+
+    T *operator=(T *lp)
+    {
+        return _assign(lp);
+    }
+
+    template<class Q>
+    T *operator=(Q *lp)
+    {
+        return _assign(lp);
+    }
+
+    T *operator=(const weak_ptr<T> &lp)
+    {
+        return _assign(lp);
+    }
+
+    template<class Q>
+    T *operator=(const weak_ptr<Q> &lp)
+    {
+        return _assign(lp);
+    }
+
+    operator T *() const
+    {
+        return p ? (T *)p->obj() : (T *)NULL;
+    }
+
+    T &operator*() const
+    {
+        return *p;
+    }
+
+    T **operator&()
+    {
+        return (T **)&p;
+    }
+
+    bool operator!() const
+    {
+        return (p == NULL) || (p->obj() == NULL);
+    }
+
+    bool operator==(T *pT) const
+    {
+        if (p == NULL)
+            return pT == NULL;
+
+        if (pT == NULL)
+            return false;
+
+        return (T *)p->obj() == pT;
+    }
+
+    T *operator->()
+    {
+        return p ? (T *)p->obj() : (T *)NULL;
+    }
+
+    void Release()
+    {
+        obj_base::weak_stub *p1 = exlib::atom_xchg(&p, (obj_base::weak_stub *)NULL);
+        if (p1)
+            p1->Unref();
+    }
+
+private:
+    T *_assign(T *p2)
+    {
+        obj_base::weak_stub *p2_ = p2 ? p2->weak_ptr() : (obj_base::weak_stub *)NULL;
+        if (p2_)
+            p2_->Ref();
+
+        obj_base::weak_stub *p1 = exlib::atom_xchg(&p, p2_);
+        if (p1)
+            p1->Unref();
+
+        return p2;
+    }
+
+private:
+    obj_base::weak_stub *volatile p;
 };
 
 template<class T>
