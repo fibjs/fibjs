60f9e87b68c8044034515ce52721c5c09659caae
响马 <lion@9465.net>
2014-09-30 23:14:42 +0800

    修复 --prof 参数运行时阻塞错误

--- vender/v8/src/base/platform/platform-fiber.cc
+++ vender/v8/src/base/platform/platform-fiber.cc
@@ -27,79 +27,77 @@ namespace base
 class Thread::PlatformData
 {
 public:
-	PlatformData() :
-			thread_(NULL)
-	{
-	}
+    PlatformData() :
+        thread_(NULL)
+    {
+    }
 
-	exlib::Fiber* thread_; // Thread handle for pthread.
+    exlib::Fiber *thread_; // Thread handle for pthread.
 };
 
-Thread::Thread(const Options& options) :
-		data_(new PlatformData()), stack_size_(options.stack_size())
+Thread::Thread(const Options &options) :
+    data_(new PlatformData()), stack_size_(options.stack_size()), start_semaphore_(NULL)
 {
-	set_name(options.name());
+    set_name(options.name());
 }
 
 Thread::~Thread()
 {
-	data_->thread_->Unref();
-	delete data_;
+    data_->thread_->Unref();
+    delete data_;
 }
 
-static void* ThreadEntry(void* arg)
+static void *ThreadEntry(void *arg)
 {
-	Thread* thread = reinterpret_cast<Thread*>(arg);
-
-	thread->data()->thread_ = exlib::Fiber::Current();
-	thread->Run();
-	return NULL;
+    Thread *thread = reinterpret_cast<Thread *>(arg);
+    thread->NotifyStartedAndRun();
+    return NULL;
 }
 
-void Thread::set_name(const char* name)
+void Thread::set_name(const char *name)
 {
-	strncpy(name_, name, sizeof(name_));
-	name_[sizeof(name_) - 1] = '\0';
+    strncpy(name_, name, sizeof(name_));
+    name_[sizeof(name_) - 1] = '\0';
 }
 
 void Thread::Start()
 {
-	data_->thread_ = exlib::Service::CreateFiber(ThreadEntry, this);
+    data_->thread_ = exlib::Service::CreateFiber(ThreadEntry, this);
 }
 
 void Thread::Join()
 {
-	data_->thread_->join();
+    data_->thread_->join();
 }
 
 Thread::LocalStorageKey Thread::CreateThreadLocalKey()
 {
-	return static_cast<LocalStorageKey>(exlib::Service::tlsAlloc());
+    return static_cast<LocalStorageKey>(exlib::Service::tlsAlloc());
 }
 
 void Thread::DeleteThreadLocalKey(LocalStorageKey key)
 {
-	exlib::Service::tlsFree(static_cast<int>(key));
+    exlib::Service::tlsFree(static_cast<int>(key));
 }
 
-void* Thread::GetThreadLocal(LocalStorageKey key)
+void *Thread::GetThreadLocal(LocalStorageKey key)
 {
-	return exlib::Service::tlsGet(static_cast<int>(key));
+    return exlib::Service::tlsGet(static_cast<int>(key));
 }
 
-void Thread::SetThreadLocal(LocalStorageKey key, void* value)
+void Thread::SetThreadLocal(LocalStorageKey key, void *value)
 {
-	exlib::Service::tlsPut(static_cast<int>(key), value);
+    exlib::Service::tlsPut(static_cast<int>(key), value);
 }
 
 void Thread::YieldCPU()
 {
-	exlib::Fiber::yield();
+    exlib::Fiber::yield();
 }
 
 void OS::Sleep(int milliseconds)
 {
-	exlib::Fiber::sleep(milliseconds);
+    exlib::Fiber::sleep(milliseconds);
 }
 
 }
