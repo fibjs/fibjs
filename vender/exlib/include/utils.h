9fd423d4dfa90d5cd67f0d62f807f92c745e51e6
响马 <lion@9465.net>
2015-04-23 23:21:39 +0800

    封装 atomic 对象

--- vender/exlib/include/utils.h
+++ vender/exlib/include/utils.h
@@ -22,4 +22,135 @@
 #include "utils_arm.h"
 #endif
 
+namespace exlib
+{
+
+class atomic
+{
+public:
+	atomic() : m_v(0) {
+	}
+
+	atomic(int32_t new_value)
+	{
+		operator=(new_value);
+	}
+
+	atomic(const atomic &new_value)
+	{
+		operator=(new_value);
+	}
+
+public:
+	int32_t operator=(int32_t new_value)
+	{
+		xchg(new_value);
+		return new_value;
+	}
+
+	int32_t operator=(const atomic &new_value)
+	{
+		int32_t v = new_value.m_v;
+
+		xchg(v);
+		return v;
+	}
+
+	operator int32_t () const
+	{
+		return m_v;
+	}
+
+	int32_t value() const
+	{
+		return m_v;
+	}
+
+	int32_t CompareAndSwap(int32_t old_value, int32_t new_value)
+	{
+		return exlib::CompareAndSwap(&m_v, old_value, new_value);
+	}
+
+	inline int32_t add(int32_t incr)
+	{
+		return atom_add(&m_v, incr);
+	}
+
+	inline int32_t xchg(int32_t new_value)
+	{
+		return atom_xchg(&m_v, new_value);
+	}
+
+	inline int32_t inc()
+	{
+		return add(1);
+	}
+
+	inline int32_t dec()
+	{
+		return add(-1);
+	}
+
+private:
+	volatile int32_t m_v;
+};
+
+template<class T>
+class atomic_ptr
+{
+public:
+	atomic_ptr() : m_v(0) {
+	}
+
+	atomic_ptr(T* new_value)
+	{
+		operator=(new_value);
+	}
+
+	atomic_ptr(const atomic_ptr &new_value)
+	{
+		operator=(new_value);
+	}
+
+public:
+	T* operator=(T* new_value)
+	{
+		atom_xchg(&m_v, new_value);
+		return new_value;
+	}
+
+	T* operator=(const atomic_ptr &new_value)
+	{
+		T* v = new_value.m_v;
+
+		atom_xchg(&m_v, v);
+		return v;
+	}
+
+	operator T* () const
+	{
+		return m_v;
+	}
+
+	T* value() const
+	{
+		return m_v;
+	}
+
+	T* CompareAndSwap(T* old_value, T* new_value)
+	{
+		return exlib::CompareAndSwap(&m_v, old_value, new_value);
+	}
+
+	inline T* xchg(T* new_value)
+	{
+		return atom_xchg(&m_v, new_value);
+	}
+
+private:
+	T *volatile m_v;
+};
+
+}
+
 #endif
