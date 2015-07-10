528b2f8e1e5eb4c1f233db306e6082850037a226
响马 <lion@9465.net>
2015-07-11 01:38:16 +0800

    使用 path 查询全路径，修复 windows 下测试用例错误

--- test/main.js
+++ test/main.js
@@ -3,9 +3,9 @@
 var test = require("test");
 var coroutine = require('coroutine');
 var process = require('process');
-var common = require('./common.js');
+var path = require('path');
 
-process.chdir(common.basedir);
+process.chdir(path.dirname(path.fullpath(process.argv[1])));
 
 run("assert_test.js");
 run("expect_test.js");
