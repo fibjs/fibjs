52a0e8744ea693349919fb459266dc3ec6eb0e33
响马 <lion@9465.net>
2015-08-26 00:03:08 +0800

    socket api 提供工作线程版本

--- fibjs/include/Socket_api.h
+++ fibjs/include/Socket_api.h
@@ -18,11 +18,18 @@ namespace socket
 
 void *create();
 void destroy(void *sock);
+
 int32_t connect(void *sock, const char *host, int32_t port);
 void *connect(const char *host, int32_t port);
 int32_t recv(void *sock, void *buffer, int32_t cbBuffer);
 int32_t read(void *sock, void *buffer, int32_t cbBuffer);
 int32_t send(void *sock, const void *buffer, int32_t cbBuffer);
+
+int32_t c_connect(void *sock, const char *host, int32_t port);
+void *c_connect(const char *host, int32_t port);
+int32_t c_recv(void *sock, void *buffer, int32_t cbBuffer);
+int32_t c_read(void *sock, void *buffer, int32_t cbBuffer);
+int32_t c_send(void *sock, const void *buffer, int32_t cbBuffer);
 
 }
 
--- fibjs/src/net/Socket_api.cpp
+++ fibjs/src/net/Socket_api.cpp
@@ -155,6 +155,125 @@ int32_t send(void *sock, const void *buffer, int32_t cbBuffer)
     return (int32_t) strBuf.length();
 }
 
+int32_t c_connect(void *sock, const char *host, int32_t port)
+{
+    if (!sock)
+    {
+        Runtime::setError(CALL_E_INVALID_CALL);
+        return 0;
+    }
+
+    result_t hr = ((Socket *) sock)->cc_connect(host, port);
+    if (hr < 0)
+    {
+        Runtime::setError(hr);
+        return 0;
+    }
+
+    return 1;
+}
+
+void *c_connect(const char *host, int32_t port)
+{
+    void *socket;
+
+    socket = create();
+    if (!c_connect(socket, host, port))
+    {
+        destroy(socket);
+        return NULL;
+    }
+
+    return socket;
+}
+
+int32_t c_recv(void *sock, void *buffer, int32_t cbBuffer)
+{
+    if (!sock)
+    {
+        Runtime::setError(CALL_E_INVALID_CALL);
+        return -1;
+    }
+
+    obj_ptr<Buffer_base> retVal;
+
+    result_t hr = ((Socket *) sock)->cc_recv(cbBuffer, retVal);
+    if (hr < 0)
+    {
+        Runtime::setError(hr);
+        return -1;
+    }
+
+    std::string strBuf;
+
+    if (hr != CALL_RETURN_NULL)
+    {
+        retVal->toString(strBuf);
+        memcpy(buffer, strBuf.c_str(), strBuf.length());
+    }
+
+    return (int32_t) strBuf.length();
+}
+
+int32_t c_read(void *sock, void *buffer, int32_t cbBuffer)
+{
+    if (!sock)
+    {
+        Runtime::setError(CALL_E_INVALID_CALL);
+        return -1;
+    }
+
+    if (cbBuffer <= 0)
+        return 0;
+
+    obj_ptr<Buffer_base> retVal;
+
+    result_t hr = ((Socket *) sock)->cc_read(cbBuffer, retVal);
+    if (hr < 0)
+    {
+        Runtime::setError(hr);
+        return -1;
+    }
+
+    std::string strBuf;
+
+    if (hr != CALL_RETURN_NULL)
+    {
+        retVal->toString(strBuf);
+        if ((int32_t) strBuf.length() < cbBuffer)
+            return -1;
+        memcpy(buffer, strBuf.c_str(), strBuf.length());
+    }
+
+    return (int32_t) strBuf.length();
+}
+
+int32_t c_send(void *sock, const void *buffer, int32_t cbBuffer)
+{
+    if (!sock)
+    {
+        Runtime::setError(CALL_E_INVALID_CALL);
+        return -1;
+    }
+
+    if (cbBuffer <= 0)
+        return 0;
+
+    std::string strBuf((const char *) buffer, cbBuffer);
+    obj_ptr<Buffer_base> buf;
+
+    buf = new Buffer(strBuf);
+
+    result_t hr = ((Socket *) sock)->cc_send(buf);
+    if (hr < 0)
+    {
+        Runtime::setError(hr);
+        return -1;
+    }
+
+    return (int32_t) strBuf.length();
+}
+
 }
 
 }
