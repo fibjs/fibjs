3357a2aba396688b16feab8c7938616342073c92
响马 <lion@9465.net>
2015-12-11 22:10:40 +0800

    修改工作线程管理

--- fibjs/src/base/acPool.cpp
+++ fibjs/src/base/acPool.cpp
@@ -11,7 +11,6 @@ static exlib::Queue<AsyncEvent> s_acPool;
 
 static int32_t s_threads;
 static exlib::atomic s_idleThreads;
-static int32_t s_idleCount;
 
 class _acThread: public exlib::OSThread
 {
@@ -40,7 +39,8 @@ public:
             }
 
             p = s_acPool.get();
-            s_idleThreads.dec();
+            if (s_idleThreads.dec() == 0)
+                new _acThread();
 
             p->invoke();
         }
@@ -52,43 +52,16 @@ void AsyncEvent::async()
     s_acPool.put(this);
 }
 
-static class _acThreadDog: public exlib::OSThread
-{
-public:
-    virtual void Run()
-    {
-        int32_t cpus = 0;
-
-        os_base::CPUs(cpus);
-        if (cpus < 3)
-            cpus = 3;
-
-        s_threads = cpus;
-        s_idleCount = 100;
-
-        while (1)
-        {
-            if (s_idleThreads < s_threads)
-            {
-                if (++s_idleCount > 5)
-                {
-                    s_idleCount = 0;
-
-                    for (int32_t i = (int32_t)s_idleThreads; i < s_threads; i++)
-                        new _acThread();
-                }
-            }
-            else
-                s_idleCount = 0;
-
-            sleep(100);
-        }
-    }
-} s_dog;
-
 void init_acThread()
 {
-    s_dog.start();
+    int32_t cpus = 0;
+
+    os_base::CPUs(cpus);
+    if (cpus < 3)
+        cpus = 3;
+
+    s_threads = cpus * 3;
+    new _acThread();
 }
 
 }
