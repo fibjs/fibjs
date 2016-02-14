f24c98257462a39314ce36b0e7c27029f7087604
响马 <lion@9465.net>
2016-02-14 16:36:01 +0800

    独立 Timer.h 抽象定时器逻辑

---
+++ fibjs/include/Timer.h
@@ -0,0 +1,99 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Sun 14, 2016
+ *      Author: lion
+ */
+
+#include "ifs/Timer.h"
+#include <vector>
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+namespace fibjs
+{
+
+class Timer : public Timer_base,
+	public exlib::Task_base
+{
+public:
+	Timer(int32_t timeout = 0, bool repeat = false) :
+		m_timeout(timeout), m_repeat(repeat), m_cancel(false)
+	{
+		if (timeout > 0)
+			sleep();
+		else
+			resume();
+
+		Ref();
+	}
+
+public:
+	// Timer_base
+	virtual result_t clear()
+	{
+		if (!m_cancel)
+		{
+			m_cancel = true;
+			exlib::Fiber::cancel_sleep(this);
+		}
+
+		return 0;
+	}
+
+public:
+	// exlib::Task_base
+	virtual void suspend()
+	{
+	}
+
+	virtual void suspend(exlib::spinlock& lock)
+	{
+		lock.unlock();
+	}
+
+	virtual void resume()
+	{
+		syncCall(holder(), _callback, this);
+	}
+
+	void sleep()
+	{
+		exlib::Fiber::sleep(m_timeout, this);
+	}
+
+public:
+	virtual void on_timer()
+	{
+	}
+
+private:
+	void callback()
+	{
+		if (!m_cancel)
+		{
+			on_timer();
+
+			if (m_repeat && !m_cancel)
+				sleep();
+			else
+				Unref();
+		} else
+			Unref();
+	}
+
+	static void _callback(Timer* pThis)
+	{
+		pThis->callback();
+	}
+
+private:
+	int32_t m_timeout;
+	bool m_repeat;
+	bool m_cancel;
+};
+
+}
+
+#endif
--- fibjs/src/global/Timer.cpp
+++ fibjs/src/global/Timer.cpp
@@ -7,99 +7,29 @@
 
 #include "ifs/global.h"
 #include "Fiber.h"
+#include "Timer.h"
 
 namespace fibjs
 {
 
-class Timer : public Timer_base,
-	public exlib::Task_base
+class JSTimer : public Timer
 {
 public:
-	Timer(v8::Local<v8::Function> callback, int32_t timeout, bool repeat = false) :
-		m_timeout(timeout), m_repeat(repeat), m_cancel(false)
+	JSTimer(v8::Local<v8::Function> callback, int32_t timeout = 0, bool repeat = false) :
+		Timer(timeout, repeat)
 	{
 		wrap()->SetHiddenValue(holder()->NewFromUtf8("callback"), callback);
-
-		if (m_timeout < 1)
-			m_timeout = 1;
-
-		sleep();
-		Ref();
-	}
-
-	Timer(v8::Local<v8::Function> callback) :
-		m_timeout(0), m_repeat(false), m_cancel(false)
-	{
-		wrap()->SetHiddenValue(holder()->NewFromUtf8("callback"), callback);
-
-		resume();
-		Ref();
 	}
 
 public:
-	// Timer_base
-	virtual result_t clear()
+	virtual void on_timer()
 	{
-		if (!m_cancel)
-		{
-			m_cancel = true;
-			exlib::Fiber::cancel_sleep(this);
-		}
+		JSFiber::scope s;
+		v8::Local<v8::Value> v = wrap()->GetHiddenValue(holder()->NewFromUtf8("callback"));
 
-		return 0;
+		if (!v.IsEmpty() && v->IsFunction())
+			v8::Local<v8::Function>::Cast(v)->Call(wrap(), 0, NULL);
 	}
-
-public:
-	// exlib::Task_base
-	virtual void suspend()
-	{
-	}
-
-	virtual void suspend(exlib::spinlock& lock)
-	{
-		lock.unlock();
-	}
-
-	virtual void resume()
-	{
-		syncCall(holder(), _callback, this);
-	}
-
-	void sleep()
-	{
-		exlib::Fiber::sleep(m_timeout, this);
-	}
-
-private:
-	void callback()
-	{
-		if (!m_cancel)
-		{
-			{
-				JSFiber::scope s;
-				v8::Local<v8::Value> v = wrap()->GetHiddenValue(holder()->NewFromUtf8("callback"));
-
-				if (!v.IsEmpty() && v->IsFunction())
-					v8::Local<v8::Function>::Cast(v)->Call(wrap(), 0, NULL);
-			}
-
-			if (m_repeat && !m_cancel)
-				sleep();
-			else
-				Unref();
-		} else
-			Unref();
-	}
-
-	static void _callback(Timer* pThis)
-	{
-		pThis->callback();
-	}
-
-private:
-	int32_t m_timeout;
-	bool m_repeat;
-	bool m_cancel;
 };
 
 result_t global_base::clearInterval(Timer_base* t)
@@ -119,19 +49,25 @@ result_t global_base::clearImmediate(Timer_base* t)
 
 result_t global_base::setInterval(v8::Local<v8::Function> callback, int32_t timeout, obj_ptr<Timer_base>& retVal)
 {
-	retVal = new Timer(callback, timeout, true);
+	if (timeout < 1)
+		timeout = 1;
+
+	retVal = new JSTimer(callback, timeout, true);
 	return 0;
 }
 
 result_t global_base::setTimeout(v8::Local<v8::Function> callback, int32_t timeout, obj_ptr<Timer_base>& retVal)
 {
-	retVal = new Timer(callback, timeout);
+	if (timeout < 1)
+		timeout = 1;
+
+	retVal = new JSTimer(callback, timeout);
 	return 0;
 }
 
 result_t global_base::setImmediate(v8::Local<v8::Function> callback, obj_ptr<Timer_base>& retVal)
 {
-	retVal = new Timer(callback);
+	retVal = new JSTimer(callback);
 	return 0;
 }
 
