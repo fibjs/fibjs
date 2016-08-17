ff19d5cbf2f636f873e614692c6e2df59e6c832d
响马 <lion@9465.net>
2016-08-17 15:15:44 +0800

    消除测试用例误判

--- test/net_test.js
+++ test/net_test.js
@@ -299,7 +299,7 @@ describe("net", function() {
 		});
 		var t2 = new Date();
 
-		assert.greaterThan(t2 - t1, 50);
+		assert.greaterThan(t2 - t1, 49);
 		assert.lessThan(t2 - t1, 100);
 
 		var c2 = new net.Socket();
