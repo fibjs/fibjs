f9fadaa62eb4723270fe6a29bd2c6863993aadc5
响马 <lion@9465.net>
2017-01-22 23:43:37 +0800

    重构 pathAdd 实现，创建 Path 对象用于连续 add

--- fibjs/include/path.h
+++ fibjs/include/path.h
@@ -29,45 +29,113 @@ inline bool pathcmpchr(char ch1, char ch2)
 
 #endif
 
+class Path
+{
+public:
+	Path()
+	{}
+
+	Path(exlib::string init)
+	{
+		append(init);
+	}
+
+public:
+#ifdef _WIN32
+	void append(exlib::string other)
+	{
+		if (other.empty())
+			return;
+
+		const char* c_str = other.c_str();
+
+		if (isPathSlash(c_str[0]) && isPathSlash(c_str[1]))
+		{
+			m_buf = other;
+			return;
+		}
+
+		char diskId = 0;
+
+		if (qisascii(c_str[0]) && c_str[1] == ':')
+		{
+			diskId = c_str[0];
+			m_buf = m_disks[diskId & 0x1f];
+			c_str += 2;
+		}
+
+		exlib::string base;
+		const char* c_strBuf = m_buf.c_str();
+		if (*c_strBuf)
+		{
+			if (qisascii(c_strBuf[0]) && c_strBuf[1] == ':')
+			{
+				diskId = c_strBuf[0];
+				base = m_buf.substr(2);
+			} else
+				base = m_buf;
+		}
+
+		if (!base.empty() && !isPathSlash(base[base.length() - 1]))
+			base.append(1, PATH_SLASH);
+
+		if (isPathSlash(c_str[0]))
+			base = c_str;
+		else
+			base.append(c_str);
+
+		if (diskId)
+		{
+			m_buf.assign(1, diskId);
+			m_buf.append(1, ':');
+			m_buf.append(base);
+		} else
+			m_buf = base;
+
+		if (diskId)
+			m_disks[diskId & 0x1f] = m_buf;
+	}
+
+private:
+	exlib::string m_disks[27];
+
+#else
+	void append(exlib::string other)
+	{
+		if (other.empty())
+			return;
+
+		const char* c_str = other.c_str();
+
+		if (isPathSlash(c_str[0]))
+			m_buf = other;
+		else
+		{
+			if (!m_buf.empty() && !isPathSlash(m_buf[m_buf.length() - 1]))
+				m_buf.append(1, PATH_SLASH);
+
+			m_buf.append(other);
+		}
+	}
+
+#endif
+
+public:
+	exlib::string str()
+	{
+		return m_buf;
+	}
+
+private:
+	exlib::string m_buf;
+};
+
 inline void pathAdd(exlib::string& strBuffer, exlib::string other)
 {
-	bool bRoot;
+	Path p(strBuffer);
 
-	if (!other.empty())
-	{
-		const char* c_str = other.c_str();
-#ifdef _WIN32
-		char diskID = 0;
-
-		if (strBuffer[0] != 0 && strBuffer[1] == ':')
-			diskID = strBuffer[0];
-
-		if (c_str[0] != 0 && c_str[1] == ':')
-		{
-			if (!pathcmpchr(c_str[0], diskID) && !isPathSlash(c_str[2]))
-			{
-				c_str += 2;
-				bRoot = false;
-			}
-			else
-				bRoot = true;
-		}
-		else if (isPathSlash(c_str[0]) && isPathSlash(c_str[1]))
-			bRoot = true;
-		else
-#endif
-			if (isPathSlash(c_str[0]))
-				bRoot = true;
-			else
-				bRoot = false;
-
-		if (bRoot)
-			strBuffer.clear();
-		else if (!strBuffer.empty() && !isPathSlash(strBuffer[strBuffer.length() - 1]))
-			strBuffer.append(1, PATH_SLASH);
-
-		strBuffer.append(c_str);
-	}
+	p.append(other);
+	strBuffer = p.str();
 }
 
 } /* namespace fibjs */
