2280c1b183876276ee2d9afa968d7ea89c1c6a37
Hengfei Zhuang <zhuanghengfei@gmail.com>
2017-04-24 17:10:22 +0800

    fix: build on linux (#201)
    * fix: build on linux
    
    * chore:simplify

--- fibjs/src/os/os_posix.cpp
+++ fibjs/src/os/os_posix.cpp
@@ -16,8 +16,13 @@
 #include <sys/utsname.h>
 #include <ifaddrs.h>
 #include <net/if.h>
-#include <net/if_dl.h>
 #include <string.h>
+
+#ifdef Linux
+#include <linux/if_packet.h>
+#elif defined(Darwin)
+#include <net/if_dl.h>
+#endif
 
 namespace fibjs {
 
@@ -131,7 +136,11 @@ result_t os_base::networkInfo(v8::Local<v8::Object>& retVal)
     for (ent = addrs; ent != NULL; ent = ent->ifa_next) {
         if (!((ent->ifa_flags & IFF_UP) && (ent->ifa_flags & IFF_RUNNING)) ||
             (ent->ifa_addr == NULL) ||
+#ifdef Linux
+            (ent->ifa_addr->sa_family != AF_PACKET)) {
+#else
             (ent->ifa_addr->sa_family != AF_LINK)) {
+#endif
             continue;
         }
 
@@ -147,13 +156,19 @@ result_t os_base::networkInfo(v8::Local<v8::Object>& retVal)
         }
         ret = v8::Local<v8::Array>::Cast(retVal->Get(name));
 
+#ifdef Linux
+        struct sockaddr_ll *s = (struct sockaddr_ll*)ent->ifa_addr;
+        int macAddrlen = 0;
+        for(int i = 0; i < 6; i++)
+            macAddrlen += sprintf(mac + macAddrlen, "%02X%s", s->sll_addr[i], i < 5 ? ":" : "");
+#else
         ptr = (unsigned char *)LLADDR((struct sockaddr_dl*)(ent->ifa_addr));
         sprintf(mac, "%02x:%02x:%02x:%02x:%02x:%02x",
-                                    *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5));
-    
+                *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5));
+#endif
         int32_t len = ret->Length();
         for (int i = 0; i < len; i++)
-        {   
+        {
             v8::Local<v8::Object> o = v8::Local<v8::Object>::Cast(ret->Get(i));
             o->Set(isolate->NewFromUtf8("mac"), isolate->NewFromUtf8(mac));
         }
