dfeb3ed9465de6c3b9b8f48006410a1df544c893
Hengfei Zhuang <zhuanghengfei@gmail.com>
2017-05-13 09:20:54 +0800

    feat: auto release (#206)

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,1 +1,39 @@
-# todo
+#!/bin/bash
+
+set -ev
+
+TARGET_ARCH=${ARCH}
+
+case "${ARCH}" in
+  i386)
+    TARGET_ARCH=x86
+      ;;
+  amd64)
+    TARGET_ARCH=x64
+      ;;
+esac
+
+mkdir -p dist
+
+DIST_FILE=""
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then # linux
+  DIST_FILE=bin/Linux_${ARCH}_release
+
+  FIBJS_FILE=${DIST_FILE}/fibjs
+  INSTALLER_FILE=${DIST_FILE}/installer.sh
+
+  cp ${FIBJS_FILE} dist/fibjs-linux-${TARGET_ARCH}
+  cp ${INSTALLER_FILE} dist/installer-linux-${TARGET_ARCH}.sh
+
+else # darwin
+  DIST_FILE=bin/Darwin_amd64_release
+
+  FIBJS_FILE=${DIST_FILE}/fibjs
+  INSTALLER_FILE=${DIST_FILE}/installer.sh
+
+  cp ${FIBJS_FILE} dist/fibjs-darwin-x64
+  cp ${INSTALLER_FILE} dist/installer-darwin-x64.sh
+fi
+
+exit 0;
