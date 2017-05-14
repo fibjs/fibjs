811b7c01bf95f77967cdb193f343686cbc9570c4
ngot <zhuanghengfei@gmail.com>
2017-05-14 11:59:27 +0800

    chore:change build image

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "start docker build..."
-    DIR=`pwd`;sudo  docker run -it -v ${DIR}:/home/ci fibjs/fibjs:build-env /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH}"
+    DIR=`pwd`;sudo  docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH}"
 else
     sh build -j2 ${ARCH}
 fi
