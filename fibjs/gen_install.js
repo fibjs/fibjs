f33910493229b8d013b58902627df8fcaff93cc0
ngot <zhuanghengfei@gmail.com>
2017-05-19 00:23:51 +0800

    feat: windows cab files

--- fibjs/gen_install.js
+++ fibjs/gen_install.js
@@ -3,18 +3,18 @@ var process = require("process");
 
 var build = false;
 
-if (!fs.exists("js.cab"))
+if (!fs.exists("fibjs.cab"))
 	build = true;
 else {
 	var st1 = fs.stat("fibjs.exe");
-	var st2 = fs.stat("js.cab");
+	var st2 = fs.stat("fibjs.cab");
 	build = st2.mtime < st1.mtime;
 }
 
 if (build)
-	process.run("makecab", ["fibjs.exe", "js.cab"]);
+	process.run("makecab", ["fibjs.exe", "fibjs.cab"]);
 
-var txt = JSON.stringify(fs.open("js.cab").readAll().toArray());
+var txt = JSON.stringify(fs.open("fibjs.cab").readAll().toArray());
 var src = "unsigned char js_data[] = {" + txt.substr(1, txt.length - 2) + "};\n";
 if (!fs.exists("js.h") || fs.readFile("js.h") != src)
 	fs.writeFile("js.h", src);
