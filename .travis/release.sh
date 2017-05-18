30750fb8decc7c63d01d8c58e63d89fabb15a2f2
ngot <zhuanghengfei@gmail.com>
2017-05-18 23:07:50 +0800

    feat: release compress files

--- .travis/release.sh
+++ .travis/release.sh
@@ -22,18 +22,26 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then # linux
 
   FIBJS_FILE=${DIST_FILE}/fibjs
   INSTALLER_FILE=${DIST_FILE}/installer.sh
+  XZ_FILE=${DIST_FILE}/fibjs.xz
+  GZ_FILE=${DIST_FILE}/fibjs.tar.gz
 
   cp ${FIBJS_FILE} dist/fibjs-linux-${TARGET_ARCH}
   cp ${INSTALLER_FILE} dist/installer-linux-${TARGET_ARCH}.sh
+  cp ${XZ_FILE} dist/fibjs-linux-${TARGET_ARCH}.xz
+  cp ${GZ_FILE} dist/fibjs-linux-${TARGET_ARCH}.tar.gz
 
 else # darwin
   DIST_FILE=bin/Darwin_amd64_release
 
   FIBJS_FILE=${DIST_FILE}/fibjs
   INSTALLER_FILE=${DIST_FILE}/installer.sh
+  XZ_FILE=${DIST_FILE}/fibjs.xz
+  GZ_FILE=${DIST_FILE}/fibjs.tar.gz
 
   cp ${FIBJS_FILE} dist/fibjs-darwin-x64
   cp ${INSTALLER_FILE} dist/installer-darwin-x64.sh
+  cp ${XZ_FILE} dist/fibjs-darwin-x64.xz
+  cp ${GZ_FILE} dist/fibjs-darwin-x64.tar.gz
 fi
 
 exit 0;
