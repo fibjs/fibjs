019890c4c1fc1e7349917b2cb6240ea1b1547136
ngot <zhuanghengfei@gmail.com>
2017-05-19 19:10:19 +0800

    fix: add env param

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "start docker build..."
-    DIR=`pwd`;sudo  docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH}"
+    DIR=`pwd`;sudo  docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
 else
     sh build -j2 ${ARCH}
 fi
