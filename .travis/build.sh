ff922c0d1786ab610f84fa5e0860f9b4843c3323
ngot <zhuanghengfei@gmail.com>
2017-05-19 22:15:21 +0800

    fix:osx build

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "start docker build..."
     DIR=`pwd`;sudo  docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
 else
-    sh build -j2 ${ARCH}
+    sh build -j2 ${ARCH} ci
 fi
 
 exit 0;
