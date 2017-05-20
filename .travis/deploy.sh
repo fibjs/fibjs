1472cfa0340ccafd7e1f5b20e4acacde88156c80
ngot <zhuanghengfei@gmail.com>
2017-05-20 14:39:08 +0800

    fix: ssh upload path

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb;
 fi
 
-sshpass -p $DEPLOY_PASS scp -r ./${TRAVIS_TAG}/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_TAG
+cd ${TRAVIS_TAG}
+sshpass -p $DEPLOY_PASS scp -r ./ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_TAG
 
 exit 0;
