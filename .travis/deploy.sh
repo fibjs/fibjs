5fb93104dd30ddb3c4c706feb78d8c794b62fab6
ngot <zhuanghengfei@gmail.com>
2017-05-20 09:09:34 +0800

    ci:add ssh deploy

---
+++ .travis/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ev
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb;
+fi
+
+cd dist
+sshpass -p $DEPLOY_PASS scp -r ./ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_TAG
+
+exit 0;
