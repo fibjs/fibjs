a46b64be03726496c6e4e04263ae1ef0041b1be7
ngot <zhuanghengfei@gmail.com>
2017-05-20 13:22:49 +0800

    feat:add version to filename

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb;
 fi
 
-cd dist
-sshpass -p $DEPLOY_PASS scp -r ./ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_TAG
+sshpass -p $DEPLOY_PASS scp -r ./${TRAVIS_TAG}/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/$TRAVIS_TAG
 
 exit 0;
