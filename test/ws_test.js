9b5e96e3a8f17c1241db963e792b6b82a3d97a2c
响马 <lion@9465.net>
2017-05-27 00:32:32 +0800

    fix: enable ignored test case.

--- test/ws_test.js
+++ test/ws_test.js
@@ -18,7 +18,7 @@ describe('ws', () => {
         });
     });
 
-    xdescribe('Message', () => {
+    describe('Message', () => {
         function load_msg(data) {
             var ms = new io.MemoryStream();
             ms.write(data);
@@ -583,7 +583,7 @@ describe('ws', () => {
 
     });
 
-    xdescribe('WebSocket', () => {
+    describe('WebSocket', () => {
         it("server", () => {
             var httpd = new http.Server(8814 + base_port, new mq.Routing({
                 "^/ws$": ws.upgrade((s) => {
