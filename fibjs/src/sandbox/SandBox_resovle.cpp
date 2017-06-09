6e6ee02be49e7d5d20344d55e2b219ba648877e7
响马 <lion@9465.net>
2017-06-09 14:42:03 +0800

    SandBox: Organize source code.

--- fibjs/src/sandbox/SandBox_resovle.cpp
+++ fibjs/src/sandbox/SandBox_resovle.cpp
@@ -9,11 +9,14 @@
 #include "SandBox.h"
 #include "ifs/encoding.h"
 #include "ifs/fs.h"
+#include "ifs/util.h"
 #include "path.h"
 #include "LruCache.h"
 #include "Buffer.h"
 
 namespace fibjs {
+
+exlib::string s_root;
 
 result_t SandBox::loadFile(exlib::string fname, obj_ptr<Buffer_base>& data)
 {
@@ -134,5 +137,111 @@ result_t SandBox::resovleFile(exlib::string& fname, obj_ptr<Buffer_base>& data,
     }
 
     return CALL_E_FILE_NOT_FOUND;
+}
+
+result_t SandBox::resovleModule(exlib::string base, exlib::string& id, obj_ptr<Buffer_base>& data,
+    v8::Local<v8::Value>& retVal)
+{
+    Isolate* isolate = holder();
+    v8::Local<v8::Object> _mods = mods();
+
+    retVal = _mods->Get(isolate->NewFromUtf8(id));
+    if (!IsEmpty(retVal))
+        return 1;
+
+    v8::Local<v8::Value> func = GetPrivate("require");
+    if (!func->IsUndefined()) {
+        v8::Local<v8::Value> arg = isolate->NewFromUtf8(id);
+        retVal = v8::Local<v8::Function>::Cast(func)->Call(wrap(), 1, &arg);
+        if (retVal.IsEmpty())
+            return CALL_E_JAVASCRIPT;
+
+        if (!IsEmpty(retVal)) {
+            if (!retVal->IsProxy() && retVal->IsObject() && !object_base::getInstance(retVal))
+                util_base::clone(retVal, retVal);
+            InstallModule(id, retVal);
+
+            return 0;
+        }
+    }
+
+    result_t hr;
+    exlib::string fname;
+
+    fname = s_root;
+    pathAdd(fname, id);
+
+    hr = resovleFile(fname, data, &retVal);
+    if (hr != CALL_E_FILE_NOT_FOUND && hr != CALL_E_PATH_NOT_FOUND) {
+        id = fname;
+        return hr;
+    }
+
+    if (!base.empty()) {
+        exlib::string str;
+
+        str = base;
+        while (true) {
+            if (isPathSlash(str[str.length() - 1]))
+                fname = str.substr(0, str.length() - 1);
+            else
+                path_base::dirname(str, fname);
+            if (str.length() == fname.length())
+                break;
+
+            str = fname;
+
+            if (fname.length())
+                fname += PATH_SLASH;
+            fname += "node_modules";
+            fname += PATH_SLASH;
+            fname += id;
+
+            hr = resovleFile(fname, data, &retVal);
+            if (hr != CALL_E_FILE_NOT_FOUND && hr != CALL_E_PATH_NOT_FOUND) {
+                id = fname;
+                return hr;
+            }
+        }
+    }
+
+    return CALL_E_FILE_NOT_FOUND;
+}
+
+result_t SandBox::resovle(exlib::string base, exlib::string& id, obj_ptr<Buffer_base>& data,
+    v8::Local<v8::Value>& retVal)
+{
+    const char* c_str = id.c_str();
+
+    if (c_str[0] == '.' && (isPathSlash(c_str[1]) || (c_str[1] == '.' && isPathSlash(c_str[2])))) {
+        exlib::string strPath;
+
+        path_base::dirname(base, strPath);
+        pathAdd(strPath, id);
+        path_base::normalize(strPath, id);
+    } else {
+        bool isAbs;
+
+        path_base::normalize(id, id);
+        path_base::isAbsolute(id, isAbs);
+        if (!isAbs)
+            return resovleModule(base, id, data, retVal);
+    }
+
+    return resovleFile(id, data, &retVal);
+}
+
+result_t SandBox::resovle(exlib::string id, exlib::string base, exlib::string& retVal)
+{
+    result_t hr;
+    obj_ptr<Buffer_base> data;
+    v8::Local<v8::Value> v;
+
+    hr = resovle(base, id, data, v);
+    if (hr < 0)
+        return hr;
+
+    retVal = id;
+    return 0;
 }
 }
