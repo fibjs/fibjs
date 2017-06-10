7d91bd082dd855133a641264e3e5d0c1681b7c93
ngot <zhuanghengfei@gmail.com>
2017-06-10 16:18:01 +0800

    wasm, fix: fix test

--- test/wasm_test.js
+++ test/wasm_test.js
@@ -8,11 +8,10 @@ const util = require('util');
 
 const wasmDir = path.join(__dirname, 'wasm');
 
-function loadWebAssembly(name) {
+function loadWebAssembly(name, done) {
   var buf = fs.readFile(path.join(wasmDir, name)).hex(' ');
   buf = new Uint8Array(buf.trim().split(/[\s\r\n]+/g).map(str => parseInt(str, 16)));
   var event = new coroutine.Event();
-  var res, err;
   coroutine.start(function() {
     WebAssembly.compile(buf).then(module => {
       var imports = {};
@@ -29,50 +28,22 @@ function loadWebAssembly(name) {
       }
 
       const instance = new WebAssembly.Instance(module, imports)
-      res = instance;
-    }).catch(e => {
-      err = e;
-    });
+      done(undefined, instance);
+    }).catch(done);
   });
-  event.wait();
-  if (err) {
-    throw err;
-  }
-  return res;
 }
 
 describe("wasm", () => {
-  it('test1', () => {
-    var buf = fs.readFile(path.join(wasmDir, 'math.wasm')).hex(' ');
-    buf = new Uint8Array(buf.trim().split(/[\s\r\n]+/g).map(str => parseInt(str, 16)));
-    var event = new coroutine.Event();
-    coroutine.start(function() {
-      WebAssembly.compile(buf).then(module => {
-        var imports = {};
-        imports.env = imports.env || {}
-
-        imports.env.memoryBase = imports.env.memoryBase || 0
-        if (!imports.env.memory) {
-          imports.env.memory = new WebAssembly.Memory({ initial: 256 })
-        }
-
-        imports.env.tableBase = imports.env.tableBase || 0
-        if (!imports.env.table) {
-          imports.env.table = new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
-        }
-
-        const instance = new WebAssembly.Instance(module, imports)
-        var math = instance.exports;
-        assert.equal(math._square(3), 9);
-      }).catch(e => console.log(e.stack));
+  it('load module', util.sync(done => {
+    loadWebAssembly('math.wasm', (err, instance) => {
+      if (err) {
+        return done(err);
+      }
+      var math = instance.exports;
+      assert.equal(math._square(3), 9);
+      done();
     });
-  });
-
-  it('test2', () => {
-    var instance = loadWebAssembly('math.wasm');
-    var math = instance.exports;
-    assert.equal(math._square(3), 10);
-  })
+  }));
 });
 
 test.run(console.DEBUG);
