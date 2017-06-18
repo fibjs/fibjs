d6b2c147edf59db619b23975706cf71e44d315bc
ngot <zhuanghengfei@gmail.com>
2017-06-18 23:00:36 +0800

    ci, fuxbug: fix the wrong arch runner

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,7 +5,9 @@ set -ev
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ $ARCH == "amd64" ]]; then
         ./bin/Linux_amd64_release/fibjs test/main.js
-    else
+    fi
+
+    if [[ $ARCH == "i386" ]]; then
         ./bin/Linux_i386_release/fibjs test/main.js
     fi
 else # darwin
