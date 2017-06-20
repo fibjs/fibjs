f48354b22cd0e3ee6e9392fc2c1cb10be7919bfd
Hengfei Zhuang <zhuanghengfei@gmail.com>
2017-06-20 01:39:26 -0700

    ci, chore: only run x64 test on linux (#262)
    * ci, fuxbug: fix the wrong arch runner
    
    * ci, chore: only run x64 test on linux

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,8 +5,6 @@ set -ev
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ $ARCH == "amd64" ]]; then
         ./bin/Linux_amd64_release/fibjs test/main.js
-    else
-        ./bin/Linux_i386_release/fibjs test/main.js
     fi
 else # darwin
     if [[ $ARCH == "amd64" ]]; then
