00ebd123081e22acb9fc0fadb56c5d25ac9a06f6
ngot <zhuanghengfei@gmail.com>
2017-06-20 11:17:33 +0800

    core, fixbug: fix gen windows installer.exe

--- fibjs/gen_install.js
+++ fibjs/gen_install.js
@@ -14,7 +14,7 @@ else {
 if (build)
 	process.run("makecab", ["fibjs.exe", "fibjs.cab"]);
 
-var txt = JSON.stringify(fs.open("fibjs.cab").readAll().toArray());
+var txt = JSON.stringify(fs.openFile("fibjs.cab").readAll().toArray());
 var src = "unsigned char js_data[] = {" + txt.substr(1, txt.length - 2) + "};\n";
 if (!fs.exists("js.h") || fs.readFile("js.h") != src)
 	fs.writeFile("js.h", src);
