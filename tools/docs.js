24843e4f2e3679430542c2b101f7aa452e8ab74f
Hengfei Zhuang <zhuanghengfei@gmail.com>
2017-06-26 18:44:22 +0800

    idl, refactor: move idl to idl dir & move en idl to this repo (#281)
    * idl, refactor: move idl to idl dir & move en idl to this repo
    
    * tools, refactor: adjust the idl path
    
    * core, chore: regenerate the global.h

--- tools/docs.js
+++ tools/docs.js
@@ -2,7 +2,7 @@ var path = require('path');
 var parser = require('./util/parser');
 var gen_docs = require('./util/gen_docs');
 
-var baseFolder = path.join(__dirname, "../fibjs/include/ifs/");
+var baseFolder = path.join(__dirname, "../idl/zh-cn");
 var docsFolder = path.join(__dirname, "../docs/docs/manual/");
 
 var defs = parser(baseFolder);
--- tools/idlc.js
+++ tools/idlc.js
@@ -2,7 +2,8 @@ var path = require('path');
 var parser = require('./util/parser');
 var gen_code = require('./util/gen_code');
 
+var idlFolder = path.join(__dirname, "../idl/zh-cn");
 var baseFolder = path.join(__dirname, "../fibjs/include/ifs/");
 
-var defs = parser(baseFolder);
+var defs = parser(idlFolder);
 gen_code(defs, baseFolder);
