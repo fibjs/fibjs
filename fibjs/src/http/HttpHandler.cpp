d8435324244f943a090786e42cb0e0fa2159d825
响马 <lion@9465.net>
2017-06-30 17:18:14 +0800

    http: change output compression limit to 64M.

--- fibjs/src/http/HttpHandler.cpp
+++ fibjs/src/http/HttpHandler.cpp
@@ -239,7 +239,7 @@ result_t HttpHandler::invoke(object_base* v, obj_ptr<Handler_base>& retVal,
 
             pThis->m_rep->get_length(len);
 
-            if (len > 128 && len < 1024 * 1024 * 5) {
+            if (len > 128 && len < 1024 * 1024 * 64) {
                 Variant hdr;
 
                 if (pThis->m_req->firstHeader("Accept-Encoding",
