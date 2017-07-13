b3e49a118a8ed0c7bf144c3588bb18500648ef04
响马 <lion@9465.net>
2017-07-13 15:53:34 +0800

    v8: fix snapshot error on windows.

--- test/vm_test/build_jsc.js
+++ test/vm_test/build_jsc.js
@@ -2,5 +2,5 @@ var os = require('os');
 var fs = require('fs');
 var util = require('util');
 
-fs.writeFile("jsc_test_" + os.arch() + ".jsc",
+fs.writeFile(__dirname + "/jsc_test_" + os.arch() + ".jsc",
     util.compile("jsc_test.js", "module.exports = {a : 100};"));
