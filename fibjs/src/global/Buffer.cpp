0753f741642112079e872b13792c6634319e38b3
响马 <lion@9465.net>
2017-08-07 19:22:45 +0800

    Buffer, bugfix: unused variable.

--- fibjs/src/global/Buffer.cpp
+++ fibjs/src/global/Buffer.cpp
@@ -91,7 +91,7 @@ result_t Buffer_base::isBuffer(v8::Local<v8::Value> v, bool& retVal)
     return 0;
 }
 
-result_t Buffer_base::from(v8::Local<v8::Array> datas, 
+result_t Buffer_base::from(v8::Local<v8::Array> datas,
     obj_ptr<Buffer_base>& retVal)
 {
     retVal = new Buffer();
@@ -99,7 +99,7 @@ result_t Buffer_base::from(v8::Local<v8::Array> datas,
     return 0;
 }
 
-result_t Buffer_base::from(v8::Local<v8::ArrayBuffer> datas, 
+result_t Buffer_base::from(v8::Local<v8::ArrayBuffer> datas,
     obj_ptr<Buffer_base>& retVal)
 {
     retVal = new Buffer();
@@ -359,7 +359,6 @@ result_t Buffer::fill(exlib::string v, int32_t offset, int32_t end, obj_ptr<Buff
     retVal = this;
     return 0;
 }
-
 
 result_t Buffer::fill(Buffer_base* v, int32_t offset, int32_t end, obj_ptr<Buffer_base>& retVal)
 {
@@ -883,7 +882,6 @@ result_t Buffer::keys(v8::Local<v8::Object>& retVal)
     Isolate* isolate = holder();
     v8::Local<v8::Array> a = v8::Array::New(isolate->m_isolate, (int32_t)m_data.length());
     int32_t i;
-    const char* _data = m_data.c_str();
 
     for (i = 0; i < (int32_t)m_data.length(); i++)
         a->Set(i, v8::Number::New(isolate->m_isolate, i));
