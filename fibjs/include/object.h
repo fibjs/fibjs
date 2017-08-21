2eff954e3e4cb49ef9d1c000e8f13ab713d868c6
响马 <lion@9465.net>
2017-08-21 23:54:20 +0800

    core: bugfix: method count error in class info of  object.

--- fibjs/include/object.h
+++ fibjs/include/object.h
@@ -477,7 +477,7 @@ inline ClassInfo& object_base::class_info()
     };
 
     static ClassData s_cd = {
-        "object", false, NULL, NULL, 5, s_method, 0,
+        "object", false, NULL, NULL, 4, s_method, 0,
         NULL, 0, NULL, NULL, NULL, NULL
     };
 
