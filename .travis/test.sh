ff90700a11e27f9b10edb75a76ebc5990f61ed06
xicilion <lion@9465.net>
2017-10-17 22:12:20 +0800

    ci, feat: test --prof.

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,13 +4,13 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ $ARCH == "amd64" ]]; then
-        ./bin/Linux_amd64_release/fibjs test/main.js
+        ./bin/Linux_amd64_release/fibjs --prof test
     fi
 else # darwin
     if [[ $ARCH == "amd64" ]]; then
-        ./bin/Darwin_amd64_release/fibjs test/main.js
+        ./bin/Darwin_amd64_release/fibjs --prof test
     else
-        ./bin/Darwin_i386_release/fibjs test/main.js
+        ./bin/Darwin_i386_release/fibjs --prof test
     fi
 fi
 
