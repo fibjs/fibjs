5612b45c8f987a6d259386bc46b621e74dd6c738
ngot <zhuanghengfei@gmail.com>
2017-11-06 22:21:24 +0800

    f

--- test/path_test.js
+++ test/path_test.js
@@ -495,65 +495,31 @@ describe('path', () => {
     });
 
     it("resolve", () => {
-
         var resolveTestsWin32 =
-            // arguments                                    result
-            [
-                [
-                    ['c:/blah\\blah', 'd:/games', 'c:../a'], 'c:\\blah\\a'
-                ],
-                [
-                    ['c:blah\\blah', '/path'], 'c:\\path'
-                ],
-                [
-                    ['c:/ignore', 'd:\\a/b\\c/d', '\\e.exe'], 'd:\\e.exe'
-                ],
-                [
-                    ['c:/ignore', 'c:/some/file'], 'c:\\some\\file'
-                ],
-                [
-                    ['d:/ignore', 'd:some/dir//'], 'd:\\ignore\\some\\dir'
-                ],
-                [
-                    ['.'], process.cwd().replace(/\//g, '\\')
-                ],
-                [
-                    ['//server/share', '..', 'relative\\'], '\\\\server\\share\\relative'
-                ],
-                // [[‘c:/‘, '//'], 'c:\\'],
-                // [[‘c:/‘, '//dir'], 'c:\\dir'],
-                [
-                    ['c:/', '//server/share'], '\\\\server\\share\\'
-                ],
-                [
-                    ['c:/', '//server/share/file'], '\\\\server\\share\\file'
-                ],
-                [
-                    ['c:/', '//server//share'], '\\\\server\\share\\'
-                ],
-                [
-                    ['c:/', '/some//dir'], 'c:\\some\\dir'
-                ]
+            // arguments                               result
+            [[['c:/blah\\blah', 'd:/games', 'c:../a'], 'c:\\blah\\a'],
+            [['c:/ignore', 'd:\\a/b\\c/d', '\\e.exe'], 'd:\\e.exe'],
+            [['c:/ignore', 'c:/some/file'], 'c:\\some\\file'],
+            [['d:/ignore', 'd:some/dir//'], 'd:\\ignore\\some\\dir'],
+            [['.'], process.cwd()],
+            [['//server/share', '..', 'relative\\'], '\\\\server\\share\\relative'],
+            [['c:/', '//'], 'c:\\'],
+            [['c:/', '//dir'], 'c:\\dir'],
+            [['c:/', '//server/share'], '\\\\server\\share\\'],
+            [['c:/', '//server//share'], '\\\\server\\share\\'],
+            [['c:/', '///some//dir'], 'c:\\some\\dir'],
+            [['C:\\foo\\tmp.3\\', '..\\tmp.3\\cycles\\root.js'],
+                'C:\\foo\\tmp.3\\cycles\\root.js']
             ];
         // Posix
         var resolveTestsPosix =
             // arguments                                    result
-            [
-                [
-                    ['/var/lib', '../', 'file/'], '/var/file'
-                ],
-                [
-                    ['/var/lib', '/../', 'file/'], '/file'
-                ],
-                [
-                    ['a/b/c/', '../../..'], process.cwd()
-                ],
-                [
-                    ['.'], process.cwd()
-                ],
-                [
-                    ['/some/dir', '.', '/absolute/'], '/absolute'
-                ]
+            [[['/var/lib', '../', 'file/'], '/var/file'],
+            [['/var/lib', '/../', 'file/'], '/file'],
+            [['a/b/c/', '../../..'], process.cwd()],
+            [['.'], process.cwd()],
+            [['/some/dir', '.', '/absolute/'], '/absolute'],
+            [['/foo/tmp.3/', '../tmp.3/cycles/root.js'], '/foo/tmp.3/cycles/root.js']
             ];
 
         var failures = [];
