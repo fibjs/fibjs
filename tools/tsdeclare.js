50555bebb9792eccd07fb5e5a52ffd7178b695bd
Ray <richardo2016@gmail.com>
2018-06-04 01:27:01 +0800

    tools, feat: support generating *.d.ts by tools/tsdeclare.js based on idlc.js (#421)
    * sandbox, bugfix: fix core dump when required ts in sub sandbox.
    
    * tools, feat: basic support for generating *.d.ts from '*.idl'.
    
    * tools, bugfix: fix lack of overload methods when generating *.d.ts
    
    * tools, feat: separate tsdeclare.js from idlc.js
    
    * tools, bugfix: remove fib-types in tools/package.json

---
+++ tools/tsdeclare.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const gen_ts_type = require('./util/gen_ts_type');
+const defs = require('./idlc')
+
+const typesDistDir = process.env.FIBJS_IDL2TYPE_DIST
+
+if (!typesDistDir) {
+    throw `no target dist for *.d.ts files to be generated.`
+    if (!fs.exists(typesDistDir)) {
+        throw `${FIBJS_IDL2TYPE_DIST} doesn's exist.`
+    }
+
+    console.log(`would write *.d.ts to ${typesDistDir}`)
+    gen_ts_type(defs, typesDistDir);
+}
