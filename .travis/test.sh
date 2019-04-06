a9f2ad3db5b7d0e02218e76d7032098022e5835e
ngot <zhuanghengfei@gmail.com>
2019-04-06 23:29:04 +1100

    build, bugfix: clang build env

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,7 +4,14 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ $ARCH == "amd64" ]]; then
-        ./bin/Linux_amd64_release/fibjs --prof test
+        # Test on ubuntu:18.04
+        DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci ubuntu:18.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs --prof test/main.js"
+
+        # Test on ubuntu:16.04
+        DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci ubuntu:16.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs --prof test/main.js"
+
+        # Test on ubuntu:14.04
+        DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci ubuntu:14.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs --prof test/main.js"
     fi
 else # darwin
     if [[ $ARCH == "amd64" ]]; then
