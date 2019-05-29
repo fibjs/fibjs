6dd3435f42048065c4ad0e9080151dcfb90b7865
richardo2016 <richardo2016@gmail.com>
2019-05-30 02:09:40 +0800

    process, bugfix: fix test case about `process.stdin`/`process.stdout`.

--- test/process/exec.chargeable.js
+++ test/process/exec.chargeable.js
@@ -2,6 +2,16 @@ var io = require('io')
 
 var is = new io.BufferedStream(process.stdin)
 
+/**
+ * @why in platform win32, EOL may be one of [`\r\n`, `\n`];
+ * 
+ * @todo normalize behavior about `EOL` of
+ *       BufferesStream **from stdio** in the future
+ */
+if (process.platform === 'win32') {
+    is.EOL = '\n';
+}
+
 var input = null
 while (true) {
     input = is.readLine()
