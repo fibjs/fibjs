a2a42d097476fe9506b882044c5f423c19ac3ad5
richardo2016 <richardo2016@gmail.com>
2019-12-09 05:42:50 +0800

    [WIP 6] gui_scripts.js support customize built-in javascript in webview.

--- tools/gui_script.js
+++ tools/gui_script.js
@@ -2,28 +2,51 @@ var UglifyJS = require("uglify-js");
 var fs = require('fs');
 var path = require('path');
 
-var code = fs.readTextFile(path.join(__dirname, "../fibjs/src/gui/windows/script/console.js"));
-code = code.replace(/\n[ \t]*/g, '\n');
-code = UglifyJS.minify(code).code;
-var src = [];
-
-var pos = 0;
-var sz = 80;
-
-function c_encode(s) {
-    return s.replace(/[\\\n'"]/g, (c) => {
-        if (c == '\n')
-            return "\\n"
-        return "\\" + c;
-    });
+var platforms = {
+    'windows': {
+        IFMACRO: '_WIN32'
+    },
+    'darwin': {
+        IFMACRO: '__APPLE__'
+    },
+    'linux': {
+        IFMACRO: ''
+    }
 }
+Object.keys(platforms).forEach((platform) => {
+    var js_path = path.join(__dirname, `../fibjs/src/gui/${platform}/script/console.js`);
+    var jscpp_path = path.join(__dirname, `../fibjs/src/gui/${platform}/script/console_js.cpp`);
 
-while (pos < code.length - sz) {
-    src.push(c_encode(code.substr(pos, sz)));
-    pos += sz;
-}
+    if (!fs.exists(js_path))
+        return ;
 
-src.push(c_encode(code.substr(pos)));
+    var pconfig= platforms[platform];
 
-fs.writeFile(path.join(__dirname, "../fibjs/src/gui/windows/script/console_js.cpp"),
-    '#ifdef _WIN32\n\nnamespace fibjs {\nconst wchar_t* g_console_js = L"' + src.join('"\n"') + '";\n}\n\n#endif');
+    var code = fs.readTextFile(js_path);
+    uglify_code: {
+        code = code.replace(/\n[ \t]*/g, '\n');
+        code = UglifyJS.minify(code).code;
+        var src = [];
+    
+        var pos = 0;
+        var sz = 80;
+    
+        function c_encode(s) {
+            return s.replace(/[\\\n'"]/g, (c) => {
+                if (c == '\n')
+                    return "\\n"
+                return "\\" + c;
+            });
+        }
+    
+        while (pos < code.length - sz) {
+            src.push(c_encode(code.substr(pos, sz)));
+            pos += sz;
+        }
+    
+        src.push(c_encode(code.substr(pos)));
+    }
+
+    fs.writeFile(jscpp_path,
+        `#ifdef ${pconfig.IFMACRO}\n\nnamespace fibjs {\nconst wchar_t* g_console_js = L"` + src.join('"\n"') + '";\n}\n\n#endif');
+});
