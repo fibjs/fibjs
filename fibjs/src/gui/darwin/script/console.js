20dff83586238c425195ee6b715529e4964116ed
Ray Chan <richardo2016@gmail.com>
2020-06-15 17:11:26 +0800

    chore: update darwin webview's userscript.

--- fibjs/src/gui/darwin/script/console.js
+++ fibjs/src/gui/darwin/script/console.js
@@ -1,21 +1,4 @@
 (function (w) {
-    if (!w.JSON)
-        w.JSON = {};
-
-    function f(n) {
-        return n < 10 ? "0" + n : n;
-    }
-    if (typeof Date.prototype.toJSON !== "function") {
-        Date.prototype.toJSON = function (key) {
-            return isFinite(this.valueOf()) ? this.getUTCFullYear() + "-" + f(this.getUTCMonth() + 1) + "-" + f(this.getUTCDate()) + "T" + f(this.getUTCHours()) + ":" + f(this.getUTCMinutes()) + ":" + f(this.getUTCSeconds()) + "Z" : null;
-        };
-        String.prototype.toJSON = Number.prototype.toJSON = Boolean.prototype.toJSON = function (key) {
-            return this.valueOf();
-        };
-    }
-})(this);
-
-(function (w) {
     function stringify(obj) {
         if (obj !== null && typeof obj === 'object' && typeof obj.toJSON !== 'function') {
             decirc(obj, '', [], null)
