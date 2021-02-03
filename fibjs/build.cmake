0ad49fa1dca65cd4b624d7fa7b8cf33ac8e9feb7
xicilion <lion@9465.net>
2021-02-03 19:58:10 +0800

    build, refactor: cmake build script.

---
+++ fibjs/build.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required(VERSION 3.12)
+
+include(../vender/tools/get_env.cmake)
+
+set(OUT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../out/${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
+build("${CMAKE_CURRENT_SOURCE_DIR}" "${OUT_PATH}/fibjs")
