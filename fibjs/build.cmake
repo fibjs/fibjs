7b7b4c3b512e9743f7629947a3bfe2dd097bf0a0
xicilion <lion@9465.net>
2021-02-07 12:24:41 +0800

    build, refactor: refactor cmake build script, unify compilation flags.

--- fibjs/build.cmake
+++ fibjs/build.cmake
@@ -3,6 +3,11 @@ cmake_minimum_required(VERSION 3.10)
 if(NOT "${CLEAN_BUILD}" STREQUAL "true")
     include(../vender/tools/get_env.cmake)
 
-    set(OUT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../out/${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
+    set(WORK_ROOT $ENV{WORK_ROOT})
+    if(NOT WORK_ROOT)
+        set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/..")
+    endif()
+
+    set(OUT_PATH "${WORK_ROOT}/out/${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
     build("${CMAKE_CURRENT_SOURCE_DIR}" "${OUT_PATH}/fibjs")
 endif()
