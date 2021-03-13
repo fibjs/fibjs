eee1c25f77c8f0dc11fc5bccc28bf151c1454777
xicilion <lion@9465.net>
2021-03-13 19:33:38 +0800

    gui, feat: add key/mouse event test.

--- test/cef_test.js
+++ test/cef_test.js
@@ -37,8 +37,185 @@ gui.config({
     }
 });
 
+var key_code = {
+    "Backspace": 8,
+    "Tab": 9,
+    "Enter": 13,
+    "ShiftLeft": 16,
+    "ShiftRight": 16,
+    "ControlLeft": 17,
+    "ControlRight": 17,
+    "AltLeft": 18,
+    "AltRight": 18,
+    "Pause": 19,
+    "CapsLock": 20,
+    "Escape": 27,
+    "Theevent": 32,
+    "PageUp": 33,
+    "PageDown": 34,
+    "End": 35,
+    "Home": 36,
+    "ArrowLeft": 37,
+    "ArrowUp": 38,
+    "ArrowRight": 39,
+    "ArrowDown": 40,
+    "PrintScreen": 44,
+    "Insert": 45,
+    "Delete": 46,
+    "Digit0": 48,
+    "Digit1": 49,
+    "Digit2": 50,
+    "Digit3": 51,
+    "Digit4": 52,
+    "Digit5": 53,
+    "Digit6": 54,
+    "Digit7": 55,
+    "Digit8": 56,
+    "Digit9": 57,
+    "KeyA": 65,
+    "KeyB": 66,
+    "KeyC": 67,
+    "KeyD": 68,
+    "KeyE": 69,
+    "KeyF": 70,
+    "KeyG": 71,
+    "KeyH": 72,
+    "KeyI": 73,
+    "KeyJ": 74,
+    "KeyK": 75,
+    "KeyL": 76,
+    "KeyM": 77,
+    "KeyN": 78,
+    "KeyO": 79,
+    "KeyP": 80,
+    "KeyQ": 81,
+    "KeyR": 82,
+    "KeyS": 83,
+    "KeyT": 84,
+    "KeyU": 85,
+    "KeyV": 86,
+    "KeyW": 87,
+    "KeyX": 88,
+    "KeyY": 89,
+    "KeyZ": 90,
+    "MetaLeft": 91,
+    "MetaRight": 92,
+    "ContextMenu": 93,
+    "Numpad0": 96,
+    "Numpad1": 97,
+    "Numpad2": 98,
+    "Numpad3": 99,
+    "Numpad4": 100,
+    "Numpad5": 101,
+    "Numpad6": 102,
+    "Numpad7": 103,
+    "Numpad8": 104,
+    "Numpad9": 105,
+    "NumpadMultiply": 106,
+    "NumpadAdd": 107,
+    "NumpadSubtract": 109,
+    "NumpadDecimal": 110,
+    "NumpadDivide": 111,
+    "F1": 112,
+    "F2": 113,
+    "F3": 114,
+    "F4": 115,
+    "F5": 116,
+    "F6": 117,
+    "F7": 118,
+    "F8": 119,
+    "F9": 120,
+    "F10": 121,
+    "F11": 122,
+    "F12": 123,
+    "NumLock": 144,
+    "ScrollLock": 145,
+    "AudioVolumeMute": 173,
+    "AudioVolumeDown": 174,
+    "AudioVolumeUp": 175,
+    "LaunchMediaPlayer": 181,
+    "LaunchApplication1": 182,
+    "LaunchApplication2": 183,
+    "Semicolon": 186,
+    "Equal": 187,
+    "Comma": 188,
+    "Minus": 189,
+    "Period": 190,
+    "Slash": 191,
+    "Backquote": 192,
+    "BracketLeft": 219,
+    "Backslash": 220,
+    "BracketRight": 221,
+    "Quote": 222
+};
+
 describe("cef", () => {
     var svr;
+
+    function getOuterHTML(win, selector) {
+        var doc = win.dev.DOM.getDocument();
+        var e = win.dev.DOM.querySelector({
+            nodeId: doc.root.nodeId,
+            selector: selector
+        });
+
+        var html = win.dev.DOM.getOuterHTML({
+            nodeId: e.nodeId
+        });
+
+        return html.outerHTML;
+    }
+
+    function click(win, selector) {
+        var doc = win.dev.DOM.getDocument();
+        var e = win.dev.DOM.querySelector({
+            nodeId: doc.root.nodeId,
+            selector: selector
+        });
+
+        var box = win.dev.DOM.getBoxModel({
+            nodeId: e.nodeId
+        });
+
+        var mi = {
+            x: box.model.content[0] + 1,
+            y: box.model.content[1] + 1,
+            button: 'left',
+            clickCount: 1
+        }
+
+        mi.type = "mousePressed";
+        win.dev.Input.dispatchMouseEvent(mi);
+
+        mi.type = "mouseReleased";
+        win.dev.Input.dispatchMouseEvent(mi);
+    }
+
+    function type(win, text) {
+        for (char of text) {
+            win.dev.Input.dispatchKeyEvent({
+                type: "char",
+                text: char
+            });
+        }
+    }
+
+    function press(win, key) {
+        var code = key_code[key];
+        var ki = {
+            key: key,
+            code: key,
+            windowsVirtualKeyCode: code,
+            nativeVirtualKeyCode: code,
+            macCharCode: code
+        };
+
+        ki.type = "keyDown";
+        win.dev.Input.dispatchKeyEvent(ki);
+
+        ki.type = "keyUp";
+        win.dev.Input.dispatchKeyEvent(ki);
+    }
 
     describe("basic", () => {
         it("basic", () => {
@@ -181,18 +358,10 @@ describe("cef", () => {
                 var win = gui.open(`http://cef_test/${item}.html`, opt);
 
                 win.on("load", () => {
-                    var doc = win.dev.DOM.getDocument();
-                    var e = win.dev.DOM.querySelector({
-                        nodeId: doc.root.nodeId,
-                        selector: "div"
-                    });
-
                     var html;
                     for (var i = 0; i < 10; i++) {
-                        html = win.dev.DOM.getOuterHTML({
-                            nodeId: e.nodeId
-                        });
-                        if (html.outerHTML != "<div id=\"test\"></div>")
+                        html = getOuterHTML(win, "div#test");
+                        if (html != "<div id=\"test\"></div>")
                             break;
                         coroutine.sleep(10);
                     }
@@ -200,7 +369,7 @@ describe("cef", () => {
                     win.close();
 
                     try {
-                        assert.equal(html.outerHTML, `<div id=\"test\">${txt}</div>`);
+                        assert.equal(html, `<div id=\"test\">${txt}</div>`);
                         done();
                     } catch (e) {
                         done(e);
@@ -293,6 +462,41 @@ describe("cef", () => {
                     win.close();
                 });
             })
+        });
+
+        it("dispatch key/mouse event", done => {
+            var win = gui.open("cef://test/form.html", {
+                // headless: true
+            });
+
+            win.on("load", () => {
+                click(win, "input#f1");
+                type(win, "hello, 这是文本。");
+
+                press(win, "ArrowLeft");
+                press(win, "ArrowLeft");
+                press(win, "ArrowLeft");
+                type(win, "测试");
+
+                click(win, "button#b1");
+
+                var html;
+                for (var i = 0; i < 10; i++) {
+                    html = getOuterHTML(win, "div#test");
+                    if (html != "<div id=\"test\"></div>")
+                        break;
+                    coroutine.sleep(10);
+                }
+
+                win.close();
+
+                try {
+                    assert.equal(html, `<div id=\"test\">hello, 这是测试文本。</div>`);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
         });
 
         it("FIX: screenshot with wrong clip", done => {
