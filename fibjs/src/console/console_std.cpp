218e55d98dcab04afb19fbe91fcf4f9eef6b3d44
xicilion <lion@9465.net>
2021-04-05 17:41:42 +0800

    console, bugfix: on windows, line breaks are output as \n when stdout/stderr is redirected.

--- fibjs/src/console/console_std.cpp
+++ fibjs/src/console/console_std.cpp
@@ -8,10 +8,57 @@
 #include "console.h"
 #include "Buffer.h"
 #include "UVStream.h"
+#include "ifs/tty.h"
+#include <stdio.h>
 
 namespace fibjs {
 
 extern exlib::LockedList<Isolate> s_isolates;
+
+#ifdef WIN32
+
+inline bool is_atty(int32_t fd)
+{
+    bool _tty;
+    tty_base::isatty(fd, _tty);
+    return _tty;
+}
+
+void std_logger::out(exlib::string& txt, bool is_error)
+{
+    static bool s_tty_out = is_atty(_fileno(stdout));
+    static bool s_tty_err = is_atty(_fileno(stderr));
+    static HANDLE s_console;
+
+    obj_ptr<Stream_base> out;
+    Isolate* isolate = s_isolates.head();
+
+    if (is_error) {
+        if (!s_tty_err) {
+            fwrite(txt.c_str(), 1, txt.length(), stderr);
+            return;
+        }
+
+        if (!isolate->m_stderr)
+            isolate->m_stderr = new UVStream(_fileno(stderr));
+        out = isolate->m_stderr;
+    } else {
+        if (!s_tty_out) {
+            fwrite(txt.c_str(), 1, txt.length(), stdout);
+            return;
+        }
+
+        if (!isolate->m_stdout)
+            isolate->m_stdout = new UVStream(_fileno(stdout));
+        out = isolate->m_stdout;
+    }
+
+    obj_ptr<Buffer_base> data = new Buffer(txt);
+    out->cc_write(data);
+}
+
+#else
+
 void std_logger::out(exlib::string& txt, bool is_error)
 {
     obj_ptr<Stream_base> out;
@@ -32,12 +79,15 @@ void std_logger::out(exlib::string& txt, bool is_error)
     out->cc_write(data);
 }
 
+#endif
+
 result_t std_logger::write(AsyncEvent* ac)
 {
     item* p1;
 
     while ((p1 = m_workinglogs.getHead()) != 0) {
         exlib::string txt;
+
         if (p1->m_priority == console_base::C_NOTICE)
             txt = logger::notice() + p1->m_msg + COLOR_RESET + "\n";
         else if (p1->m_priority == console_base::C_WARN)
