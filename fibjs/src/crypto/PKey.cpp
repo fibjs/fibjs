e3b82dae6764918a38bcd3e0a2cfbd010b275d9c
xicilion <lion@9465.net>
2022-02-04 21:15:25 +0800

    crypto, feat: support P-256K alias.

--- fibjs/src/crypto/PKey.cpp
+++ fibjs/src/crypto/PKey.cpp
@@ -39,6 +39,7 @@ static const curve_info supported_curves[] = {
     { MBEDTLS_ECP_DP_BP512R1, "brainpoolP512r1" },
     { MBEDTLS_ECP_DP_SECP192K1, "secp192k1" },
     { MBEDTLS_ECP_DP_SECP224K1, "secp224k1" },
+    { MBEDTLS_ECP_DP_SECP256K1, "P-256K" },
     { MBEDTLS_ECP_DP_SECP256K1, "secp256k1" },
     { MBEDTLS_ECP_DP_SM2P256R1, "sm2p256r1" },
 };
