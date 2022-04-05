5a136fbbe3c75995d919a5abb1486edd49f72415
xicilion <lion@9465.net>
2022-04-05 23:24:00 +0800

    test, feat: profiler test.

--- test/profiler_test.js
+++ test/profiler_test.js
@@ -17,7 +17,7 @@ var vmid = coroutine.vmid;
 function unlink(pathname) {
     try {
         fs.unlink(pathname);
-    } catch (e) {}
+    } catch (e) { }
 }
 
 describe("profiler", () => {
@@ -60,6 +60,40 @@ describe("profiler", () => {
             "allocated_nodes": 0,
             "details": []
         });
+    });
+
+    it("getHeapCodeStatistics", () => {
+        var hcs = profiler.getHeapCodeStatistics();
+
+        assert.property(hcs, "code_and_metadata_size");
+        assert.property(hcs, "bytecode_and_metadata_size");
+        assert.property(hcs, "external_script_source_size");
+    });
+
+    it("getHeapSpaceStatistics", () => {
+        var hss = profiler.getHeapSpaceStatistics();
+
+        assert.property(hss[0], "space_name");
+        assert.property(hss[0], "space_size");
+        assert.property(hss[0], "space_used_size");
+        assert.property(hss[0], "space_available_size");
+        assert.property(hss[0], "physical_space_size");
+    });
+
+    it("getHeapStatistics", () => {
+        var hs = profiler.getHeapStatistics();
+
+        assert.property(hs, "total_heap_size");
+        assert.property(hs, "total_heap_size_executable");
+        assert.property(hs, "total_physical_size");
+        assert.property(hs, "total_available_size");
+        assert.property(hs, "used_heap_size");
+        assert.property(hs, "heap_size_limit");
+        assert.property(hs, "malloced_memory");
+        assert.property(hs, "external_memory");
+        assert.property(hs, "peak_malloced_memory");
+        assert.property(hs, "number_of_native_contexts");
+        assert.property(hs, "number_of_detached_contexts");
     });
 
     it("Fiber.stack", () => {
