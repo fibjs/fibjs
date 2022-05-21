6f64eaf9b4b6464b1404909868797c83c047ff2c
xicilion <lion@9465.net>
2022-05-21 11:29:14 +0000

    build, refactor: refactor action ci to using native cross compiler.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,10 +6,7 @@ if [[ ! -z $USE_VENDER_DIST ]]; then
     ./scripts/download_vender.sh;
 fi
 
-if [[ "$TARGET_OS" == 'Linux' ]]; then
-    echo "start docker build..."
-    DIR=`pwd`; docker run -t --env GIT_BRANCH=${GIT_BRANCH} --env USE_VENDER_DIST=${USE_VENDER_DIST} --env CI=ci -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; sh build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
-elif [[ "$TARGET_OS" == 'Darwin' ]]; then
+if [[ "$TARGET_OS" != 'Windows' ]]; then
     sh build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci
 else # Window
     exit 1;
--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,38 +3,22 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ $TARGET_ARCH == "amd64" ]]; then
-        # Test on ubuntu:18.04
-        DIR=`pwd`;sudo docker run -t --env CI=$CI -v ${DIR}:/home/ci ubuntu:18.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs test/main.js"
+    case $TARGET_ARCH in
+        arm) export QEMU_LD_PREFIX="/usr/arm-linux-gnueabihf"
+            ;;
+        arm64) export QEMU_LD_PREFIX="/usr/aarch64-linux-gnu"
+            ;;
+        armv6) export QEMU_LD_PREFIX="/usr/arm-linux-gnueabi"
+            ;;
+        mips) export QEMU_LD_PREFIX="/usr/mips-linux-gnu"
+            ;;
+        mips64) export QEMU_LD_PREFIX="/usr/mips64-linux-gnuabi64"
+            ;;
+        i386|amd64)
+            ;;
+    esac
 
-        # Test on ubuntu:16.04
-        DIR=`pwd`;sudo docker run -t --env CI=$CI -v ${DIR}:/home/ci ubuntu:16.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs test/main.js"
-
-        # Test on ubuntu:14.04
-        DIR=`pwd`;sudo docker run -t --env CI=$CI -v ${DIR}:/home/ci ubuntu:14.04 /bin/sh -c "cd /home/ci; ./bin/Linux_amd64_release/fibjs test/main.js"
-    fi
-
-    if [[ $TARGET_ARCH == "arm" ]]; then
-        # Test on arm using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t --env CI=$CI -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_armhf.sh;
-        cp -f ./ci/bin/$DIST_DIR/fibjs ./arm_root_fs/bin/fibjs;
-        cp -rf ./ci/test ./arm_root_fs/home/test
-        chroot ./arm_root_fs fibjs /home/test/main.js"
-    fi
-
-    if [[ $TARGET_ARCH == "arm64" ]]; then
-        # Test on arm64 using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t --env CI=$CI -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_arm64.sh;
-        cp -f ./ci/bin/$DIST_DIR/fibjs ./arm64_root_fs/bin/fibjs;
-        cp -rf ./ci/test ./arm64_root_fs/home/test
-        chroot ./arm64_root_fs fibjs /home/test/main.js"
-    fi
+    ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js
 else # Windows/Darwin
     ./bin/$DIST_DIR/fibjs test
 fi
