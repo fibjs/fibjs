367032bce1ded2be0545a412e0d05fdb84882845
响马 <lion@9465.net>
2022-05-21 18:24:29 +0800

    Create setup_ubuntu.sh

---
+++ .github/workflows/setup_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install clang -y
+apt install make -y
+apt install cmake -y
+apt install git -y
+apt install ccache -y
+apt install libx11-dev -y
+
+apt install g++-x86-64-linux-gnu -y
+
+apt install g++-i686-linux-gnu -y
+
+apt install g++-arm-linux-gnueabihf -y
+
+apt install g++-aarch64-linux-gnu -y
+
+apt install g++-mips-linux-gnu -y
+
+apt install g++-mips64-linux-gnuabi64 -y
+
+apt install qemu-user-static -y
