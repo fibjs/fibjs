b950f8c981ee1c63eaa422dd2fbd6fa5a4dcdc4b
richardo2016 <richardo2016@gmail.com>
2022-08-31 18:32:34 +0800

    build, chore: better changelog generating tool

--- tools/changelog.js
+++ tools/changelog.js
@@ -1,19 +1,45 @@
-var io = require('io');
+var fs = require('fs');
 var path = require('path');
+var io = require('io');
 var child_process = require('child_process');
+
+const changelog = path.resolve(__dirname, '../CHANGELOG.md');
+process.on('exit', () => {
+    if (allOutputs.length) {
+        child_process.run('git', 'checkout HEAD -- CHANGELOG.md'.split(' '))
+
+        let content = fs.readFile(changelog);
+        const d = new Date();
+        const dateStr = `${d.getFullYear()}-${(d.getMonth() + 1 + '').padStart(2, '0')}-${(d.getDate() + '').padStart(2, '0')}`;
+        const versionStr = '0.xx.0';
+        content = [
+            `## ${dateStr}, Version ${versionStr}`,
+            `${allOutputs.join('\n')}\n`,
+            content
+        ].join('\n');
+        fs.writeFile(changelog, content);
+    }
+})
+let allOutputs = [];
+function logToOutput(...msg) {
+    if (process.env.CHANGELOG) {
+        allOutputs = allOutputs.concat(msg.join(' '));
+    }
+    console.log(...msg);
+}
 
 process.chdir(path.join(__dirname, '..'));
 
 function pick_subprocess_out_lines(popen_r) {
     var stdout = new io.BufferedStream(popen_r.stdout);
 
-    return stdout.readLines();
+    return stdout.readLines().map(line => line.trimEnd());
 }
 
 var hash = pick_subprocess_out_lines(
-    child_process.spawn('git', 'rev-list --tags --max-count=1'.split(' '))
+    child_process.spawn('git', 'rev-list --tags=v[0-9]*.[0-9]*.[0-9]* --max-count=1'.split(' '))
 )[0]
-var describe = pick_subprocess_out_lines(child_process.spawn('git', ['describe', '--match', 'v[0-9]*.[0-9]*.[0-9]*', hash]));
+var describe = pick_subprocess_out_lines(child_process.spawn('git', ['describe', '--match', 'v[0-9]*.[0-9]*.[0-9]*', '--tags']));
 var info = (/^(v[\d\.]+)-(\d+)-g([a-f\d]+)/g).exec(describe);
 
 var logs = pick_subprocess_out_lines(
@@ -23,6 +49,9 @@ var logs = pick_subprocess_out_lines(
         '-' + (new Number(info[2] - 1))
     ])
 );
+if (process.platform === 'win32') {
+    logs = logs.reduce((accu, line) => accu.concat(line.split('\n')), []);
+}
 
 var commits = [];
 var changes = {};
@@ -33,7 +62,13 @@ var alias = {
 };
 
 logs.forEach(log => {
+    if (!log) {
+        throw new Error('[changelog] invalid log: ' + log);
+    }
     var log_info = /^([a-f\d]+)-\s*(([^\,\:]*)(\s*,\s*([^\:]*))?\s*:)?\s*(.*)$/.exec(log);
+    if (!log_info) {
+        throw new Error('[changelog] parse log failed: ' + log);
+    }
 
     var hash = log_info[1].substr(0, 10);
     var module = log_info[3];
@@ -76,7 +111,7 @@ function out_changes(key) {
     if (group === undefined)
         return;
 
-    console.log(`* **${key}** :`);
+    logToOutput(`* **${key}** :`);
 
     var keys = Object.keys(group);
 
@@ -87,16 +122,16 @@ function out_changes(key) {
     keys.forEach(k => {
         var chgs = group[k];
         if (k === '~') {
-            console.log(`    * ` + chgs.join('\n    * '));
+            logToOutput(`    * ` + chgs.join('\n    * '));
         } else {
             if (chgs.length === 1)
-                console.log(`    * ${k} - ${chgs[0]}`);
+                logToOutput(`    * ${k} - ${chgs[0]}`);
             else
-                console.log(`    * ${k}:\n      - ` + chgs.join('\n      - '));
+                logToOutput(`    * ${k}:\n      - ` + chgs.join('\n      - '));
         }
     });
 
-    console.log();
+    logToOutput();
 
     delete changes[key];
 }
@@ -109,5 +144,5 @@ out_changes('refactor');
 for (var k in changes)
     out_changes(k);
 
-console.log("### Commits");
-console.log(commits.join('\n'));
+logToOutput("### Commits");
+logToOutput(commits.join('\n'));
