92d5d8c2bb377a0d402103cf707c9c72b8f4caac
xicilion <lion@9465.net>
2023-01-08 22:44:46 +0800

    build, feat: update dockerfiles.

--- dockerfiles/push-image.sh
+++ dockerfiles/push-image.sh
@@ -6,6 +6,7 @@ docker image push fibjs/linux-build-env:i386
 docker image push fibjs/linux-build-env:arm64
 docker image push fibjs/linux-build-env:arm
 docker image push fibjs/linux-build-env:mips64
+docker image push fibjs/linux-build-env:ppc64
 
 docker image push fibjs/alpine-build-env:amd64
 docker image push fibjs/alpine-build-env:arm64
