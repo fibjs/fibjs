c077329dba266f8c46c11127a12f1341714c3fa9
xicilion <lion@9465.net>
2023-01-10 12:54:35 +0800

    build, refactor: refactor dockerfiles, support loong64.

--- dockerfiles/push-image.sh
+++ dockerfiles/push-image.sh
@@ -7,6 +7,7 @@ docker image push fibjs/linux-build-env:arm64
 docker image push fibjs/linux-build-env:arm
 docker image push fibjs/linux-build-env:mips64
 docker image push fibjs/linux-build-env:ppc64
+docker image push fibjs/linux-build-env:loong64
 
 docker image push fibjs/alpine-build-env:amd64
 docker image push fibjs/alpine-build-env:arm64
