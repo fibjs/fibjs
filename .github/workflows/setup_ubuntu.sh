b71f7d8a1c3f1ead51f6b87971d95aa2ca6d3446
xicilion <lion@9465.net>
2023-01-10 14:28:54 +0800

    build, refactor: change ci to docker.

--- .github/workflows/setup_ubuntu.sh
+++ .github/workflows/setup_ubuntu.sh
@@ -2,36 +2,4 @@
 
 sudo apt update
 
-if [ "$BUILD_TARGET" == "" ]; then
-  sudo apt install clang -y
-  sudo apt install make -y
-  sudo apt install cmake -y
-  sudo apt install git -y
-  sudo apt install libx11-dev -y
-
-  if [[ "$TARGET_ARCH" == "amd64" ]]; then
-    sudo apt install g++-x86-64-linux-gnu -y
-  fi
-
-  if [[ "$TARGET_ARCH" == "i386" ]]; then
-    sudo apt install g++-i686-linux-gnu -y
-  fi
-
-  if [[ "$TARGET_ARCH" == "arm" ]]; then
-    sudo apt install g++-arm-linux-gnueabihf -y
-  fi
-
-  if [[ "$TARGET_ARCH" == "arm64" ]]; then
-    sudo apt install g++-aarch64-linux-gnu -y
-  fi
-
-  if [[ "$TARGET_ARCH" == "mips64" ]]; then
-    sudo apt install g++-mips64el-linux-gnuabi64 -y
-  fi
-
-  if [[ "$TARGET_ARCH" == "ppc64" ]]; then
-    sudo apt install g++-powerpc64le-linux-gnu -y
-  fi
-fi
-
 sudo apt install qemu-user-static -y
--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,25 +3,12 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ "$BUILD_TARGET" != "" ]]; then
-        CUR=`pwd`
-        docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
-    else
-        case $TARGET_ARCH in
-            arm) export QEMU_LD_PREFIX="/usr/arm-linux-gnueabihf"
-                ;;
-            arm64) export QEMU_LD_PREFIX="/usr/aarch64-linux-gnu"
-                ;;
-            mips64) export QEMU_LD_PREFIX="/usr/mips64el-linux-gnuabi64"
-                ;;
-            ppc64) export QEMU_LD_PREFIX="/usr/powerpc64le-linux-gnu"
-                ;;
-            i386|amd64)
-                ;;
-        esac
-
-        ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js
+    if [[ "$BUILD_TARGET" == "" ]]; then
+        BUILD_TARGE="linux"
     fi
+
+    CUR=`pwd`
+    docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
 else # Windows/Darwin
     ./bin/$DIST_DIR/fibjs test
 fi
