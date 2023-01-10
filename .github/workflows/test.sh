e42df18f94b24e678876f59117557b1742e3c954
响马 <lion@9465.net>
2023-01-10 14:52:46 +0800

    build, bugfix: test docker name error on linux.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,12 +3,13 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ "$BUILD_TARGET" == "" ]]; then
-        BUILD_TARGE="linux"
-    fi
-
     CUR=`pwd`
-    docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
+
+    if [[ "$BUILD_TARGET" == "" ]]; then
+        docker run --rm -v ${CUR}:/fibjs fibjs/linux-build-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
+    else
+        docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
+    fi
 else # Windows/Darwin
     ./bin/$DIST_DIR/fibjs test
 fi
