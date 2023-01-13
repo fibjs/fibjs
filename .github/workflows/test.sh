a35bc2dafdc8063992693af8f89cd21764cd73ba
xicilion <lion@9465.net>
2023-01-14 02:45:36 +0800

    build, bugfix: alpine test error in ci.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,10 +5,10 @@ set -ev
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
     CUR=`pwd`
 
-    if [[ "$BUILD_TARGET" == "" ]]; then
-        docker run -t --rm -e CI=${CI} -v ${CUR}:/fibjs fibjs/linux-build-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
+    if [[ "$BUILD_TARGET" == "alpine" ]]; then
+        docker run -t --rm -e CI=${CI} -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/${DIST_DIR}/fibjs test/main.js"
     else
-        docker run -t --rm -e CI=${CI} -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/Linux_${TARGET_ARCH}_${BUILD_TYPE}/fibjs test/main.js"
+        docker run -t --rm -e CI=${CI} -v ${CUR}:/fibjs fibjs/linux-build-env:${TARGET_ARCH} bash -c "cd /fibjs; ./bin/${DIST_DIR}/fibjs test/main.js"
     fi
 else # Windows/Darwin
     ./bin/$DIST_DIR/fibjs test
