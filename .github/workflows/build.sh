e36cc83abd9e82b52fc7a846de274bb641305ee2
xicilion <lion@9465.net>
2023-01-14 02:21:47 +0800

    build, refactor: remove SLOW_DCHECKS option.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -10,10 +10,10 @@ if [[ "$TARGET_OS" != 'Windows' ]]; then
     TARGET_OS=`uname`
     if [[ "$BUILD_TARGET" != "" ]]; then
         CUR=`pwd`
-        docker run -t --rm -e USE_VENDER_DIST -e CI=${CI} -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
+        docker run -t --rm -e USE_VENDER_DIST -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
     elif [[ "$TARGET_OS" == "Linux" ]]; then
         CUR=`pwd`
-        docker run -t --rm -e USE_VENDER_DIST -e CI=${CI} -v ${CUR}:/fibjs fibjs/linux-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
+        docker run -t --rm -e USE_VENDER_DIST -v ${CUR}:/fibjs fibjs/linux-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
     else
         bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci
     fi
