2c00b695aa8cf11faa8f43151f109763702f61c0
xicilion <lion@9465.net>
2023-01-17 20:15:31 +0000

    build, refactor: add i385&arm docker.

--- dockerfiles/push-image.sh
+++ dockerfiles/push-image.sh
@@ -1,6 +1,3 @@
-docker image push fibjs/android-build-env:amd64
-docker image push fibjs/android-build-env:arm64
-
 docker image push fibjs/linux-build-env:amd64
 docker image push fibjs/linux-build-env:i386
 docker image push fibjs/linux-build-env:arm64
@@ -10,9 +7,16 @@ docker image push fibjs/linux-build-env:ppc64
 docker image push fibjs/linux-build-env:loong64
 
 docker image push fibjs/alpine-build-env:amd64
+docker image push fibjs/alpine-build-env:i386
 docker image push fibjs/alpine-build-env:arm64
+docker image push fibjs/alpine-build-env:arm
 docker image push fibjs/alpine-build-env:ppc64
 
 docker image push fibjs/alpine-test-env:amd64
+docker image push fibjs/alpine-test-env:i386
 docker image push fibjs/alpine-test-env:arm64
+docker image push fibjs/alpine-test-env:arm
 docker image push fibjs/alpine-test-env:ppc64
+
+docker image push fibjs/android-build-env:amd64
+docker image push fibjs/android-build-env:arm64
