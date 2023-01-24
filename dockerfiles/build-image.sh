e020ab3329c83e19ba1295312984d008d9e13e30
xicilion <lion@9465.net>
2023-01-24 20:40:53 +0000

    build, refactor: remove arm alpine ci.

--- dockerfiles/build-image.sh
+++ dockerfiles/build-image.sh
@@ -9,14 +9,10 @@ docker build --rm -f linux-build-env.loong64 -t fibjs/linux-build-env:loong64 .
 docker build --rm -f alpine-build-env.amd64 -t fibjs/alpine-build-env:amd64 .
 docker build --rm -f alpine-build-env.i386 -t fibjs/alpine-build-env:i386 .
 docker build --rm -f alpine-build-env.arm64 -t fibjs/alpine-build-env:arm64 .
-docker build --rm -f alpine-build-env.arm -t fibjs/alpine-build-env:arm .
-docker build --rm -f alpine-build-env.ppc64 -t fibjs/alpine-build-env:ppc64 .
 
 docker build --rm -f alpine-test-env.amd64 -t fibjs/alpine-test-env:amd64 .
 docker build --rm -f alpine-test-env.i386 -t fibjs/alpine-test-env:i386 .
 docker build --rm -f alpine-test-env.arm64 -t fibjs/alpine-test-env:arm64 .
-docker build --rm -f alpine-test-env.arm -t fibjs/alpine-test-env:arm .
-docker build --rm -f alpine-test-env.ppc64 -t fibjs/alpine-test-env:ppc64 .
 
 docker build --rm -f android-build-env.amd64 -t fibjs/android-build-env:amd64 .
 docker build --rm -f android-build-env.arm64 -t fibjs/android-build-env:arm64 .
