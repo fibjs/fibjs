91737af6f066b29600935763d65d22e14f38ae5f
xicilion <lion@9465.net>
2023-03-29 12:26:15 +0800

    build, feat: add riscv64 docker file.

--- dockerfiles/build-image.sh
+++ dockerfiles/build-image.sh
@@ -4,6 +4,7 @@ docker build --rm -f linux-build-env.arm64 -t fibjs/linux-build-env:arm64 .
 docker build --rm -f linux-build-env.arm -t fibjs/linux-build-env:arm .
 docker build --rm -f linux-build-env.mips64 -t fibjs/linux-build-env:mips64 .
 docker build --rm -f linux-build-env.ppc64 -t fibjs/linux-build-env:ppc64 .
+docker build --rm -f linux-build-env.riscv64 -t fibjs/linux-build-env:riscv64 .
 docker build --rm -f linux-build-env.loong64 -t fibjs/linux-build-env:loong64 .
 
 docker build --rm -f alpine-build-env.amd64 -t fibjs/alpine-build-env:amd64 .
--- dockerfiles/push-image.sh
+++ dockerfiles/push-image.sh
@@ -4,6 +4,7 @@ docker image push fibjs/linux-build-env:arm64
 docker image push fibjs/linux-build-env:arm
 docker image push fibjs/linux-build-env:mips64
 docker image push fibjs/linux-build-env:ppc64
+docker image push fibjs/linux-build-env:riscv64
 docker image push fibjs/linux-build-env:loong64
 
 docker image push fibjs/alpine-build-env:amd64
