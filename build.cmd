48ef6589f762dff38d6692ecdaca77df95b16337
xicilion <lion@9465.net>
2024-01-12 21:43:41 +0800

    build, refactor: refactor build.cmd and CMakeLists.txt

--- build.cmd
+++ build.cmd
@@ -3,28 +3,54 @@
 set WORK_ROOT=%cd%
 set SOURCE_ROOT=%~dp0
 
+set dev=0
+set build_addon=0
+
+set i=0
+for %%a in (%*) do (
+    if "%%a"=="dev" (
+        set dev=1
+        set build_addon=1
+    ) else if "%%a"=="ci" (
+        set USE_VENDER_DIST=1
+        set build_addon=1
+    ) else (
+        set new_args[!i!]=%%a
+        set /a i+=1
+    )
+)
+
+set args_count=0
+for /f %%a in ('set new_args[ 2^>nul ^| find /c "="') do set args_count=%%a
+
+if %dev%==0 (
+    set USE_VENDER_DIST=1
+)
+
 if "%USE_VENDER_DIST%" == "" (
 	if exist "%SOURCE_ROOT%/vender/build.cmd" (
 		cd /d "%SOURCE_ROOT%/vender"
-		call build %*%
+		call build %new_args%
 		if ERRORLEVEL 1 goto exitbuild
 	) else goto inform
 )
 
 cd /d "%SOURCE_ROOT%/fibjs"
-call build %*%
+call build %new_args%
 if ERRORLEVEL 1 goto exitbuild
 
 cd /d "%SOURCE_ROOT%/fibjs/program"
-call build %*%
+call build %new_args%
 if ERRORLEVEL 1 goto exitbuild
 
-cd /d "%SOURCE_ROOT%/fibjs/addons"
-call build %*%
-if ERRORLEVEL 1 goto exitbuild
+if %build_addon%==0 (
+	cd /d "%SOURCE_ROOT%/fibjs/addons"
+	call build %new_args%
+	if ERRORLEVEL 1 goto exitbuild
+)
 
 cd /d "%SOURCE_ROOT%/fibjs/installer"
-call build %*%
+call build %new_args%
 if ERRORLEVEL 1 goto exitbuild
 
 cd /d "%SOURCE_ROOT%"
