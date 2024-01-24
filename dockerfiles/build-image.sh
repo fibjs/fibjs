c4f5ac1dfd63bdc09cc952c3b70a91beb2e5293d
xicilion <lion@9465.net>
2024-01-24 22:50:26 +0800

    build, refactor: add support for loong64ow architecture.

--- dockerfiles/build-image.sh
+++ dockerfiles/build-image.sh
@@ -8,6 +8,7 @@ docker build --rm -f linux-build-env.mips64 -t fibjs/linux-build-env:mips64 .
 docker build --rm -f linux-build-env.ppc64 -t fibjs/linux-build-env:ppc64 .
 docker build --rm -f linux-build-env.riscv64 -t fibjs/linux-build-env:riscv64 .
 docker build --rm -f linux-build-env.loong64 -t fibjs/linux-build-env:loong64 .
+docker build --rm -f linux-build-env.loong64ow -t fibjs/linux-build-env:loong64ow .
 
 docker build --rm -f alpine-build-env.x64 -t fibjs/alpine-build-env:x64 .
 docker build --rm -f alpine-build-env.ia32 -t fibjs/alpine-build-env:ia32 .
--- dockerfiles/push-image.sh
+++ dockerfiles/push-image.sh
@@ -6,6 +6,7 @@ docker image push fibjs/linux-build-env:mips64
 docker image push fibjs/linux-build-env:ppc64
 docker image push fibjs/linux-build-env:riscv64
 docker image push fibjs/linux-build-env:loong64
+docker image push fibjs/linux-build-env:loong64ow
 
 docker image push fibjs/alpine-build-env:x64
 docker image push fibjs/alpine-build-env:ia32
