0ffdab302b9d01a9e3c9cf28dbe9a44057f70868
xicilion <lion@9465.net>
2024-02-07 04:15:16 +0800

    build, refactor: refactor build docker.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,14 +6,10 @@ DIST_EXEC="bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs"
 if [[ $HOST_OS == 'Linux' ]]; then
     CUR=$(pwd)
 
-    if [[ "$BUILD_TARGET" == "linux" ]]; then
-        if [[ $BUILD_ARCH == 'x64' ]]; then
-            docker run -t --rm -e CI=${CI} -v ${CUR}:${CUR} ubuntu:10.04 bash -c "cd ${CUR}; ${DIST_EXEC} test"
-        else
-            docker run -t --rm -e CI=${CI} -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; ${DIST_EXEC} test"
-        fi
+    if [[ $BUILD_ARCH == 'x64' ]]; then
+        docker run -t --rm -e CI=${CI} -v ${CUR}:${CUR} ubuntu:10.04 bash -c "cd ${CUR}; ${DIST_EXEC} test"
     else
-        docker run -t --rm -e CI=${CI} -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-test-env:${BUILD_ARCH} bash -c "cd ${CUR}; ${DIST_EXEC} test"
+        docker run -t --rm -e CI=${CI} -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; ${DIST_EXEC} test"
     fi
 else # Windows/Darwin
     ${DIST_EXEC} test
