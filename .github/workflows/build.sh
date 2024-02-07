c0d16468d8b7d24b05c2b80e57c676c13d693f41
xicilion <lion@9465.net>
2024-02-08 00:50:03 +0800

    build, refactor: file compress error in windows ci.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -16,8 +16,8 @@ if [[ "${BUILD_TAG}" != "" && "${BUILD_TYPE}" == "release" ]]; then
         cp bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs_gui.exe release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-gui-${BUILD_ARCH}.exe
         cp bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/installer.exe release/installer-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}.exe
         cp bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs.cab release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}.cab
-        7z a -tzip release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}.zip fibjs.exe
-        7z a -tzip release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-gui-${BUILD_ARCH}.zip fibjs_gui.exe
+        7z a -tzip release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}.zip bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs.exe
+        7z a -tzip release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-gui-${BUILD_ARCH}.zip bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs_gui.exe
     else
         cp bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/fibjs release/fibjs-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}
         cp bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/installer.sh release/installer-${BUILD_TAG}-${BUILD_TARGET}-${BUILD_ARCH}.sh
