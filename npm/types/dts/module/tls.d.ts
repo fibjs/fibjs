3867e55d4e4d6a61214ed6a8e03c0fcb6c404dd5
xicilion <lion@9465.net>
2024-03-14 17:11:52 +0800

    tls, refactor: update document.

--- npm/types/dts/module/tls.d.ts
+++ npm/types/dts/module/tls.d.ts
@@ -20,14 +20,15 @@ declare module 'tls' {
      *      - cert: PEM 格式的证书链。每个私钥应提供一个证书链。每个证书链应包含所提供的私有 key 的 PEM 格式的证书，后跟 PEM 格式的中间证书（如果有），按顺序排列，并且不包括根 CA（根 CA 必须是预先生成的）。提供多个证书链时，它们的顺序不必与 key 中的私钥相同。如果未提供中间证书，对等方将无法验证证书，握手将失败。
      *      - key: PEM 格式的私钥。PEM 允许选择加密私钥。加密的密钥将使用 options.passphrase 进行解密。
      *      - passphrase: 用于单个私钥和/或 PFX 的共享密码。
-     *      - rejectUnauthorized: 如果不是 false，服务器将拒绝任何未经所提供 CA 列表授权的连接。默认值: true。
+     *      - requestCert: 如果为 true，则服务器将要求客户端证书进行身份验证。默认值: true。
+     *      - rejectUnauthorized: 如果不是 false，服务器将拒绝任何未经所提供 CA 列表授权的连接。客户端模式默认值: true，服务端模式默认值: false。
      *      - maxVersion: 设置允许的最大 TLS 版本。 'TLSv1.3' 、 'TLSv1.2' 、 'TLSv1.1' 或 'TLSv1' 之一。不能与 secureProtocol 选项一起指定。
      *      - minVersion: 设置允许的最低 TLS 版本。 'TLSv1.3' 、 'TLSv1.2' 、 'TLSv1.1' 或 'TLSv1' 之一。不能与 secureProtocol 选项一起指定。
      *      - secureProtocol: 传统机制选择要使用的 TLS 协议版本，不支持最小和最大版本的独立控制，也不支持将协议限制为 TLSv1.3。建议改用 minVersion 和 maxVersion。
      *      - sessionTimeout: 经过多少秒后，服务器创建的 TLS 会话将不再可恢复。默认值: 300。
      * 
      *      @param options 创建安全上下文的选项
-     *      @param isServer 是否是服务器模式
+     *      @param isServer 是否是服务器模式，默认为 false
      *      @return 返回创建的安全上下文
      *      
      */
@@ -35,7 +36,7 @@ declare module 'tls' {
 
     /**
      * @description 创建一个 SecureContext 对象，用于在 tls 模块中维护安全上下文
-     *      @param isServer 是否是服务器模式
+     *      @param isServer 是否是服务器模式，默认为 false
      *      @return 返回创建的安全上下文
      *      
      */
@@ -49,7 +50,7 @@ declare module 'tls' {
     /**
      * @description 根据 url 创建一个 TLS/SSL 连接
      *      @param url 指定连接的 URL
-     *      @param timeout 指定连接超时时间
+     *      @param timeout 指定连接超时时间，默认为 0
      *      @return 返回 TLS/SSL 连接对象
      *      
      */
@@ -61,7 +62,7 @@ declare module 'tls' {
      * @description 根据 url 创建一个 TLS/SSL 连接
      *      @param url 指定连接的 URL
      *      @param secureContext 指定安全上下文
-     *      @param timeout 指定连接超时时间
+     *      @param timeout 指定连接超时时间，默认为 0
      *      @return 返回 TLS/SSL 连接对象
      *      
      */
