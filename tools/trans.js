a5ebed7b4f2c9cf6992101cfc6d8c367fc5d0209
xicilion <lion@9465.net>
2024-10-27 02:15:17 +0800

    tool, refaxctor: refactor trans.js using gui module.

--- tools/trans.js
+++ tools/trans.js
@@ -1,10 +1,9 @@
-var path = require("path");
-var os = require("os");
 var coroutine = require("coroutine");
+var path = require("path");
 var fs = require("fs");
-var hash = require("hash");
-
-var driver = require("./util/driver");
+var crypto = require("crypto");
+var gui = require("gui");
+var driver = require("fib-driver");
 
 var langs = [
     "ca",
@@ -23,6 +22,8 @@ var langs = [
 
 var docs_path = path.join(__dirname, "../docs/web/dist");
 
+const pages = [];
+
 function translate(fname, lang) {
     var out_fname = path.join(docs_path, lang, fname);
     fname = path.join(docs_path, fname);
@@ -39,113 +40,93 @@ function translate(fname, lang) {
         return true;
     }
 
-    var done = false;
+    var page = pages.pop() || gui.open({
+        width: 1280,
+        height: 1024,
+        visible: false
+    });
 
-    function trans_page() {
-        function trans() {
-            page.Runtime.evaluate(`var e = document.querySelector("div.social-share");if(e)e.innerHTML=""`);
-            page.Runtime.evaluate(`var e = document.querySelector("div#toc");if(e)e.innerHTML=""`);
+    page.loadFile(fname);
+    coroutine.sleep(1000);
 
-            page.Runtime.evaluate(`var node = document.createElement('div');node.id='google_translate_element';document.body.appendChild(node);`);
-            page.Runtime.evaluate(`var node = document.createElement('script');node.id="google_translate_callback";node.text="function googleTranslateElementInit() {var tr = new google.translate.TranslateElement({ pageLanguage: 'zh-CN', autoDisplay: false }, 'google_translate_element');}";document.body.appendChild(node);`);
-            page.Runtime.evaluate(`var node = document.createElement('script');node.id="google_translate_script";node.src='https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';document.body.appendChild(node);`);
+    for (var i = 0; i < 500 && !page.isReady(); i++)
+        coroutine.sleep(100);
 
-            var e = page.wait("select.goog-te-combo");
-            coroutine.sleep(1000);
-
-            page.Runtime.evaluate(`var e = document.querySelector("select.goog-te-combo");var eo=e.options;for(i=0;i<eo.length;i++)if(eo[i].value="${lang}")e.selectedIndex=i;e.dispatchEvent(new Event('change'))`);
-
-            var html;
-            for (var i = 0; i < 100; i++) {
-                html = page.getOuterHTML("div.footer");
-                if (html.indexOf("支持我们") == -1)
-                    break;
-                coroutine.sleep(100);
-            }
-            if (html.indexOf("支持我们") != -1)
-                return false;
-
-            coroutine.sleep(1000);
-
-            return true;
-        }
-
-        var html;
-
-        function fix_html() {
-            html = page.getOuterHTML();
-
-            html = html.replace(/<html.*?>/g, "<html>");
-            html = html.replace(/<link type.*?head>/g, "</head>");
-            html = html.replace(/<body.+?>/g, "<body>");
-            html = html.replace(/<body.*?svg>/g, "<body>");
-            html = html.replace(/<div class=\"skiptranslate.*?\/div>/g, "");
-            html = html.replace(/<div id=\"google.*iframe>/g, "");
-        }
-
-        var res_dir = {
-            imgs: 1,
-            css: 1,
-            js: 1,
-            download: 1
-        }
-
-        function fix_res() {
-            html = html.replace(/(href|src)=\"([\.\.\/]*)([^\"]*?)(\/.*?)\"/g, (s, s1, s2, s3, s4) => {
-                if (res_dir[s3] && s2 !== "./")
-                    s = `${s1}="${s2}../${s3}${s4}"`;
-                return s;
-            });
-
-            html = html.replace(/url\(\'\.\/imgs/g, "url(\'..\/imgs");
-        }
-
-        function fix_lang() {
-            html = html.replace(/<div class=\"dropdown-menu\">.*?<\/div>/g, (s) => s.replace(/href=\"/g, s => s + "../"));
-        }
-
-        var win = page.Browser.getWindowForTarget(page.targetId);
-        var doc = page.DOM.getDocument();
-        var e = page.DOM.querySelector(doc.root.nodeId, "body");
-        var box = page.DOM.getBoxModel(e.nodeId);
-
-        page.Browser.setWindowBounds(win.windowId, {
-            width: box.model.width,
-            height: box.model.height
-        });
-
-        if (trans()) {
-            fix_html();
-            fix_res();
-            fix_lang();
-
-            console.notice("update", out_fname);
-            fs.mkdir(path.dirname(out_fname), {
-                recursive: true
-            });
-            fs.writeFile(out_fname, html);
-            done = true;
-        } else
-            console.error("timeout", out_fname);
-
-        page.close();
-        ev.set();
+    if (!page.isReady()) {
+        console.error("page not ready");
+        pages.push(page);
+        return false;
     }
 
-    var browser = driver.launch([
-        '--window-size=2000,100'
-    ]);
-    var page = browser.open();
-    page.Page.enable();
-    page.on("Page.loadEventFired", trans_page);
-    page.Page.navigate(`file://${fname}`);
+    page.eval('document.body.style.transform = "scale(0.1)";');
+    page.eval('document.body.style.transformOrigin = "0 0";');
 
-    var ev = new coroutine.Event();
-    ev.wait();
-    browser.close();
+    page.eval(`var e = document.querySelector("div.social-share");if(e)e.innerHTML=""`);
+    page.eval(`var e = document.querySelector("div#toc");if(e)e.innerHTML=""`);
 
-    return done;
+    page.eval(`var node = document.createElement('div');node.id='google_translate_element';document.body.appendChild(node);`);
+    page.eval(`var node = document.createElement('script');node.id="google_translate_callback";node.text="function googleTranslateElementInit() {var tr = new google.translate.TranslateElement({ pageLanguage: 'zh-CN', autoDisplay: false }, 'google_translate_element');}";document.body.appendChild(node);`);
+    page.eval(`var node = document.createElement('script');node.id="google_translate_script";node.src='https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';document.body.appendChild(node);`);
+
+    page.querySelector("select.goog-te-combo");
+    coroutine.sleep(1000);
+
+    page.eval(`var e = document.querySelector("select.goog-te-combo");var eo=e.options;for(i=0;i<eo.length;i++)if(eo[i].value="${lang}")e.selectedIndex=i;e.dispatchEvent(new Event('change'))`);
+
+    for (var i = 0; i < 500; i++) {
+        var footer = page.querySelector("div.footer").innerHTML;
+        if (footer.indexOf("支持我们") == -1)
+            break;
+        else
+            coroutine.sleep(100);
+
+    }
+
+    if (page.querySelector("div.footer").innerHTML.indexOf("支持我们") != -1) {
+        console.error("translate failed.", lang, fname);
+        pages.push(page);
+        return false;
+    }
+
+    coroutine.sleep(1000);
+
+    var html;
+    html = page.getHtml();
+
+    html = html.replace(/<html.*?>/g, "<html>");
+    html = html.replace(/<link type.*?head>/g, "</head>");
+    html = html.replace(/<body.+?>/g, "<body>");
+    html = html.replace(/<body.*?svg>/g, "<body>");
+    html = html.replace(/<div class=\"skiptranslate.*?\/div>/g, "");
+    html = html.replace(/<div id=\"google.*iframe>/g, "");
+
+    var res_dir = {
+        imgs: 1,
+        css: 1,
+        js: 1,
+        download: 1
+    }
+    html = html.replace(/(href|src)=\"([\.\.\/]*)([^\"]*?)(\/.*?)\"/g, (s, s1, s2, s3, s4) => {
+        if (res_dir[s3] && s2 !== "./")
+            s = `${s1}="${s2}../${s3}${s4}"`;
+        return s;
+    });
+
+    html = html.replace(/url\(\'\.\/imgs/g, "url(\'..\/imgs");
+
+    html = html.replace(/<div class=\"dropdown-menu\">.*?<\/div>/g, (s) => s.replace(/href=\"/g, s => s + "../"));
+
+    console.notice("update", out_fname);
+    fs.mkdir(path.dirname(out_fname), {
+        recursive: true
+    });
+    fs.writeFile(out_fname, html);
+
+    pages.push(page);
+
+    return true;
 }
+
 
 function check_docs() {
     function check_folder(lang, p) {
@@ -177,7 +158,7 @@ var docs_md5;
 var new_docs_md5 = {};
 
 function queue_one(fname) {
-    var md5 = hash.md5(fs.readFile(path.join(docs_path, fname))).digest().hex();
+    var md5 = crypto.createHash("md5").update(fs.readFile(path.join(docs_path, fname))).digest("hex");
     new_docs_md5[md5] = true;
 
     langs.forEach(lang => {
@@ -238,3 +219,8 @@ trans_queue = trans_queue.sort((n1, n2) => {
 coroutine.parallel(trans_queue, item => {
     translate(item[0], item[1], item[2]) || translate(item[0], item[1], item[2]);
 }, 20);
+
+pages.forEach(page => page.close());
+
+// var item = trans_queue[0];
+// translate(item[0], item[1], item[2]);
