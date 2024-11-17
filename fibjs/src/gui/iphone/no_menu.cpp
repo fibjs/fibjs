b76c3f69d9d9ae65203467b78537a51bd6a12090
xicilion <lion@9465.net>
2024-11-18 03:12:18 +0800

    gui, feat: implement alert and confirm methods for iPhone platform.

--- fibjs/src/gui/iphone/no_menu.cpp
+++ fibjs/src/gui/iphone/no_menu.cpp
@@ -27,26 +27,6 @@ result_t gui_base::createTray(v8::Local<v8::Object> opt, obj_ptr<Tray_base>& ret
     return Runtime::setError("Tray not supported in this platform");
 }
 
-result_t gui_base::alert(exlib::string message, AsyncEvent* ac)
-{
-    return Runtime::setError("Alert not supported in this platform");
-}
-
-result_t gui_base::alert(exlib::string title, exlib::string message, AsyncEvent* ac)
-{
-    return Runtime::setError("Alert not supported in this platform");
-}
-
-result_t gui_base::confirm(exlib::string message, bool& retVal, AsyncEvent* ac)
-{
-    return Runtime::setError("Confirm not supported in this platform");
-}
-
-result_t gui_base::confirm(exlib::string title, exlib::string message, bool& retVal, AsyncEvent* ac)
-{
-    return Runtime::setError("Confirm not supported in this platform");
-}
-
 result_t gui_base::chooseFile(v8::Local<v8::Object> options, obj_ptr<NArray>& retVal, AsyncEvent* ac)
 {
     return Runtime::setError("ChooseFile not supported in this platform");
