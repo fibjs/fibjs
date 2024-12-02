0b9c9007e18c40c64dc269ab895b6cda6c705b47
xicilion <lion@9465.net>
2024-12-02 16:23:55 +0800

    path, refactor: refactor test case struct.

--- test/path_match_test.js
+++ test/path_match_test.js
@@ -4,54 +4,152 @@ test.setup();
 var path = require("path");
 
 describe("path.matchesGlob", () => {
-    const basicTests = {
-        "Basic glob patterns": [
-            ["abc", "a*", true],
-            ["abc", "*c", true],
-            ["abc", "a*c", true],
-            ["abc", "*b*", true],
-            ["abc", "?bc", true],
-            ["abc", "a?c", true],
-            ["abc", "??c", true],
-            ["abc", "a*****?c", true],
-            ["abc", "?*****??", true],
-            ["abc", "*****??", true],
-            ["abc", "?***?****c", true],
-            ["abc", "?***?****?", true],
-            ["abc", "?***?****", true],
-            ["abc", "*******c", true],
-            ["abc", "*******?", true]
-        ],
+    const commonTests = {
+        "Basic Pattern Tests": {
+            "Basic glob patterns": [
+                ["a*", "abc", true],
+                ["*c", "abc", true],
+                ["a*c", "abc", true],
+                ["*b*", "abc", true],
+                ["?bc", "abc", true],
+                ["a?c", "abc", true],
+                ["??c", "abc", true],
+                ["a*****?c", "abc", true],
+                ["?*****??", "abc", true],
+                ["*****??", "abc", true],
+                ["?***?****c", "abc", true],
+                ["?***?****?", "abc", true],
+                ["?***?****", "abc", true],
+                ["*******c", "abc", true],
+                ["*******?", "abc", true]
+            ],
 
-        "Character classes": [
-            ["abc", "[abc]bc", true],
-            ["bbc", "[abc]bc", true],
-            ["dbc", "[abc]bc", false],
-            ["abc", "[a-c]b*", true],
-            ["abd", "[a-c]b*", true],
-            ["abe", "[a-c]b*", true],
-            ["bb", "[a-c]b*", true],
-            ["cb", "[a-c]b*", true],
-            ["dbc", "[a-c]b*", false],
-            ["dd", "[^a-c]*", true],
-            ["abc", "[^a-c]*", false],
-            ["-", "[-abc]", true],
-            ["-", "[abc-]", true]
-        ],
+            "Multiple asterisks": [
+                ["a***c", "abc", true],
+                ["a*****?c", "abc", true],
+                ["?*****??", "abc", true],
+                ["*****??", "abc", true],
+                ["?*****?c", "abc", true]
+            ],
 
-        "Empty strings": [
-            ["", "", true],
-            ["abc", "", false],
-            ["", "abc", false]
-        ],
+            "Empty strings": [
+                ["", "", true],
+                ["", "abc", false],
+                ["abc", "", false]
+            ],
 
-        "Multiple asterisks": [
-            ["abc", "a***c", true],
-            ["abc", "a*****?c", true],
-            ["abc", "?*****??", true],
-            ["abc", "*****??", true],
-            ["abc", "?*****?c", true]
-        ]
+            "Character classes": [
+                ["[abc]bc", "abc", true],
+                ["[abc]bc", "bbc", true],
+                ["[abc]bc", "dbc", false],
+                ["[a-c]b*", "abc", true],
+                ["[a-c]b*", "abd", true],
+                ["[a-c]b*", "abe", true],
+                ["[a-c]b*", "bb", true],
+                ["[a-c]b*", "cb", true],
+                ["[a-c]b*", "dbc", false],
+                ["[^a-c]*", "dd", true],
+                ["[^a-c]*", "abc", false],
+                ["[-abc]", "-", true],
+                ["[abc-]", "-", true]
+            ]
+        },
+
+        "Brace and Range Tests": {
+            "brace expansion": {
+                "Basic expansion": [
+                    ["a{b,c}d", ["abd", "acd"], true],
+                    ["a{b,}c", ["abc", "ac"], true],
+                    ["a{1..3}b", ["a1b", "a2b", "a3b"], true],
+                    ["a{b,c}d{e,f}g", ["abdeg", "abdfg", "acdeg", "acdfg"], true]
+                ],
+
+                "Nested braces": [
+                    ["a{b,c{d,e}}f", ["abf", "acdf", "acef"], true],
+                    ["a{b,c{d,e},f}g", ["abg", "acdg", "aceg", "afg"], true],
+                    ["a{b{c,d},e{f,g}}h", ["abch", "abdh", "aefh", "aegh"], true]
+                ],
+
+                "Numeric sequences": [
+                    ["a{1..5}b", ["a1b", "a2b", "a3b", "a4b", "a5b"], true],
+                    ["a{01..03}b", ["a01b", "a02b", "a03b"], true],
+                    ["a{-1..2}b", ["a-1b", "a0b", "a1b", "a2b"], true]
+                ],
+
+                "Special cases": [
+                    ["a{b}c", ["abc"], true],
+                    ["a{b,}c", ["abc", "ac"], true],
+                    ["a{,b}c", ["ac", "abc"], true],
+                    ["a{b,,c}d", ["abd", "ad", "acd"], true],
+                    ["{a,b{,c}d", ["a", "bcd"], true]
+                ],
+
+                "Combined with other patterns": [
+                    ["a{b,c}*", ["ab*", "ac*"], true],
+                    ["a{b,c}/*.js", ["ab/*.js", "ac/*.js"], true],
+                    ["{a,b}${c}", ["a$c", "b$c"], true],
+                    ["${a}{b,c}", ["$ab", "$ac"], true]
+                ],
+
+                "non-match": [
+                    ["a{1..3}b", "a4b", false],
+                    ["a{b,c}d", "abd1", false],
+                    ["a{b,c}d", "abcd", false],
+                    ["a{01..03}b", "a1b", false],
+                    ["a{b,c}/*.js", "ab/dir/test.js", false]
+                ]
+            },
+
+            "range test": {
+                "Number Ranges": [
+                    ["{1..3}.txt", "1.txt", true],
+                    ["{1..3}.txt", "2.txt", true],
+                    ["{1..3}.txt", "3.txt", true],
+                    ["{1..3}.txt", "4.txt", false],
+                    ["{1..3}.txt", "0.txt", false],
+                    ["{01..05}.txt", "01.txt", true],
+                    ["{01..05}.txt", "02.txt", true],
+                    ["{01..05}.txt", "05.txt", true],
+                    ["{01..05}.txt", "1.txt", false],
+                    ["{01..05}.txt", "5.txt", false],
+                    ["{-2..2}.txt", "-1.txt", true],
+                    ["{-2..2}.txt", "0.txt", true],
+                    ["{-2..2}.txt", "2.txt", true],
+                    ["{-2..2}.txt", "-3.txt", false],
+                    ["{-2..2}.txt", "3.txt", false],
+                    ["{-1..1}.txt", "-1.txt", true],
+                    ["{-1..1}.txt", "0.txt", true],
+                    ["{-1..1}.txt", "1.txt", true],
+                    ["{40..50}.txt", "42.txt", true],
+                    ["{40..50}.txt", "39.txt", false],
+                    ["{40..50}.txt", "51.txt", false],
+                    ["file{01..10}.txt", "file01.txt", true],
+                    ["file{01..10}.txt", "file10.txt", true],
+                    ["file{01..10}.txt", "file00.txt", false],
+                    ["file{01..10}.txt", "file11.txt", false],
+                    ["test/{1..3}.txt", "test/1.txt", true],
+                    ["test/{1..3}.txt", "test/4.txt", false],
+                    ["{1..2}-{1..2}.txt", "1-1.txt", true],
+                    ["{1..2}-{1..2}.txt", "2-2.txt", true],
+                    ["{1..2}-{1..2}.txt", "1-3.txt", false],
+                    ["{1..2}-{1..2}.txt", "3-1.txt", false]
+                ],
+
+                "Mixed Number and Text": [
+                    ["test{1..3}a.txt", "test1a.txt", true],
+                    ["test{1..3}a.txt", "test2a.txt", true],
+                    ["test{1..3}a.txt", "test3a.txt", true],
+                    ["test{1..3}a.txt", "test1b.txt", false],
+                    ["test{1..3}a.txt", "test4a.txt", false],
+                    ["test{1..3}_*.txt", "test1_file.txt", true],
+                    ["test{1..3}_*.txt", "test2_data.txt", true],
+                    ["test{1..3}_*.txt", "test4_file.txt", false],
+                    ["file{1..3}[[]*.txt", "file1[data].txt", true],
+                    ["file{1..3}[[]*.txt", "file2[info].txt", true],
+                    ["file{1..3}[[]*.txt", "file4[data].txt", false]
+                ]
+            }
+        }
     };
 
     const platformTests = {
@@ -88,6 +186,12 @@ describe("path.matchesGlob", () => {
             "Escaped characters": [
                 ["abc", "\\*", false],
                 ["*", "\\*", false]
+            ],
+            "complex patterns": [
+                ["a\\b\\c\\d\\e", "a/**/**/e", true],
+                ["a\\b\\c\\d\\e", "**/e", true],
+                ["a\\b\\c\\d\\e", "a/**/d/**", true],
+                ["a\\b\\c\\d\\e", "x/**/e", false]
             ]
         },
 
@@ -124,62 +228,41 @@ describe("path.matchesGlob", () => {
             "Escaped characters": [
                 ["abc", "\\*", false],
                 ["*", "\\*", true]
+            ],
+            "complex patterns": [
+                ["a/b/c/d/e", "a/**/**/e", true],
+                ["a/b/c/d/e", "**/e", true],
+                ["a/b/c/d/e", "a/**/d/**", true],
+                ["a/b/c/d/e", "x/**/e", false]
             ]
         }
     };
 
-    const complexTests = [
-        ["a\\b\\c\\d\\e", "a/**/**/e", true, "win32"],
-        ["a\\b\\c\\d\\e", "**/e", true, "win32"],
-        ["a\\b\\c\\d\\e", "a/**/d/**", true, "win32"],
-        ["a\\b\\c\\d\\e", "x/**/e", false, "win32"],
-        ["a/b/c/d/e", "a/**/**/e", true, "posix"],
-        ["a/b/c/d/e", "**/e", true, "posix"],
-        ["a/b/c/d/e", "a/**/d/**", true, "posix"],
-        ["a/b/c/d/e", "x/**/e", false, "posix"]
-    ];
-
-    const braceTests = {
-        "Basic expansion": [
-            ["a{b,c}d", ["abd", "acd"]],
-            ["a{b,}c", ["abc", "ac"]],
-            ["a{1..3}b", ["a1b", "a2b", "a3b"]],
-            ["a{b,c}d{e,f}g", ["abdeg", "abdfg", "acdeg", "acdfg"]]
-        ],
-        "Nested braces": [
-            ["a{b,c{d,e}}f", ["abf", "acdf", "acef"]],
-            ["a{b,c{d,e},f}g", ["abg", "acdg", "aceg", "afg"]],
-            ["a{b{c,d},e{f,g}}h", ["abch", "abdh", "aefh", "aegh"]]
-        ],
-        "Numeric sequences": [
-            ["a{1..5}b", ["a1b", "a2b", "a3b", "a4b", "a5b"]],
-            ["a{01..03}b", ["a01b", "a02b", "a03b"]],
-            ["a{-1..2}b", ["a-1b", "a0b", "a1b", "a2b"]]
-        ],
-        "Special cases": [
-            ["a{b}c", ["abc"]],
-            ["a{b,}c", ["abc", "ac"]],
-            ["a{,b}c", ["ac", "abc"]],
-            ["a{b,,c}d", ["abd", "ad", "acd"]],
-            ["{a,b{,c}d", ["a", "bcd"]]
-        ],
-        "Combined with other patterns": [
-            ["a{b,c}*", ["ab*", "ac*"]],
-            ["a{b,c}/*.js", ["ab/*.js", "ac/*.js"]],
-            ["{a,b}${c}", ["a$c", "b$c"]],
-            ["${a}{b,c}", ["$ab", "$ac"]]
-        ]
-    };
-
-    describe("common patterns", () => {
-        Object.entries(basicTests).forEach(([groupName, tests]) => {
-            it(groupName, () => {
-                tests.forEach(([str, pattern, expected]) => {
-                    assert.equal(path.matchesGlob(str, pattern), expected);
+    function testSuite(name, tests) {
+        if (Array.isArray(tests)) {
+            it(name, () => {
+                tests.forEach(([pattern, matches, expected]) => {
+                    if (Array.isArray(matches)) {
+                        matches.forEach(match => {
+                            assert.equal(path.win32.matchesGlob(match, pattern), true);
+                            assert.equal(path.posix.matchesGlob(match, pattern), true);
+                        });
+                    } else {
+                        assert.equal(path.win32.matchesGlob(matches, pattern), expected);
+                        assert.equal(path.posix.matchesGlob(matches, pattern), expected);
+                    }
                 });
             });
-        });
-    });
+        } else {
+            describe(name, () => {
+                Object.entries(tests).forEach(([groupName, tests]) => {
+                    testSuite(groupName, tests);
+                });
+            });
+        }
+    }
+
+    testSuite("common patterns", commonTests);
 
     describe("platform specific patterns", () => {
         Object.entries(platformTests).forEach(([platform, groups]) => {
@@ -191,99 +274,6 @@ describe("path.matchesGlob", () => {
                         });
                     });
                 });
-            });
-        });
-    });
-
-    it("complex patterns", () => {
-        complexTests.forEach(([str, pattern, expected, platform]) => {
-            assert.equal(path[platform].matchesGlob(str, pattern), expected);
-        });
-    });
-
-    describe("brace expansion", () => {
-        Object.entries(braceTests).forEach(([groupName, tests]) => {
-            it(groupName, () => {
-                tests.forEach(([pattern, matches]) => {
-                    matches.forEach(match => {
-                        assert.equal(path.win32.matchesGlob(match, pattern), true);
-                        assert.equal(path.posix.matchesGlob(match, pattern), true);
-                    });
-                });
-            });
-        });
-
-        const nonMatchTests = [
-            ["a{1..3}b", "a4b"],
-            ["a{b,c}d", "abd1"],
-            ["a{b,c}d", "abcd"],
-            ["a{01..03}b", "a1b"],
-            ["a{b,c}/*.js", "ab/dir/test.js"]
-        ];
-
-        it("non-matches", () => {
-            nonMatchTests.forEach(([pattern, nonMatch]) => {
-                assert.equal(path.win32.matchesGlob(nonMatch, pattern), false);
-                assert.equal(path.posix.matchesGlob(nonMatch, pattern), false);
-            });
-        });
-    });
-
-    const rangeTests = {
-        "Number Ranges": [
-            ["1.txt", "{1..3}.txt", true],
-            ["2.txt", "{1..3}.txt", true],
-            ["3.txt", "{1..3}.txt", true],
-            ["4.txt", "{1..3}.txt", false],
-            ["0.txt", "{1..3}.txt", false],
-            ["01.txt", "{01..05}.txt", true],
-            ["02.txt", "{01..05}.txt", true],
-            ["05.txt", "{01..05}.txt", true],
-            ["1.txt", "{01..05}.txt", false],
-            ["5.txt", "{01..05}.txt", false],
-            ["-1.txt", "{-2..2}.txt", true],
-            ["0.txt", "{-2..2}.txt", true],
-            ["2.txt", "{-2..2}.txt", true],
-            ["-3.txt", "{-2..2}.txt", false],
-            ["3.txt", "{-2..2}.txt", false],
-            ["-1.txt", "{-1..1}.txt", true],
-            ["0.txt", "{-1..1}.txt", true],
-            ["1.txt", "{-1..1}.txt", true],
-            ["42.txt", "{40..50}.txt", true],
-            ["39.txt", "{40..50}.txt", false],
-            ["51.txt", "{40..50}.txt", false],
-            ["file01.txt", "file{01..10}.txt", true],
-            ["file10.txt", "file{01..10}.txt", true],
-            ["file00.txt", "file{01..10}.txt", false],
-            ["file11.txt", "file{01..10}.txt", false],
-            ["test/1.txt", "test/{1..3}.txt", true],
-            ["test/4.txt", "test/{1..3}.txt", false],
-            ["1-1.txt", "{1..2}-{1..2}.txt", true],
-            ["2-2.txt", "{1..2}-{1..2}.txt", true],
-            ["1-3.txt", "{1..2}-{1..2}.txt", false],
-            ["3-1.txt", "{1..2}-{1..2}.txt", false]
-        ],
-
-        "Mixed Number and Text": [
-            ["test1a.txt", "test{1..3}a.txt", true],
-            ["test2a.txt", "test{1..3}a.txt", true],
-            ["test3a.txt", "test{1..3}a.txt", true],
-            ["test1b.txt", "test{1..3}a.txt", false],
-            ["test4a.txt", "test{1..3}a.txt", false],
-            ["test1_file.txt", "test{1..3}_*.txt", true],
-            ["test2_data.txt", "test{1..3}_*.txt", true],
-            ["test4_file.txt", "test{1..3}_*.txt", false],
-            ["file1[data].txt", "file{1..3}[[]*.txt", true],
-            ["file2[info].txt", "file{1..3}[[]*.txt", true],
-            ["file4[data].txt", "file{1..3}[[]*.txt", false]
-        ]
-    };
-
-    Object.entries(rangeTests).forEach(([groupName, testCases]) => {
-        it(groupName, () => {
-            testCases.forEach(([str, pattern, expected]) => {
-                assert.equal(path.win32.matchesGlob(str, pattern), expected);
-                assert.equal(path.posix.matchesGlob(str, pattern), expected);
             });
         });
     });
