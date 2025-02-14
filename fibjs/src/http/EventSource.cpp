feea6e0d5417057c2eab6a9d49ad5947b8370409
xicilion <lion@9465.net>
2025-02-14 17:16:24 +0800

    http, refactor: change EventSource requests from GET to POST for improved functionality

--- fibjs/src/http/EventSource.cpp
+++ fibjs/src/http/EventSource.cpp
@@ -42,7 +42,7 @@ public:
         if (m_es->m_readyState == EventSource::C_CLOSED)
             return next();
 
-        return m_hc->request("GET", "", v8::Local<v8::Object>(), m_es->m_response, next(opened), true);
+        return m_hc->request("POST", "", v8::Local<v8::Object>(), m_es->m_response, next(opened), true);
     }
 
     ON_STATE(AsyncEventSource, opened)
@@ -171,7 +171,7 @@ result_t EventSource_base::_new(exlib::string url, v8::Local<v8::Object> options
 
     AsyncEventSource* ac = new AsyncEventSource(hc.As<HttpClient>(), es, url);
 
-    result_t hr = hc.As<HttpClient>()->get_request_opts("GET", url, options, ac);
+    result_t hr = hc.As<HttpClient>()->get_request_opts("POST", url, options, ac);
     if (hr != CALL_E_NOSYNC)
         return hr;
 
