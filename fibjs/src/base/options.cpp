e1430c28105b327dd1112450ed6f8ab44be15102
xicilion <lion@9465.net>
2025-05-11 02:41:53 +0800

    v8, refactor: disable turbo store elimination in V8 flags.

--- fibjs/src/base/options.cpp
+++ fibjs/src/base/options.cpp
@@ -202,6 +202,8 @@ void options(int32_t& pos, char* argv[])
     v8::internal::v8_flags.stack_size = stack_size - GUARD_SIZE;
     v8::internal::v8_flags.wasm_code_gc = false;
 
+    v8::internal::v8_flags.turbo_store_elimination = false;
+
 #ifdef iPhone
     v8::internal::v8_flags.expose_wasm = false;
 #endif
