2bfa8d799af3e35c30e02066ba4e5f3dcf3dbe10
xicilion <lion@9465.net>
2026-03-27 15:12:37 +0000

    http, feat: add compatibility for HttpResponse.prototype.pipe to support Node.js stream.Readable

--- fibjs/scripts/internal/sandbox_init.js
+++ fibjs/scripts/internal/sandbox_init.js
@@ -6,6 +6,32 @@
 require('./domexception');
 require('./webstream');
 
+// HttpResponse.prototype.pipe - Node.js stream.Readable compat for node-fetch etc.
+// Reads body data from C++ Stream and pushes into a JS Writable/Duplex dest.
+(function () {
+    try { var proto = require('http').Response.prototype; } catch (_) { return; }
+    if (typeof proto.pipe === 'function') return;
+
+    proto.pipe = function (dest, options) {
+        var src = this;
+
+        src.on('data', function (chunk) {
+            dest.write(chunk);
+        });
+
+        src.on('end', function () {
+            if (typeof dest.end === 'function' && (!options || options.end !== false))
+                dest.end();
+        });
+
+        src.on('error', function (e) {
+            if (dest.emit) dest.emit('error', e);
+        });
+
+        return dest;
+    };
+})();
+
 // process.hrtime.bigint() - Node.js compat: returns high-res time as BigInt nanoseconds
 if (typeof process !== 'undefined' && typeof process.hrtime === 'function' && typeof process.hrtime.bigint !== 'function') {
     process.hrtime.bigint = function () {
